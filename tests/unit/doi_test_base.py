3bd0548c66f5932aff6e1c18561498afa6ba1348
piotrm <piotrm@truera.com>
2022-02-14 13:11:48 -0800

    added model_input test for doi

--- tests/unit/doi_test_base.py
+++ tests/unit/doi_test_base.py
@@ -1,6 +1,10 @@
 import numpy as np
+from trulens.nn.attribution import InternalInfluence
 from trulens.nn.backend import get_backend
 from trulens.nn.distributions import GaussianDoi, LinearDoi, PointDoi
+from trulens.nn.quantities import LambdaQoI
+from trulens.nn.slices import Cut, InputCut, OutputCut
+from trulens.utils.typing import ModelInputs
 
 
 class DoiTestBase(object):
@@ -9,6 +13,16 @@ class DoiTestBase(object):
         # Create an example tensor to use for the tests.
         self.B = get_backend()
         self.z = self.B.as_tensor(np.array([[1., 2., 3.], [0., -1., -2.]]))
+
+
+        # Weights of a simple neural network for testing optional model_input args.
+        self.l1_coeff = 1
+        self.l1_exp = 2
+        self.l2_coeff = 3
+        self.l2_exp = 4
+        # output = l2_coeff * (l1_coeff * (input ^ l1_exp)) ^ l2_exp
+
+        self.consts = np.array([1.0, 2.0, 5.0])
 
     # Tests for PointDoI.
 
@@ -80,6 +94,59 @@ class DoiTestBase(object):
         self.assertTrue(
             np.all(self.B.as_array(res[-1]) == self.B.as_array(self.z + 42)),
             'End point of linear baseline should be computed cut value.')
+
+    def test_model_inputs_for_baseline(self):
+        # Test the model_inptus optional argument for callable baseline.
+        pass
+
+    def test_model_inputs_for_doi(self):
+        # Test the model_inputs optional argument for doi's.
+
+        class DoiOnInput(PointDoi):
+            def __call__(self, z, *, model_inputs: ModelInputs):
+                # ignore value at cut, return model input instead
+                return [model_inputs.args[0]]
+
+        class DoiOnCut(PointDoi):
+            def __call__(self, z, *, model_inputs: ModelInputs):
+                return [z]
+
+        doi1 = DoiOnCut(cut=Cut(1))   
+
+        doi2 = DoiOnInput(cut=Cut(1))
+        # will return values at Cut(0) despite defining values for Cut(1)
+
+        infl1 = InternalInfluence(
+            self.model,
+            (Cut(1), OutputCut()),
+            LambdaQoI(lambda out: out),
+            doi1,
+            multiply_activation=False)
+
+        res1 = infl1.attributions(self.consts)
+
+        l0 = self.consts
+        l1 = self.l1_coeff * (l0 ** self.l1_exp)
+        l2 = self.l2_coeff * (l1 ** self.l2_exp)
+
+        # d l2 / d l1 evaluated at l1
+        expect1 = self.l2_coeff * self.l2_exp * (l1 ** (self.l2_exp-1.0))
+
+        self.assertTrue(np.allclose(res1, expect1))
+        
+        infl2 = InternalInfluence(
+            self.model,
+            (Cut(1), OutputCut()),
+            LambdaQoI(lambda out: out),
+            doi2,
+            multiply_activation=False)
+
+        res2 = infl2.attributions(self.consts)
+
+        # d l2 / d l1 evaluated at l0 (due to doi2 returning model inputs = l0)
+        expect2 = self.l2_coeff * self.l2_exp * (l0 ** (self.l2_exp-1.0))
+
+        self.assertTrue(np.allclose(res2, expect2))
 
     # Tests for GaussianDoI.
 
