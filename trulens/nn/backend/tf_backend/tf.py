adcf18551fac4bf4f3428dfb578fd3618cd5003c
piotrm <piotrm@truera.com>
2022-03-10 18:38:12 -0800

    revert formatting things for now

--- trulens/nn/backend/tf_backend/tf.py
+++ trulens/nn/backend/tf_backend/tf.py
@@ -6,9 +6,7 @@
 import numpy as np
 import tensorflow as tf
 
-from trulens.nn.backend import _ALL_BACKEND_API_FUNCTIONS
-from trulens.nn.backend import Backend
-
+from trulens.nn.backend import _ALL_BACKEND_API_FUNCTIONS, Backend
 __all__ = _ALL_BACKEND_API_FUNCTIONS + ['tf1']
 
 floatX = tf.keras.backend.floatx()
