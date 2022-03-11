3006ca56eba715920615224f7343df2470be66ea
piotrm <piotrm@truera.com>
2022-03-10 18:46:39 -0800

    and one more

--- tests/pytorch/unit/doi_test.py
+++ tests/pytorch/unit/doi_test.py
@@ -5,43 +5,39 @@ from unittest import TestCase, main
 
 from tests.unit.doi_test_base import DoiTestBase
 
-from torch.nn import Module
+from torch import Tensor
+from torch.nn import Linear, Module, ReLU
+from trulens.nn.backend import get_backend
 from trulens.nn.models import get_model_wrapper
 
-
-class Exponential(Module):
-
-    def __init__(this, coeff, exp):
-        super(Exponential, this).__init__()
-
-        this.coeff = coeff
-        this.exp = exp
-
-    def forward(this, x):
-        return this.coeff * (x**this.exp)
-
-
-class DoubleExponential(Module):
-
-    def __init__(this, l1_coeff, l1_exp, l2_coeff, l2_exp):
-        super(DoubleExponential, this).__init__()
-
-        this.layer1 = Exponential(l1_coeff, l1_exp)
-        this.layer2 = Exponential(l2_coeff, l2_exp)
-
-    def forward(this, x):
-        return this.layer2(this.layer1(x))
-
-
 class DoiTest(DoiTestBase, TestCase):
-
     def setUp(self):
         super(DoiTest, self).setUp()
 
+        class Exponential(Module):
+            def __init__(this, coeff, exp):
+                super(Exponential, this).__init__()
+                
+                this.coeff = coeff
+                this.exp = exp
+
+            def forward(this, x):
+                return this.coeff * (x ** this.exp)
+
+        class DoubleExponential(Module):
+
+            def __init__(this):
+                super(DoubleExponential, this).__init__()
+
+                this.layer1 = Exponential(self.l1_coeff, self.l1_exp)
+                this.layer2 = Exponential(self.l2_coeff, self.l2_exp)
+                
+            def forward(this, x):
+                return this.layer2(this.layer1(x))
+
         self.model = get_model_wrapper(
-            DoubleExponential(
-                self.l1_coeff, self.l1_exp, self.l2_coeff, self.l2_exp),
-            input_shape=(1,))
+            DoubleExponential(), input_shape=(1,)
+        )
 
         self.layer0 = None
         self.layer1 = "layer1"
