a8211c6653eaa46aa553f671dd51106c514c773b
corey hu <corey@truera.com>
2022-08-18 17:47:28 +0000

    remove explicit anchor definition checks in models

--- trulens/nn/models/tensorflow_v2.py
+++ trulens/nn/models/tensorflow_v2.py
@@ -202,12 +202,8 @@ class Tensorflow2ModelWrapper(KerasModelWrapper
 
                         if doi_cut.anchor == 'in':
                             layer.input_intervention = intervention_fn
-                        elif doi_cut.anchor == 'out':
-                            layer.output_intervention = intervention_fn
                         else:
-                            raise ValueError(
-                                f"The doi_cut anchor must be one of ('in', 'out'). Got {doi_cut.anchor} instead."
-                            )
+                            layer.output_intervention = intervention_fn
                 else:
                     model_inputs = intervention
 
@@ -223,12 +219,8 @@ class Tensorflow2ModelWrapper(KerasModelWrapper
                             results[i] = inputs[0]
                         else:
                             results[i] = inputs
-                    elif anchor == 'out':
-                        results[i] = output
                     else:
-                        raise ValueError(
-                            f"The anchor must be one of ('in', 'out'). Got {anchor} instead."
-                        )
+                        results[i] = output
 
                 return retrieve
 
