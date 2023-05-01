c85ed226ff17c368bad17edc02f1d364276033a7
Josh Reini <josh.reini@truera.com>
2023-05-01 19:24:35 -0400

    benchmarking app

---
+++ benchmark_streamlit.py
@@ -0,0 +1,62 @@
+import dotenv
+import openai
+import pandas as pd
+import streamlit as st
+from datasets import load_dataset
+
+import tru_feedback
+
+config = dotenv.dotenv_values(".env")
+
+# Set OpenAI API key
+openai.api_key = config['OPENAI_API_KEY']
+
+# Set up Streamlit app
+st.title("Feedback Function Benchmarking")
+function_choice = st.selectbox(
+    'What feedback function do you want to benchmark?', ['sentiment'])
+
+dataset_choice = st.selectbox(
+    'What dataset do you want to use for benchmarking?', ['imdb'])
+
+num_samples = st.selectbox('How many samples do you want to test?',
+                           [1, 10, 50, 100, 1000])
+
+st.write('You selected to benchmark :', function_choice,
+         'on the benchmarking dataset: ', dataset_choice, ' with ',
+         num_samples, ' samples')
+
+# load and sample benchmarking data
+
+
+def load_data(dataset_choice):
+    data = load_dataset(dataset_choice)
+    train = pd.DataFrame(data['train'])
+    test = pd.DataFrame(data['test'])
+    data = train.append(test)
+    return data
+
+
+def sample_data(data, num_samples):
+    return data.sample(num_samples)
+
+
+data = load_data(dataset_choice)
+samples = sample_data(data, num_samples)
+
+# get feedback to test
+samples_with_feedback = samples.copy()
+
+samples_with_feedback['feedback'] = samples_with_feedback['text'].apply(
+    lambda x: tru_feedback.sentiment_function('', x)).astype(int)
+
+samples_with_feedback['correct'] = samples_with_feedback[
+    'label'] == samples_with_feedback['feedback']
+
+score = samples_with_feedback['correct'].sum() / len(samples_with_feedback)
+
+st.write(function_choice, 'scored: ', '{:.1%}'.format(score),
+         'on the benchmark: ', dataset_choice)
+
+if st.button('See Examples'):
+    st.write(samples_with_feedback)
