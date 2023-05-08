17b0952164ebb5082ce84513f294360eb1b51f49
piotrm <piotrm@truera.com>
2023-05-08 00:00:48 -0700

    finishing up

--- tru_chain.py
+++ tru_chain.py
@@ -1,30 +1,169 @@
-import json
+"""
+# Langchain instrumentation and monitoring. 
+
+## Limitations
+
+- Likely not thread safe.
+
+- If the same wrapped sub-chain is called multiple times within a single call to
+  the root chain, the record of this execution will not be exact with regards to
+  the path to the call information. All call dictionaries will appear in a list
+  addressed by the last subchain (by order in which it is instrumented). For
+  example, in a sequential chain containing two of the same chain, call records
+  will be addressed to the second of the (same) chains and contain a list
+  describing calls of both the first and second.
+
+- Some chains cannot be serialized/jsonized. Sequential chain is an example.
+  This is a limitation of langchain itself.
+
+## Basic Usage
+
+- Wrap an existing chain:
+
+```python
+
+    db = TinyDB("db.json")
+    tc = TruChain(t.llm_chain, db=db)
+
+```
+
+- Retrieve the parameters of the wrapped chain:
+
+```python
+
+    tc._model
+
+```
+
+Output:
+
+```json
+
+{'memory': None,
+ 'verbose': False,
+ 'chain': {'memory': None,
+  'verbose': True,
+  'prompt': {'input_variables': ['question'],
+   'output_parser': None,
+   'partial_variables': {},
+   'template': 'Q: {question} A:',
+   'template_format': 'f-string',
+   'validate_template': True,
+   '_type': 'prompt'},
+  'llm': {'model_id': 'gpt2',
+   'model_kwargs': None,
+   '_type': 'huggingface_pipeline'},
+  'output_key': 'text',
+  '_type': 'llm_chain'},
+ '_type': 'TruChain'}
+ 
+ ```
+
+- Make calls like you would to the wrapped chain.
+
+```python
+
+    tc("hello there")
+    tc("hello there general kanobi")
+
+```
+
+- Retrieve the records of the above executions:
+
+```python
+
+    tc.records
+
+```
+
+Output (the ... are the same as in the above model parameter dictionary):
+
+```json
+
+[{...
+  'chain': {...,
+   '_call': {'input': {'inputs': {'question': 'hello there'}},
+    'start_time': '2023-05-07 23:36:13.391052',
+    'end_time': '2023-05-07 23:36:13.566101',
+    'pid': 2340147,
+    'tid': 2340147,
+    'chain_stack': [('chain',)],
+    'output': {'text': " hey. I'm here. G-g-you can tell us some things"}}},
+  ...},
+ {...,
+  'chain': {...,
+   '_call': {'input': {'inputs': {'question': 'hello there general kanobi'}},
+    'start_time': '2023-05-07 23:36:13.573193',
+    'end_time': '2023-05-07 23:36:13.707811',
+    'pid': 2340147,
+    'tid': 2340147,
+    'chain_stack': [('chain',)],
+    'output': {'text': ' what is the name of the game? Katsu Kato: mewt'}}},
+  ...}]
+
+  ```
+
+- Query aspects of those records via TinyDB-like queries, producing a structured
+  pandas.DataFrame:
+
+```python
+
+    tc.select(
+        Record.chain.prompt.template,
+        Record.chain._call.input.inputs.question,
+        Record.chain._call.output.text,
+        where = Record.chain._call.output.text != None
+    )
+
+```
+
+Output (pd.DataFrame):
+
+```
+
+  Record.chain.prompt.template Record.chain._call.input.inputs.question  \
+0             Q: {question} A:                              hello there   
+1             Q: {question} A:               hello there general kanobi   
+
+                      Record.chain._call.output.text  
+0   welcome and thanks for reading my reply A: ju...  
+1   I have heard your story with the girl. Koshun... 
+
+```
+
+- Write out records to TinyDB:
+
+```python
+
+    tc._flush_records()
+
+```
+
+This will result in `tc.records` being empty. All of the records that get
+inserted into TinyDB specified at `TruChain` construction. Alternatively a
+TinyDB can be provided to `_flush_records`.
+
+Note that `tc.select` operates on a TinyDB and flushes records to it first.
+"""
+
 import os
 import threading as th
 from collections import defaultdict
-from dataclasses import asdict, dataclass
 from datetime import datetime
 from inspect import BoundArguments, signature, stack
-from pprint import PrettyPrinter
-from types import NoneType
 from typing import Any, Callable, Dict, List, Optional, Sequence, Tuple, Union
-from tinydb import TinyDB, Query
-from tinydb.table import Table
 
-
-from pydantic import Field, BaseModel
-
-pp = PrettyPrinter()
-
+import pandas as pd
 from langchain.chains.base import Chain
+from pydantic import Field
+from tinydb import Query, TinyDB
+from tinydb.table import Table
 
 # Addresses of chains or their contents. This is used to refer chains/parameters
 # even in cases where the live object is not in memory (i.e. on some remote
 # app).
-Address = Tuple[Union[Any, str, int], ...]
+Path = Tuple[Union[str, int], ...]
 
-# from dataclasses import dataclass
-from typing import Tuple
 
 # Records of a chain run are dictionaries with these keys:
 #
@@ -35,12 +174,41 @@ from typing import Tuple
 # - 'end_time': datetime -- runtime info.
 # - 'pid': int -- process id for debugging multiprocessing.
 # - 'tid': int -- thread id for debuggin threading.
+# - 'chain_stack': List[Path] -- call stack of chain runs. Elements address
+#   chains.
 
-import pandas as pd
-
+# TinyDB queries for looking up parts of records/models and/or filtering on
+# those parts. See _select.
 Record = Query()
 
-def select(table: Table, fields: List[Record], where: Optional[Record] = None) -> pd.DataFrame:
+
+def _query_str(query: Query):
+
+    def render(ks):
+        if len(ks) == 0:
+            return ""
+
+        first = ks[0]
+        if len(ks) > 1:
+            rest = ks[1:]
+        else:
+            rest = ()
+
+        if isinstance(first, str):
+            return f".{first}{render(rest)}"
+        elif isinstance(first, int):
+            return f"[{first}]{render(rest)}"
+        else:
+            RuntimeError(
+                f"Don't know how to render path element {first} of type {type(first)}."
+            )
+
+    return "Record" + render(query._path)
+
+
+def _select(table: Table,
+            queries: List[Query],
+            where: Optional[Query] = None) -> pd.DataFrame:
     rows = []
 
     if where is not None:
@@ -49,19 +217,21 @@ def select(table: Table, fields: List[Record], where: Optional[Record] = None) -
         table_rows = table.all()
 
     for row in table_rows:
-        vals = [project(address=f, obj=row) for f in fields]
+        vals = [project(query=q, obj=row) for q in queries]
         rows.append(vals)
 
-    return pd.DataFrame(rows, columns=fields)
+    return pd.DataFrame(rows, columns=map(_query_str, queries))
 
-def project(query: Record, obj: Any):
+
+def project(query: Query, obj: Any):
     return _project(query._path, obj)
+
 
 def _project(path: List, obj: Any):
     if len(path) == 0:
         return obj
-    
-    first = [0]
+
+    first = path[0]
     if len(path) > 1:
         rest = path[1:]
     else:
@@ -69,42 +239,23 @@ def _project(path: List, obj: Any):
 
     if isinstance(first, str):
         if not isinstance(obj, Dict) or first not in obj:
-            return None    
-        
-        return _project(rest, obj[first])
-    
+            return None
+
+        return _project(path=rest, obj=obj[first])
+
     elif isinstance(first, int):
         if not isinstance(obj, Sequence) or first >= len(obj):
             return None
-        
-        return _project(rest, obj[first])
+
+        return _project(path=rest, obj=obj[first])
     else:
-        raise RuntimeError(f"Don't know how to locate element with key of type {first}")
+        raise RuntimeError(
+            f"Don't know how to locate element with key of type {first}")
+
 
 class TruChain(Chain):
     """
     Wrap a langchain Chain to capture its configuration and evaluation steps. 
-    
-    TODO: NOT thread-safe. Do not use simulatenously from multiple threads for
-    now.
-    
-    Example usage:
-    
-    ```python
-        tru_chain = TruChain(chain=llm_chain)
-
-        # Get a dictionary containing the parameters defining llm_chain such as
-        # prompts. 
-
-        tru_chain.model 
-
-        ... calls to tru_chain as if it were llm_chain ...
-
-        # Get a list of the calls involved in the chain capturing their inputs
-        # and outputs: 
-        
-        tru_chain.records 
-    ```
     """
 
     # The wrapped/instrumented chain.
@@ -117,25 +268,26 @@ class TruChain(Chain):
 
     # Store records here. "exclude=True" means that these fields will not be
     # included in the json/dict dump.
-    records: Optional[List[Dict[Address, List[Dict]]]] = Field(exclude=True)
+    records: Optional[List[Dict[Any, List[Dict]]]] = Field(exclude=True)
 
     # TinyDB json database to write records to. Need to call _flush_records for
     # this though.
-    db: Optional[tinydb.TinyDB] = Field(exclude=True)
+    db: Optional[TinyDB] = Field(exclude=True)
 
-    def __init__(self, chain: Chain, db: Optional[tinydb.TinyDB] = None):
+    def __init__(self, chain: Chain, db: Optional[TinyDB] = None):
         """
         Wrap a chain for monitoring.
 
         Arguments:
         - chain: Chain -- the chain to wrap.
+        - db: Optional[TinyDB] -- TinyDB database for storing records.
         """
 
         Chain.__init__(self)
 
         self.chain = chain
 
-        self._instrument(self.chain, ("chain",))
+        self._instrument(chain=self.chain, query=Record.chain)
         self.recording = False
         self.records = []
 
@@ -145,9 +297,24 @@ class TruChain(Chain):
     def _model(self):
         return self.dict()
 
-    def _flush_records(self, db: Optional[tinydb.TinyDB] = None):
+    def select(self,
+               *query: Tuple[Query],
+               where: Optional[Query] = None,
+               table: Optional[Table] = None):
+        self._flush_records()
+
+        if isinstance(query, Query):
+            queries = [query]
+        else:
+            queries = query
+
+        table = table if table is not None else self.db.table("records")
+
+        return _select(table, queries, where)
+
+    def _flush_records(self, db: Optional[TinyDB] = None):
         # TODO: locks
-        
+
         # NOTE: TinyDB is annoyingly false.
         db = db if db is not None else self.db
 
@@ -190,6 +357,7 @@ class TruChain(Chain):
 
         try:
             ret = self.chain._call(*args, **kwargs)
+
         except BaseException as e:
             error = str(e)
             print(f"WARNING: {e}")
@@ -199,9 +367,13 @@ class TruChain(Chain):
         assert len(record) > 0, "No information recorded in call."
 
         model = self.dict()
-        for addr, calls in record.items():
-            obj = project(addr, model)
-            obj.update(dict(_call=calls))
+        for path, calls in record.items():
+            obj = _project(path=path, obj=model)
+            if obj is None:
+                print(f"WARNING: Cannot locate {path} in model.")
+                model['_call_not_found_in_model'] = calls
+            else:
+                obj.update(dict(_call=calls))
 
         self.records.append(model)
 
@@ -231,7 +403,7 @@ class TruChain(Chain):
                         f"No local named {key} in {func} found.")
 
         return None
-    
+
     def _instrument_chain_type(self, chain, prop):
         """
         Instrument the Chain class's method _chain_type which is presently used
@@ -252,7 +424,7 @@ class TruChain(Chain):
 
             except NotImplementedError as e:
 
-                ret = dict(error=e)
+                ret = dict(error=str(e))
                 ret['class'] = chain.__class__.__name__
                 ret['module'] = chain.__class__.__module__
 
@@ -263,7 +435,7 @@ class TruChain(Chain):
 
         return new_prop
 
-    def _instrument_method(self, address, func):
+    def _instrument_method(self, query: Query, func: Callable):
         """
         Instrument a Chain method to capture its inputs/outputs/errors.
         """
@@ -306,10 +478,9 @@ class TruChain(Chain):
 
                 start_time = datetime.now()
 
-                # key = self._address_hashable(address)
                 chain_stack = self._get_local_in_call_stack(
                     key="chain_stack", func=wrapper, offset=1) or []
-                chain_stack = chain_stack + [address]  # args[0] is self
+                chain_stack = chain_stack + [query._path]
 
                 try:
                     # Using sig bind here so we can produce a list of key-value
@@ -332,15 +503,23 @@ class TruChain(Chain):
                                 end_time=str(end_time),
                                 pid=os.getpid(),
                                 tid=th.get_native_id(),
-                                chain_stack=chain_stack
-                                )
+                                chain_stack=chain_stack)
 
                 if error is not None:
                     row_args['error'] = error
                 else:
                     row_args['output'] = ret
 
-                record[address].append(row_args)
+                # If there already is a call recorded at the same path, turn the calls into a list.
+                if query._path in record:
+                    existing_call = record[query._path]
+                    if isinstance(existing_call, dict):
+                        record[query._path] = [existing_call, row_args]
+                    else:
+                        record[query._path].append(row_args)
+                else:
+                    # Otherwise record just the one call not inside a list.
+                    record[query._path] = row_args
 
                 if error is not None:
                     raise error
@@ -353,13 +532,13 @@ class TruChain(Chain):
         # don't pollute its list of fields. Note that this address may be
         # deceptive if the same subchain appears multiple times in the wrapped
         # chain.
-        wrapper._address = address
+        wrapper._query = query
 
         return wrapper
 
-    def _instrument(self, chain, address=()):
+    def _instrument(self, chain, query: Query):
         if self.verbose:
-            print(f"instrumenting {address} {chain.__class__.__name__}")
+            print(f"instrumenting {query._path} {chain.__class__.__name__}")
 
         c = chain.__class__
 
@@ -381,8 +560,9 @@ class TruChain(Chain):
                 if self.verbose:
                     print(f"instrumented {cp}._call")
 
-                setattr(cp, "_call",
-                        self._instrument_method(address, original_fun))
+                setattr(
+                    cp, "_call",
+                    self._instrument_method(query=query, func=original_fun))
 
             if hasattr(cp, "_chain_type"):
                 prop = getattr(cp, "_chain_type")
@@ -399,14 +579,11 @@ class TruChain(Chain):
                 pass
 
             elif isinstance(v, Chain):
-                self._instrument(v, address=address + (k, ))
+                self._instrument(chain=v, query=query[k])
 
             elif isinstance(v, Sequence):
                 for i, sv in enumerate(v):
                     if isinstance(sv, Chain):
-                        self._instrument(sv, address=address + (
-                            k,
-                            i,
-                        ))
+                        self._instrument(chain=sv, query=query[k][i])
 
             # TODO: check if we want to instrument anything not accessible through __fields__ .
