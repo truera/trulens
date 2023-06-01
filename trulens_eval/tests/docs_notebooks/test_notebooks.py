4815343a44809af5de59c50b5845150992b76177
Ricardo Shih <rick@ailens.io>
2023-06-01 15:47:39 -0500

    add notebooks to ci/cd

---
+++ trulens_eval/tests/docs_notebooks/test_notebooks.py
@@ -0,0 +1,33 @@
+from os import listdir
+from unittest import main
+from unittest import TestCase
+
+from nbconvert.preprocessors import ExecutePreprocessor
+from nbformat import read
+
+
+class DocsNotebookTests(TestCase):
+    pass
+
+
+def get_unit_test_for_filename(filename):
+
+    def test(self):
+        with open(f'../docs/trulens_eval/{filename}') as f:
+            nb = read(f, as_version=4)
+            ExecutePreprocessor(
+                timeout=600, kernel_name='trulens_llm'
+            ).preprocess(nb, {})
+
+    return test
+
+
+for filename in listdir('../docs/trulens_eval'):
+    if filename.endswith('.ipynb'):
+        setattr(
+            DocsNotebookTests, 'test_' + filename.split('.ipynb')[0],
+            get_unit_test_for_filename(filename)
+        )
+
+if __name__ == '__main__':
+    main()
