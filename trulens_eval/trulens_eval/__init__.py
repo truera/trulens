a9a4cf6a073f51114cbceaa2f4361120ad451724
Piotr Mardziel <piotrm@gmail.com>
2023-06-03 19:47:49 -0700

    json paths and structures (#147)
    * First
    
    * work
    
    * testing work
    
    * more tracking and call printout in dashboard
    
    * testing local chain
    
    * hide a few columns by default
    
    * minor
    
    * work
    
    * work
    
    * cleanup work
    
    * more cleanup
    
    * fixing up json paths
    
    * more updating
    
    * testing the updates
    
    * update readme
    
    * work
    
    * work
    
    * work
    
    * finished
    
    * clean
    
    * delete backup
    
    * fix merge
    
    * one more
    
    * cleanup notebooks
    
    * typo
    
    ---------
    
    Co-authored-by: piotrm <piotrm@truera.com>

--- trulens_eval/trulens_eval/__init__.py
+++ trulens_eval/trulens_eval/__init__.py
@@ -1,5 +1,27 @@
 """
-Imports of most common parts of the library. Should include everything to get started.
+# Imports of most common parts of the library.
+
+    Should include everything to get started.
+
+# Module organization/dependency
+
+    Modules on lower lines should not import modules on same or above lines:
+
+    - __init__.py
+
+    - tru_chain.py
+
+    - tru.py
+
+    - tru_feedback.py
+
+    - tru_db.py
+
+    - provider_apis.py feedback_prompts.py
+
+    - schema.py
+
+    - util.py keys.py
 """
 
 __version__ = "0.1.1"
@@ -9,5 +31,6 @@ from trulens_eval.tru_feedback import Feedback
 from trulens_eval.tru_feedback import OpenAI
 from trulens_eval.tru_feedback import Huggingface
 from trulens_eval.tru import Tru
+from trulens_eval.tru_db import Query
 
-__all__ = ['TruChain', 'Feedback', 'OpenAI', 'Huggingface', 'Tru']
+__all__ = ['TruChain', 'Feedback', 'OpenAI', 'Huggingface', 'Tru', 'Query']
