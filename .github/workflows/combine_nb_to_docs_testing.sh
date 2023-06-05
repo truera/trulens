7a61b46cbe5d023eb4fd516796aa33711fe57fa3
Ricardo Shih <rick@ailens.io>
2023-06-05 10:43:28 -0500

    run script

--- .github/workflows/combine_nb_to_docs_testing.sh
+++ .github/workflows/combine_nb_to_docs_testing.sh
@@ -1,3 +1,5 @@
+rm -rf all_tools.ipynb
+
 # Combined notebook flow - will be tested
 nbmerge quickstart.ipynb logging.ipynb feedback_functions.ipynb >> all_tools.ipynb
 
@@ -16,7 +18,7 @@ sed -i .bak  '/JSON/d' quickstart.py all_tools.py
 ## Replace jupyter display with python print 
 sed -i .bak  's/display/print/g' quickstart.py all_tools.py
 ## Remove cell metadata
-sed -i .bak  '/\# In\[ \]\:/d' quickstart.py all_tools.py
+sed -i .bak  '/\# In\[/d' quickstart.py all_tools.py
 ## Collapse multiple empty line from sed replacements with a single line
 sed -i .bak -e '/./b' -e :n -e 'N;s/\\n$//;tn' quickstart.py all_tools.py
 
