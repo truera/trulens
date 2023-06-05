90770629fb49ada8fb0766fdf5cf9bf135bb0497
Piotr Mardziel <piotrm@gmail.com>
2023-06-05 12:51:07 -0700

    add var to js

--- trulens_eval/trulens_eval/ux/styles.py
+++ trulens_eval/trulens_eval/ux/styles.py
@@ -2,32 +2,34 @@ from trulens_eval.tru_feedback import default_pass_fail_color_threshold
 
 stmetricdelta_hidearrow = """
     <style> [data-testid="stMetricDelta"] svg { display: none; } </style>
-"""
+    """
 
-cellstyle_jscode = f"""
-    function(params) {{
-        if (parseFloat(params.value) < {default_pass_fail_color_threshold}) {{
-            return {{
+cellstyle_jscode = \
+    f"var default_pass_fail_color_threshold = {default_pass_fail_color_threshold};" \
+    """
+    function(params) {
+        if (parseFloat(params.value) < default_pass_fail_color_threshold) {
+            return {
                 'color': 'black',
                 'backgroundColor': '#FCE6E6'
-            }}
-        }} else if (parseFloat(params.value) >= {default_pass_fail_color_threshold}) {{
-            return {{
+            }
+        } else if (parseFloat(params.value) >= default_pass_fail_color_threshold) {
+            return {
                 'color': 'black',
                 'backgroundColor': '#4CAF50'
-            }}
-        }} else {{
-            return {{
+            }
+        } else {
+            return {
                 'color': 'black',
                 'backgroundColor': 'white'
-            }}
-        }}
-    }};
-        """
+            }
+        }
+    };
+    """
 
 hide_table_row_index = """
     <style>
     thead tr th:first-child {display:none}
     tbody th {display:none}
     </style>
-"""
+    """
