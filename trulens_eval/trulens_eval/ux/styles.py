b06db4bd5194589b069fed71f21637bd6822d82c
Piotr Mardziel <piotrm@gmail.com>
2023-06-05 12:48:00 -0700

    use threshol

--- trulens_eval/trulens_eval/ux/styles.py
+++ trulens_eval/trulens_eval/ux/styles.py
@@ -1,29 +1,33 @@
-stmetricdelta_hidearrow = """ <style> [data-testid="stMetricDelta"] svg { display: none; } </style> """
+from trulens_eval.tru_feedback import default_pass_fail_color_threshold
 
-cellstyle_jscode = """
-        function(params) {
-            if (parseFloat(params.value) < default_pass_fail_color_threshold) {
-                return {
-                    'color': 'black',
-                    'backgroundColor': '#FCE6E6'
-                }
-            } else if (parseFloat(params.value) >= default_pass_fail_color_threshold) {
-                return {
-                    'color': 'black',
-                    'backgroundColor': '#4CAF50'
-                }
-            } else {
-                return {
-                    'color': 'black',
-                    'backgroundColor': 'white'
-                }
-            }
-        };
+stmetricdelta_hidearrow = """
+    <style> [data-testid="stMetricDelta"] svg { display: none; } </style>
+"""
+
+cellstyle_jscode = f"""
+    function(params) {{
+        if (parseFloat(params.value) < {default_pass_fail_color_threshold}) {{
+            return {{
+                'color': 'black',
+                'backgroundColor': '#FCE6E6'
+            }}
+        }} else if (parseFloat(params.value) >= {default_pass_fail_color_threshold}) {{
+            return {{
+                'color': 'black',
+                'backgroundColor': '#4CAF50'
+            }}
+        }} else {{
+            return {{
+                'color': 'black',
+                'backgroundColor': 'white'
+            }}
+        }}
+    }};
         """
 
 hide_table_row_index = """
-                                <style>
-                                thead tr th:first-child {display:none}
-                                tbody th {display:none}
-                                </style>
-                                """
+    <style>
+    thead tr th:first-child {display:none}
+    tbody th {display:none}
+    </style>
+"""
