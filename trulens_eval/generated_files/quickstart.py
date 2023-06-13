cbf93d1ce85fecce39b9ebaf6e2dbb3fbe1603ab
piotrm0 <piotrm0@users.noreply.github.com>
2023-06-13 05:07:09 +0000

    Automated File Generation from Docs Notebook Changes

--- trulens_eval/generated_files/quickstart.py
+++ trulens_eval/generated_files/quickstart.py
@@ -17,11 +17,7 @@ os.environ["HUGGINGFACE_API_KEY"] = "..."
 # ### Import from LangChain and TruLens
 
 # Imports main tools:
-from trulens_eval import Feedback
-from trulens_eval import Huggingface
-from trulens_eval import Query
-from trulens_eval import Tru
-from trulens_eval import TruChain
+from trulens_eval import TruChain, Feedback, Huggingface, Tru, Query
 
 tru = Tru()
 
@@ -30,9 +26,8 @@ tru = Tru()
 # ! pip install langchain>=0.0.170
 from langchain.chains import LLMChain
 from langchain.llms import OpenAI
-from langchain.prompts.chat import ChatPromptTemplate
+from langchain.prompts.chat import ChatPromptTemplate, PromptTemplate
 from langchain.prompts.chat import HumanMessagePromptTemplate
-from langchain.prompts.chat import PromptTemplate
 
 # ### Create Simple LLM Application
 #
@@ -73,7 +68,7 @@ f_lang_match = Feedback(hugs.language_match).on(
 # ## Instrument chain for logging with TruLens
 
 truchain = TruChain(
-    chain, chain_id='Chain3_ChatApplication', feedbacks=[f_lang_match], tru=tru
+    chain, chain_id='Chain3_ChatApplication', feedbacks=[f_lang_match]
 )
 
 # Instrumented chain can operate like the original:
