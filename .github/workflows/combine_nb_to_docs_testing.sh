e5b733449baf5aa6745575d8d8bf16d180f83c56
Ricardo Shih <rick@ailens.io>
2023-06-20 17:00:21 -0500

    revert breaking sh files env

--- .github/workflows/combine_nb_to_docs_testing.sh
+++ .github/workflows/combine_nb_to_docs_testing.sh
@@ -18,21 +18,19 @@ jupyter nbconvert --to script all_tools.ipynb
 # gnu sed/gsed needed on mac:
 SED=`which -a gsed sed | head -n1`
 
-SCRIPTS_TO_PROCESS=quickstart.py llama_quickstart.py all_tools.py
-
 # Fix nbmerge ids field invalid for ipynb
 $SED -i "/id\"\:/d" all_tools.ipynb
 
 ## Remove ipynb JSON calls
-$SED -i "/JSON/d" $SCRIPTS_TO_PROCESS
+$SED -i "/JSON/d" quickstart.py llama_quickstart.py all_tools.py
 ## Replace jupyter display with python print
-$SED -i "s/display/print/g" $SCRIPTS_TO_PROCESS
+$SED -i "s/display/print/g" quickstart.py llama_quickstart.py all_tools.py
 ## Remove cell metadata
-$SED -i "/\# In\[/d" $SCRIPTS_TO_PROCESS
+$SED -i "/\# In\[/d" quickstart.py llama_quickstart.py all_tools.py
 ## Remove single # lines
-$SED -i "/\#$/d" $SCRIPTS_TO_PROCESS
+$SED -i "/\#$/d" quickstart.py llama_quickstart.py all_tools.py
 ## Collapse multiple empty line from sed replacements with a single line
-$SED -i -e "/./b" -e ":n" -e "N;s/\\n$//;tn" $SCRIPTS_TO_PROCESS
+$SED -i -e "/./b" -e ":n" -e "N;s/\\n$//;tn" quickstart.py llama_quickstart.py all_tools.py
 
 # Move all generated files to the generated_files folder
 mv README.md ../../trulens_eval/README.md
