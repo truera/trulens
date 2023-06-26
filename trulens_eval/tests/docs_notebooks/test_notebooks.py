0f911380e01a59260326c0741fa6bee6a73dcfe5
Ricardo Shih <rick@ailens.io>
2023-06-26 10:51:02 -0500

    make sure migrate happens in the preprocess

--- trulens_eval/tests/docs_notebooks/test_notebooks.py
+++ trulens_eval/tests/docs_notebooks/test_notebooks.py
@@ -50,9 +50,9 @@ class DBMigrationPreprocessor(VariableSettingPreprocessor):
         )
 
     def preprocess_cell(self, cell, resources, index, **kwargs):
-        ret = super().preprocess_cell(cell, resources, index, **kwargs)
         if 'Tru()' in cell["source"]:
             cell["source"] = cell["source"] + f"\ntru.migrate_database()\n"
+        ret = super().preprocess_cell(cell, resources, index, **kwargs)
 
         return ret
 
