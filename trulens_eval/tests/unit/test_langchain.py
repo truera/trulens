40be5beba6c87e0d347f48cb2975bcddfdadab9c
Piotr Mardziel <piotrm@gmail.com>
2023-06-27 16:41:52 -0700

    add test file

---
+++ trulens_eval/tests/unit/test_langchain.py
@@ -0,0 +1,14 @@
+from unittest import main
+from unittest import TestCase
+
+
+class LangChainCorrectness(TestCase):
+    # test for instrumented components
+
+    # test for cost tracking
+
+    # test for correct cost tracking
+
+
+if __name__ == '__main__':
+    main()
