8665dd0915efd497b4de0e1ccb8b6ae6906c29e6
corey hu <corey@truera.com>
2023-07-10 15:44:54 -0700

    Update setup.py

--- trulens_explain/setup.py
+++ trulens_explain/setup.py
@@ -5,7 +5,5 @@ setup(
     name="trulens",
     packages=find_namespace_packages(include=["trulens", "trulens.*"]),
     python_requires='>=3.8',
-    install_requires=[
-        'numpy>=1.23.5'
-    ]
+    install_requires=['numpy>=1.23.5']
 )
