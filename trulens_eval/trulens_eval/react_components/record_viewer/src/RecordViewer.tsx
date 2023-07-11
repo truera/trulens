07c068048bfad02e972b41ffe001659081ae7bfd
walnutdust <garett@truera.com>
2023-07-11 13:25:00 -0700

    functionality

--- trulens_eval/trulens_eval/react_components/record_viewer/src/RecordViewer.tsx
+++ trulens_eval/trulens_eval/react_components/record_viewer/src/RecordViewer.tsx
@@ -1,39 +1,36 @@
-import {
-  Streamlit,
-  StreamlitComponentBase,
-  withStreamlitConnection,
-} from "streamlit-component-lib"
-import { ReactNode } from "react"
-import "./RecordViewer.css"
-import { getStartAndEndTimesForNode, getTreeDepth } from "./treeUtils"
-import { DataRaw, StackTreeNode } from "./types"
-import { createTreeFromCalls } from "./utils"
-import { Box, Tooltip } from "@mui/material"
-import { GridLines } from "./TimelineBars"
+import { Streamlit, StreamlitComponentBase, withStreamlitConnection } from 'streamlit-component-lib';
+import { ReactNode } from 'react';
+import './RecordViewer.css';
+import { getStartAndEndTimesForNode, getTreeDepth } from './treeUtils';
+import { DataRaw, StackTreeNode } from './types';
+import { createTreeFromCalls } from './utils';
+import { Box, Tooltip } from '@mui/material';
+import { GridLines } from './TimelineBars';
 
 class RecordViewer extends StreamlitComponentBase {
   public render = (): ReactNode => {
     // This seems to currently be the best way to type args, since
     // StreamlitComponentBase appears happy to just give it "any".
-    const { record_json } = this.props.args as DataRaw
+    const { record_json } = this.props.args as DataRaw;
 
-    const { font: fontFamily } = this.props.theme as { font: string }
-    const { width } = this.props as { width: number }
+    const { font: fontFamily } = this.props.theme as { font: string };
+    const { width } = this.props as { width: number };
 
-    const tree = createTreeFromCalls(record_json)
-    const treeDepth = getTreeDepth(tree)
-    const { startTime: treeStart, timeTaken: totalTime } =
-      getStartAndEndTimesForNode(tree)
+    const tree = createTreeFromCalls(record_json);
+    const treeDepth = getTreeDepth(tree);
+    const { startTime: treeStart, timeTaken: totalTime, endTime: treeEnd } = getStartAndEndTimesForNode(tree);
 
     const renderTree = () => {
-      const children: ReactNode[] = []
+      const children: ReactNode[] = [];
 
       const recursiveRender = (node: StackTreeNode, depth: number) => {
-        const { startTime, timeTaken } = getStartAndEndTimesForNode(node)
-        const { name, methodName, path } = node
+        const { startTime, timeTaken } = getStartAndEndTimesForNode(node);
+        if (startTime >= treeEnd) return;
+
+        const { name, methodName, path } = node;
 
         const description = (
-          <Box sx={{ display: "flex", flexDirection: "column", gap: "8px" }}>
+          <Box sx={{ display: 'flex', flexDirection: 'column', gap: '8px' }}>
             <b>{name}</b>
             <span>
               <b>Time taken:</b> {timeTaken}ms
@@ -49,10 +46,10 @@ class RecordViewer extends StreamlitComponentBase {
               </span>
             )}
           </Box>
-        )
+        );
 
         children.push(
-          <Tooltip title={description} arrow placement="top">
+          <Tooltip title={description} arrow>
             <div
               className="timeline"
               style={{
@@ -62,46 +59,44 @@ class RecordViewer extends StreamlitComponentBase {
                 fontFamily,
               }}
               onClick={() => {
-                Streamlit.setComponentValue(node.raw?.perf.start_time ?? null)
+                Streamlit.setComponentValue(node.raw?.perf.start_time ?? null);
               }}
             >
               <span className="timeline-component-name">{node.name}</span>
               <span className="timeline-time-taken">{timeTaken}ms</span>
             </div>
           </Tooltip>
-        )
+        );
 
         for (const child of node.children ?? []) {
-          recursiveRender(child, depth + 1)
+          recursiveRender(child, depth + 1);
         }
-      }
+      };
 
-      recursiveRender(tree, 0)
+      recursiveRender(tree, 0);
 
       return (
         <div
           style={{
-            position: "relative",
+            position: 'relative',
             gridColumnStart: 1,
             gridRowStart: 1,
           }}
         >
           {children}
         </div>
-      )
-    }
+      );
+    };
 
-    const modifiedWidth = width - 16
+    const modifiedWidth = width - 16;
 
     return (
-      <div style={{ display: "flex", flexDirection: "column", gap: 16 }}>
-        <span style={{ fontFamily }}>
-          Total time taken: {totalTime / 1000}s
-        </span>
+      <div style={{ display: 'flex', flexDirection: 'column', gap: 16, fontFamily, boxSizing: 'border-box' }}>
+        <span className="detail">Total time taken: {totalTime / 1000}s</span>
         <div
           className="timeline-container"
           style={{
-            width,
+            width: modifiedWidth,
             gridTemplateRows: 32 * treeDepth + 16,
             gridTemplateColumns: modifiedWidth,
             height: 32 * treeDepth + 16,
@@ -111,8 +106,8 @@ class RecordViewer extends StreamlitComponentBase {
           {renderTree()}
         </div>
       </div>
-    )
-  }
+    );
+  };
 }
 
 // "withStreamlitConnection" is a wrapper function. It bootstraps the
@@ -120,5 +115,5 @@ class RecordViewer extends StreamlitComponentBase {
 // passing arguments from Python -> Component.
 //
 // You don't need to edit withStreamlitConnection (but you're welcome to!).
-const connectedRecordViewer = withStreamlitConnection(RecordViewer)
-export default connectedRecordViewer
+const connectedRecordViewer = withStreamlitConnection(RecordViewer);
+export default connectedRecordViewer;
--- trulens_eval/trulens_eval/react_components/record_viewer/src/TimelineBars.tsx
+++ trulens_eval/trulens_eval/react_components/record_viewer/src/TimelineBars.tsx
@@ -1,45 +1,34 @@
 type GridLinesProps = {
-  totalWidth: number
-  totalTime: number
-}
+  totalWidth: number;
+  totalTime: number;
+};
 
-const MIN_WIDTH = 200
+const MIN_WIDTH = 100;
 
-const SECOND = 1000
-const MINUTE = 60 * SECOND
+const SECOND = 1000;
+const MINUTE = 60 * SECOND;
 
-const TIME_OPTIONS = [
-  100,
-  500,
-  1 * SECOND,
-  5 * SECOND,
-  10 * SECOND,
-  30 * SECOND,
-  MINUTE,
-]
+const TIME_OPTIONS = [100, 500, 1 * SECOND, 5 * SECOND, 10 * SECOND, 30 * SECOND, MINUTE];
 
 export const GridLines = ({ totalWidth, totalTime }: GridLinesProps) => {
-  const maxCols = Math.floor(totalWidth / MIN_WIDTH)
+  const maxCols = Math.floor(totalWidth / MIN_WIDTH);
 
-  const timeOptionCols = TIME_OPTIONS.map((timeOption) =>
-    Math.floor(totalTime / timeOption)
-  )
+  const timeOptionCols = TIME_OPTIONS.map((timeOption) => Math.floor(totalTime / timeOption));
 
-  const timeOptionIndex = timeOptionCols.findIndex((c) => c < maxCols)
-  const timeOption = timeOptionIndex !== -1 ? TIME_OPTIONS[timeOptionIndex] : 1
-  const numCols = Math.floor(totalTime / timeOption)
-  const widthPerCol = (timeOption / totalTime) * totalWidth
-
-  console.log({ timeOptionCols, numCols })
+  const timeOptionIndex = timeOptionCols.findIndex((c) => c < maxCols);
+  const timeOption = timeOptionIndex !== -1 ? TIME_OPTIONS[timeOptionIndex] : 1;
+  const numCols = Math.floor(totalTime / timeOption);
+  const widthPerCol = (timeOption / totalTime) * totalWidth;
 
   return (
     <div
       style={{
-        display: "flex",
-        flexDirection: "row",
-        position: "relative",
+        display: 'flex',
+        flexDirection: 'row',
+        position: 'relative',
         gridColumnStart: 1,
         gridRowStart: 1,
+        overflow: 'hidden',
       }}
     >
       {Array(numCols)
@@ -49,22 +38,26 @@ export const GridLines = ({ totalWidth, totalTime }: GridLinesProps) => {
             <div
               key={i}
               style={{
-                height: "100%",
+                height: '100%',
                 minHeight: 20,
-                width: "1px",
-                backgroundColor: "#E0E0E0",
-                position: "relative",
+                width: '1px',
+                backgroundColor: '#E0E0E0',
+                position: 'absolute',
                 left: (i + 1) * widthPerCol,
               }}
             />
             <span
+              className="detail"
               style={{
-                position: "relative",
-                left: (i + 1) * widthPerCol,
+                position: 'absolute',
+                fontSize: '0.8rem',
+                left: (i + 1) * widthPerCol + 4,
               }}
-            ></span>
+            >
+              {(i + 1) * timeOption}ms
+            </span>
           </>
         ))}
     </div>
-  )
-}
+  );
+};
