1f8a600042f86dbf9120e1c2ea6a9b80d49eda14
walnutdust <garett@truera.com>
2023-07-11 13:44:05 -0700

    fix build issue

--- trulens_eval/trulens_eval/react_components/record_viewer/vite.config.ts
+++ trulens_eval/trulens_eval/react_components/record_viewer/vite.config.ts
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '',
   plugins: [react()],
-})
+});
