b9088e9f671da4cf65dfa4467c2d491433506baf
rshih32 <rick@ailens.io>
2023-07-12 13:11:29 -0500

    Basic llm instrumentation and raw input/output text support (#263)
    * boilerplate
    
    * get past app initialization
    
    * save state
    
    * add notebooks
    
    * remove debugs
    
    * format
    
    * add to testing
    
    * try linux nb access for testing
    
    * unused import
    
    * replace from quickstart
    
    * limit notebooks
    
    * start from known notebook
    
    * langchain test
    
    * name check
    
    * replace notebook contents
    
    * try another name
    
    * remove framework from test notebook name, causes some weird issue
    
    * remove old test
    
    * return example
    
    * upgrade nbformat
    
    * remove test temporarily
    
    * Revert "upgrade nbformat"
    
    This reverts commit f762b8de9331620ebefdcf1386b13ec77eab5923.
    
    ---------
    
    Co-authored-by: shayaks <47965099+shayaks@users.noreply.github.com>
    Co-authored-by: rick <rick@truera.com>

--- .github/workflows/combine_nb_to_docs_testing.sh
+++ .github/workflows/combine_nb_to_docs_testing.sh
@@ -7,6 +7,7 @@ nbmerge quickstart.ipynb logging.ipynb feedback_functions.ipynb >> all_tools.ipy
 # Colab quickstarts
 nbmerge colab_dependencies.ipynb quickstart.ipynb >> langchain_quickstart_colab.ipynb
 nbmerge colab_dependencies.ipynb llama_index_quickstart.ipynb >> llama_index_quickstart_colab.ipynb
+nbmerge colab_dependencies.ipynb no_framework_quickstart.ipynb >> no_framework_quickstart.ipynb
 
 # Create pypi page documentation
 jupyter nbconvert --to markdown all_tools.ipynb
@@ -20,30 +21,32 @@ cat gh_top_intro.md break.md trulens_eval_gh_top_readme.md break.md ../trulens_e
 # Create non-jupyter scripts
 jupyter nbconvert --to script quickstart.ipynb
 jupyter nbconvert --to script llama_index_quickstart.ipynb
+jupyter nbconvert --to script no_framework_quickstart.ipynb
 jupyter nbconvert --to script all_tools.ipynb
 
 # gnu sed/gsed needed on mac:
 SED=`which -a gsed sed | head -n1`
 
 # Fix nbmerge ids field invalid for ipynb
-$SED -i "/id\"\:/d" all_tools.ipynb langchain_quickstart_colab.ipynb llama_index_quickstart_colab.ipynb
+$SED -i "/id\"\:/d" all_tools.ipynb langchain_quickstart_colab.ipynb llama_index_quickstart_colab.ipynb no_framework_quickstart.ipynb
 
 ## Remove ipynb JSON calls
-$SED -i "/JSON/d" quickstart.py llama_index_quickstart.py all_tools.py
+$SED -i "/JSON/d" quickstart.py llama_index_quickstart.py no_framework_quickstart.py all_tools.py 
 ## Replace jupyter display with python print
-$SED -i "s/display/print/g" quickstart.py llama_index_quickstart.py all_tools.py
+$SED -i "s/display/print/g" quickstart.py llama_index_quickstart.py no_framework_quickstart.py all_tools.py
 ## Remove cell metadata
-$SED -i "/\# In\[/d" quickstart.py llama_index_quickstart.py all_tools.py
+$SED -i "/\# In\[/d" quickstart.py llama_index_quickstart.py no_framework_quickstart.py all_tools.py
 ## Remove single # lines
-$SED -i "/\#$/d" quickstart.py llama_index_quickstart.py all_tools.py
+$SED -i "/\#$/d" quickstart.py llama_index_quickstart.py no_framework_quickstart.py all_tools.py
 ## Collapse multiple empty line from sed replacements with a single line
-$SED -i -e "/./b" -e ":n" -e "N;s/\\n$//;tn" quickstart.py llama_index_quickstart.py all_tools.py
+$SED -i -e "/./b" -e ":n" -e "N;s/\\n$//;tn" quickstart.py llama_index_quickstart.py no_framework_quickstart.py all_tools.py
 
 # Move all generated files to the generated_files folder
 mv README.md ../../trulens_eval/README.md
 mv TOP_README.md ../../README.md
 
 mv llama_index_quickstart.py ../../trulens_eval/examples/llama_index_quickstart.py
+mv no_framework_quickstart.py ../../trulens_eval/examples/no_framework_quickstart.py
 mv *.py ../../trulens_eval/examples/
 mv *quickstart_colab.ipynb ../../trulens_eval/examples/colab/quickstarts/
 mv all_tools* ../../trulens_eval/generated_files/
