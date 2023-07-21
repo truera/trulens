36814d4a4a42d835f78d3bab9fb7e88438aa1940
shayaks <47965099+shayaks@users.noreply.github.com>
2023-07-21 09:21:48 -0700

    Minor feedback feedback function updates (#308)
    * Initial ground truth example
    
    * Cleanup
    
    * clear notebook outputs
    
    * adding metadata
    
    * work
    
    * undo, wrong pr
    
    * fixing up
    
    * move ground truth agreement out of openai
    
    * added callable option
    
    * Moved model_agreement back to openai class
    
    * fix up notes
    
    * langchain ground truth eval example
    
    * clear output
    
    * simplify ex
    
    * simplify example
    
    * Fix completition typo
    
    * Fixing issues with feedback functions and setting temperature to 0
    
    * Feedback docs fix
    
    ---------
    
    Co-authored-by: Shayak Sen <shayak@truera.com>
    Co-authored-by: Piotr Mardziel <piotrm@gmail.com>
    Co-authored-by: Josh Reini <josh.reini@truera.com>
    Co-authored-by: rshih32 <rick@ailens.io>

--- trulens_eval/trulens_eval/feedback.py
+++ trulens_eval/trulens_eval/feedback.py
@@ -7,9 +7,9 @@ specification and evaluation. A typical use-case looks like this:
 ```python 
 from trulens_eval import feedback, Select, Feedback
 
-openai = feedback.OpenAI()
+hugs = feedback.Huggingface()
 
-f_lang_match = Feedback(openai.language_match)
+f_lang_match = Feedback(hugs.language_match)
     .on_input_output()
 ```
 
@@ -1252,7 +1252,7 @@ class OpenAI(Provider):
             self.endpoint.run_me(
                 lambda: self._create_chat_completion(
                     model=self.model_engine,
-                    temperature=0.5,
+                    temperature=0.0,
                     messages=[
                         {
                             "role": "system",
@@ -1282,7 +1282,7 @@ class OpenAI(Provider):
             being "in agreement".
         """
         logger.warning("model_agreement has been deprecated. Use GroundTruthAgreement(ground_truth) instead.")
-        oai_chat_response = OpenAI().endpoint.run_me(
+        oai_chat_response = self.endpoint.run_me(
             lambda: self._create_chat_completion(
                 model=self.model_engine,
                 temperature=0.0,
@@ -1297,18 +1297,41 @@ class OpenAI(Provider):
                 ]
             )["choices"][0]["message"]["content"]
         )
-        agreement_txt = _get_answer_agreement(
+        agreement_txt = self._get_answer_agreement(
             prompt, response, oai_chat_response, self.model_engine
         )
         return _re_1_10_rating(agreement_txt) / 10
 
+    def _get_answer_agreement(self, prompt, response, check_response, model_engine="gpt-3.5-turbo"):
+        oai_chat_response = self.endpoint.run_me(
+            lambda: self._create_chat_completion(
+                model=model_engine,
+                temperature=0.0,
+                messages=[
+                    {
+                        "role":
+                            "system",
+                        "content":
+                            feedback_prompts.AGREEMENT_SYSTEM_PROMPT %
+                            (prompt, response)
+                    }, {
+                        "role": "user",
+                        "content": check_response
+                    }
+                ]
+            )["choices"][0]["message"]["content"]
+        )
+        return oai_chat_response
+
+
 
 class GroundTruthAgreement(SerialModel, WithClassInfo):
     ground_truth: Union[List[str], FunctionOrMethod]
+    provider: OpenAI
 
     ground_truth_imp: Optional[Callable] = pydantic.Field(exclude=True)
 
-    def __init__(self, ground_truth: Union[List[str], Callable, FunctionOrMethod]):
+    def __init__(self, ground_truth: Union[List[str], Callable, FunctionOrMethod], provider: OpenAI = OpenAI()):
         if isinstance(ground_truth, List):
             ground_truth_imp = None
         elif isinstance(ground_truth, FunctionOrMethod):
@@ -1326,6 +1349,7 @@ class GroundTruthAgreement(SerialModel, WithClassInfo):
         super().__init__(
             ground_truth=ground_truth,
             ground_truth_imp=ground_truth_imp,
+            provider=provider,
             obj=self # for WithClassInfo
         )
 
@@ -1357,7 +1381,7 @@ class GroundTruthAgreement(SerialModel, WithClassInfo):
         """
         ground_truth_response = self._find_response(prompt)
         if ground_truth_response:
-            agreement_txt = _get_answer_agreement(
+            agreement_txt = self.provider._get_answer_agreement(
                 prompt, response, ground_truth_response
             )
             ret = _re_1_10_rating(agreement_txt) / 10, dict(ground_truth_response=ground_truth_response)
@@ -1405,28 +1429,6 @@ class AzureOpenAI(OpenAI):
         return super()._create_chat_completion(
             *args, deployment_id=self.deployment_id, **kwargs
         )
-
-
-def _get_answer_agreement(prompt, response, check_response, model_engine="gpt-3.5-turbo"):
-    oai_chat_response = OpenAI().endpoint.run_me(
-        lambda: openai.ChatCompletion.create(
-            model=model_engine,
-            temperature=0.5,
-            messages=[
-                {
-                    "role":
-                        "system",
-                    "content":
-                        feedback_prompts.AGREEMENT_SYSTEM_PROMPT %
-                        (prompt, response)
-                }, {
-                    "role": "user",
-                    "content": check_response
-                }
-            ]
-        )["choices"][0]["message"]["content"]
-    )
-    return oai_chat_response
 
 
 # Cannot put these inside Huggingface since it interferes with pydantic.BaseModel.
