f75ecd722f6b9b3e49761b01bacb04e63680c98e
rshih32 <rick@ailens.io>
2023-07-26 10:09:41 -0500

    initial commit (#317)

--- trulens_eval/trulens_eval/pages/Progress.py
+++ trulens_eval/trulens_eval/pages/Progress.py
@@ -1,33 +1,10 @@
-from datetime import datetime
-import json
-from typing import Dict, List
-
-import pandas as pd
 from st_aggrid import AgGrid
-from st_aggrid.grid_options_builder import GridOptionsBuilder
-from st_aggrid.shared import GridUpdateMode
-from st_aggrid.shared import JsCode
 import streamlit as st
 from ux.add_logo import add_logo
 
-from trulens_eval import db
 from trulens_eval import Tru
-from trulens_eval.db import DB
-from trulens_eval.feedback import Feedback
-from trulens_eval.provider_apis import Endpoint
-from trulens_eval.provider_apis import HuggingfaceEndpoint
-from trulens_eval.provider_apis import OpenAIEndpoint
+from trulens_eval.provider_apis import DEFAULT_RPM
 from trulens_eval.schema import FeedbackResultStatus
-from trulens_eval.util import is_empty
-from trulens_eval.util import is_noserio
-from trulens_eval.util import TP
-
-from trulens_eval.keys import check_keys
-
-check_keys(
-    "OPENAI_API_KEY",
-    "HUGGINGFACE_API_KEY"
-)
 
 st.set_page_config(page_title="Feedback Progress", layout="wide")
 
@@ -40,10 +17,7 @@ add_logo()
 tru = Tru()
 lms = tru.db
 
-e_openai = OpenAIEndpoint()
-e_hugs = HuggingfaceEndpoint()
-
-endpoints = [e_openai, e_hugs]
+endpoints = ["OpenAI", "HuggingFace"]
 
 tab1, tab2, tab3 = st.tabs(["Progress", "Endpoints", "Feedback Functions"])
 
@@ -61,8 +35,8 @@ with tab1:
 
 with tab2:
     for e in endpoints:
-        st.header(e.name.upper())
-        st.metric("RPM", e.rpm)
+        st.header(e)
+        st.metric("RPM", DEFAULT_RPM)
 
 with tab3:
     feedbacks = lms.get_feedback_defs()
