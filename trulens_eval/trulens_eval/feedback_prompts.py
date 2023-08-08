c983e113f74a1061df2b552e45df39272714a4e2
Josh Reini <josh.reini@truera.com>
2023-08-08 14:04:34 -0400

    make criteria import more precise

--- trulens_eval/trulens_eval/feedback_prompts.py
+++ trulens_eval/trulens_eval/feedback_prompts.py
@@ -1,5 +1,5 @@
 from cohere.responses.classify import Example
-from langchain.evaluation.criteria import eval_chain
+from langchain.evaluation.criteria.eval_chain import _SUPPORTED_CRITERIA
 
 LLM_GROUNDEDNESS = """You are a INFORMATION OVERLAP classifier; providing the overlap of information between two statements.
 Respond only as a number from 1 to 10 where 1 is no information overlap and 10 is all information is overlapping.
@@ -184,7 +184,7 @@ The right answer is:
 Answer only with an integer from 1 to 10 based on how close the responses are to the right answer.
 """
 
-supported_criteria = {key: value.replace(" If so, response Y. If not, respond N.", '') if isinstance(value, str) else value for key, value in eval_chain._SUPPORTED_CRITERIA.items()}
+supported_criteria = {key: value.replace(" If so, response Y. If not, respond N.", '') if isinstance(value, str) else value for key, value in _SUPPORTED_CRITERIA.items()}
 
 LANGCHAIN_CONCISENESS_PROMPT =  f"{supported_criteria['conciseness']} Respond only as a number from 1 to 10 where 1 is the least concise and 10 is the most concise."
 LANGCHAIN_CORRECTNESS_PROMPT = f"{supported_criteria['correctness']} Respond only as a number from 1 to 10 where 1 is the least correct and 10 is the most correct."
