a0873c02845a76dbf09abc1657e9d4d14846402e
rick <rick@truera.com>
2023-08-10 22:58:38 +0000

    release setup

--- trulens_eval/trulens_eval/__init__.py
+++ trulens_eval/trulens_eval/__init__.py
@@ -37,7 +37,7 @@ otherwise you might get circular import errors.
     - `utils/python.py` `utils/text.py`
 """
 
-__version__ = "0.9.0"
+__version__ = "0.10.0a"
 
 from trulens_eval.feedback import Feedback
 from trulens_eval.feedback import Huggingface
