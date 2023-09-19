c15ad4c585d195a337aa66f98f1658c134331dc5
Ricardo Shih <rick@ailens.io>
2023-09-19 09:35:47 -0500

    change custom app example

--- trulens_eval/examples/frameworks/custom/custom_usage.py
+++ trulens_eval/examples/frameworks/custom/custom_usage.py
@@ -3,9 +3,11 @@ from examples.frameworks.custom.custom_app import CustomApp
 from trulens_eval import TruApp
 
 ca = CustomApp()
-ta = TruApp(ca, feedbacks=[], instrument_langchain=False)
+tru_recorder = TruApp(ca, feedbacks=[], instrument_langchain=False)
 
-ta.respond_to_query("What is the capital of Indonesia?")
-response, record = ta.respond_to_query_with_record(
+with tru_recorder as recording:
+    ca.respond_to_query("What is the capital of Indonesia?")
+
+response, record = tru_recorder.with_record(
     "What is the capital of Indonesia?"
 )
