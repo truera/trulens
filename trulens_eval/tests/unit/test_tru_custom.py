c80acdb74fa71beecda40459f49a6a6b9ff3446f
rick <rick@truera.com>
2023-09-21 22:06:49 +0000

    fix unit test path

--- trulens_eval/tests/unit/test_tru_custom.py
+++ trulens_eval/tests/unit/test_tru_custom.py
@@ -3,8 +3,7 @@ Tests for TruCustomApp.
 """
 
 from unittest import main
-
-from examples.frameworks.custom.custom_app import CustomApp
+from examples.expositional.end2end_apps.custom_app import CustomApp
 from tests.unit.test import JSONTestCase
 
 from trulens_eval import Tru
