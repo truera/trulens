b011f7d460ce492a41c808702752d07dd072a059
Josh Reini <60949774+joshreini1@users.noreply.github.com>
2023-10-06 12:15:33 -0400

    Using AWS Bedrock as Eval Model Provider (#393)
    * bedrock as model
    
    * bedrock provider
    
    * move bedrock notebook
    
    * update for bedrock ga
    
    * bedrock as a provider
    
    * move boto3 import to inside class
    
    * update rest of feedbacks
    
    * bedrock provider to docks
    
    * remove dup code
    
    * add overrides to requirements
    
    * remove dup code
    
    * remove dupe code
    
    * usage examples in docstrings
    
    * import fixes
    
    * add base provider to docs
    
    * remove override
    
    * split up feedbacks and providers in docs
    
    * provider api in docs

---
+++ trulens_eval/trulens_eval/feedback/provider/bedrock.py
@@ -0,0 +1,65 @@
+import logging
+import os
+
+from trulens_eval.feedback import prompts
+from trulens_eval.feedback.provider.base import LLMProvider
+from trulens_eval.feedback.provider.endpoint import BedrockEndpoint
+from trulens_eval.feedback.provider.endpoint.base import Endpoint
+from trulens_eval.utils.generated import re_1_10_rating
+
+import json
+
+logger = logging.getLogger(__name__)
+
+
+class Bedrock(LLMProvider):
+    model_id: str
+    region_name: str
+
+    def __init__(
+        self, *args, model_id="amazon.titan-tg1-large", region_name="us-east-1", **kwargs
+    ):
+        # NOTE(piotrm): pydantic adds endpoint to the signature of this
+        # constructor if we don't include it explicitly, even though we set it
+        # down below. Adding it as None here as a temporary hack.
+        """
+        A set of AWS Feedback Functions.
+
+        Parameters:
+
+        - model_id (str, optional): The specific model id. Defaults to
+          "amazon.titan-tg1-large".
+        - region_name (str, optional): The specific AWS region name. Defaults to
+          "us-east-1"
+
+        - All other args/kwargs passed to the boto3 client constructor.
+        """
+        # TODO: why was self_kwargs required here independently of kwargs?
+        self_kwargs = dict()
+        self_kwargs.update(**kwargs)
+
+        self_kwargs['model_id'] = model_id
+        self_kwargs['region_name'] = region_name
+        self_kwargs['endpoint'] = BedrockEndpoint(region_name = region_name, *args, **kwargs)
+
+        super().__init__(
+            **self_kwargs
+        )  # need to include pydantic.BaseModel.__init__
+
+    def _create_chat_completion(self, prompt, *args, **kwargs):
+
+        # NOTE(joshr): only tested with sso auth
+        import boto3
+        import json
+        bedrock = boto3.client(service_name='bedrock-runtime')
+
+        body = json.dumps({
+            "inputText": prompt})
+
+        modelId = self.model_id
+
+        response = bedrock.invoke_model(body=body, modelId=modelId)
+
+        response_body = json.loads(response.get('body').read()).get('results')[0]["outputText"]
+        # text
+        return response_body
