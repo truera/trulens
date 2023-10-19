1a8c4a0c0fb348de176fd02341a8903a06ba37d0
Josh Reini <joshreini1@gmail.com>
2023-10-19 16:15:43 -0400

    get replicate working for regular llm usage

--- trulens_eval/trulens_eval/feedback/provider/replicate.py
+++ trulens_eval/trulens_eval/feedback/provider/replicate.py
@@ -1,52 +1,67 @@
-import logging
 import os
+import requests
+import json
+from typing import Dict, Optional, Sequence
+
+import logging
 
 from trulens_eval.feedback import prompts
 from trulens_eval.feedback.provider.base import LLMProvider
 from trulens_eval.feedback.provider.endpoint import ReplicateEndpoint
 from trulens_eval.feedback.provider.endpoint.base import Endpoint
-from trulens_eval.keys import set_openai_key
 from trulens_eval.utils.generated import re_0_10_rating
 
 logger = logging.getLogger(__name__)
 
-
 class Replicate(LLMProvider):
-    """Out of the box feedback functions calling Replicate API.
-    """
     model_engine: str
-    endpoint: Endpoint
+    version: str
 
     def __init__(
-        self, *args, endpoint=None, model_engine="andreasjansson/llama-2-13b-chat-gguf:60ec5dda9ff9ee0b6f786c9d1157842e6ab3cc931139ad98fe99e08a35c5d4d4", **kwargs
+        self, 
+        *args, 
+        model_engine="mistralai/mistral-7b-instruct-v0.1",
+        version="83b6a56e7c828e667f21fd596c338fd4f0039b46bcfa18d973e8e70e455fda70",
+        **kwargs
     ):
-        # NOTE(piotrm): pydantic adds endpoint to the signature of this
-        # constructor if we don't include it explicitly, even though we set it
-        # down below. Adding it as None here as a temporary hack.
         """
-        Create an Replicate Provider with out of the box feedback functions.
+        Replicate Feedback Function Provider
 
-        **Usage:**
-        ```
-        from trulens_eval.feedback.provider.replicate import Replciate
-        replicate_provider = Replicate()
+        Parameters:
 
-        ```
-
-        Args:
-            model_engine (str): The Replicate completion model. Defaults to `andreasjansson/llama-2-13b-chat-gguf`
-            endpoint (Endpoint): Internal Usage for DB serialization
+        - model_engine: should be of the format: provider/model_name:version
         """
-        # TODO: why was self_kwargs required here independently of kwargs?
         self_kwargs = dict()
         self_kwargs.update(**kwargs)
         self_kwargs['model_engine'] = model_engine
+        self_kwargs['version'] = version
         self_kwargs['endpoint'] = ReplicateEndpoint(*args, **kwargs)
 
-        super().__init__(
-            **self_kwargs
-        )  # need to include pydantic.BaseModel.__init__
-    
-    def _create_chat_completion(self, input, *args, **kwargs):
+        super().__init__(**self_kwargs)
+
+    def _create_chat_completion(
+        self,
+        prompt: Optional[str] = None,
+        messages: Optional[Sequence[Dict]] = None,
+        **kwargs
+    ) -> str:
+
+        assert prompt is not None, "Replicate can only operate on `prompt`, not `messages`."
+
         import replicate
-        return replicate.run(self.model_engine, input={"prompt": input})
+        output = replicate.run(
+            f"{self.model_engine}:{self.version}",
+            input={"prompt": prompt,
+                   "temperature": 0.1}
+        )
+
+        # The predict method returns an iterator, we need to get the full output
+        full_output = []
+        for item in output:
+            # https://replicate.com/mistralai/mistral-7b-instruct-v0.1/versions/83b6a56e7c828e667f21fd596c338fd4f0039b46bcfa18d973e8e70e455fda70/api#output-schema
+            full_output.append(item)
+        # Join the list of strings into a single string
+        output_str = ''.join(full_output)
+        # Replace single quotes with double quotes
+        output_str = output_str.replace("'", '"')
+        return output_str 
