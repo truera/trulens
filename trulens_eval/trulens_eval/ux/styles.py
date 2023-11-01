54621cd6593f9bf772d5b85bc6c058ce93f4296c
Jisoo Lee <7435407+ejisoo@users.noreply.github.com>
2023-11-01 08:10:28 -0700

    [MLNN-1072] Add feedback direction `higher_is_better` by default (#528)
    * Save working code for feedback direction
    
    * Use supplied_name as key if any for direction lookup
    
    * Fix direction lookup for cell style
    
    * Use OpenAI moderation category score as is (lower score is better)
    
    * Update OpenAI moderation API test cases

--- trulens_eval/trulens_eval/ux/styles.py
+++ trulens_eval/trulens_eval/ux/styles.py
@@ -1,6 +1,17 @@
+import operator
+from collections import defaultdict
+from enum import Enum
+from typing import Callable, List, NamedTuple, Optional
+
 import numpy as np
 
 from trulens_eval.utils.serial import SerialModel
+
+
+class ResultCategoryType(Enum):
+    PASS = 0
+    WARNING = 1
+    FAIL = 2
 
 
 class CATEGORY:
@@ -15,20 +26,49 @@ class CATEGORY:
         threshold: float
         color: str
         icon: str
+        direction: Optional[str]
+        compare: Optional[Callable[[float, float], bool]]
 
-    PASS = Category(
-        name="pass", adjective="high", threshold=0.8, color="#aaffaa", icon="✅"
-    )
-    WARNING = Category(
-        name="warning",
-        adjective="medium",
-        threshold=0.6,
-        color="#ffffaa",
-        icon="⚠️"
-    )
-    FAIL = Category(
-        name="fail", adjective="low", threshold=0.0, color="#ffaaaa", icon="🛑"
-    )
+    class FeedbackDirection(NamedTuple):
+        name: str
+        ascending: bool
+        thresholds: List[float]
+
+    # support both directions by default
+    # TODO: make this configurable (per feedback definition & per app?)
+    directions = [
+        FeedbackDirection("HIGHER_IS_BETTER", False, [0, 0.6, 0.8]),
+        FeedbackDirection("LOWER_IS_BETTER", True, [0.2, 0.4, 1]),
+    ]
+
+    styling = {
+        "PASS": dict(color="#aaffaa", icon="✅"),
+        "WARNING": dict(color="#ffffaa", icon="⚠️"),
+        "FAIL": dict(color="#ffaaaa", icon="🛑"),
+    }
+
+    for category_name in ResultCategoryType._member_names_:
+        locals()[category_name] = defaultdict(dict)
+
+    for direction in directions:
+        a = sorted(
+            zip(["low", "medium", "high"], sorted(direction.thresholds)),
+            key=operator.itemgetter(1),
+            reverse=not direction.ascending,
+        )
+
+        for enum, (adjective, threshold) in enumerate(a):
+            category_name = ResultCategoryType(enum).name
+            locals()[category_name][direction.name] = Category(
+                name=category_name.lower(),
+                adjective=adjective,
+                threshold=threshold,
+                direction=direction.name,
+                compare=operator.ge
+                if direction.name == "HIGHER_IS_BETTER" else operator.le,
+                **styling[category_name],
+            )
+
     UNKNOWN = Category(
         name="unknown",
         adjective="unknown",
@@ -37,23 +77,28 @@ class CATEGORY:
         icon="?"
     )
 
+    # order matters here because `of_score` returns the first best category
     ALL = [PASS, WARNING, FAIL]  # not including UNKNOWN intentionally
 
     @staticmethod
-    def of_score(score: float) -> Category:
-        for cat in CATEGORY.ALL:
-            if score >= cat.threshold:
+    def of_score(score: float, higher_is_better: bool = True) -> Category:
+        direction_key = "HIGHER_IS_BETTER" if higher_is_better else "LOWER_IS_BETTER"
+
+        for cat in map(operator.itemgetter(direction_key), CATEGORY.ALL):
+            if cat.compare(score, cat.threshold):
                 return cat
 
         return CATEGORY.UNKNOWN
 
 
+default_direction = "HIGHER_IS_BETTER"
+
 # These would be useful to include in our pages but don't yet see a way to do
 # this in streamlit.
 root_js = f"""
-    var default_pass_threshold = {CATEGORY.PASS.threshold};
-    var default_warning_threshold = {CATEGORY.WARNING.threshold};
-    var default_fail_threshold = {CATEGORY.FAIL.threshold};
+    var default_pass_threshold = {CATEGORY.PASS[default_direction].threshold};
+    var default_warning_threshold = {CATEGORY.WARNING[default_direction].threshold};
+    var default_fail_threshold = {CATEGORY.FAIL[default_direction].threshold};
 """
 
 # Not presently used. Need to figure out how to include this in streamlit pages.
@@ -67,25 +112,29 @@ stmetricdelta_hidearrow = """
     <style> [data-testid="stMetricDelta"] svg { display: none; } </style>
     """
 
-cellstyle_jscode = f"""
-    function(params) {{
+valid_directions = ["HIGHER_IS_BETTER", "LOWER_IS_BETTER"]
+
+cellstyle_jscode = {
+    k:
+        f"""function(params) {{
         let v = parseFloat(params.value);
-        """ + \
-    "\n".join(f"""
-        if (v >= {cat.threshold}) {{
+        """ + "\n".join(
+            f"""
+        if (v {'>=' if k == "HIGHER_IS_BETTER" else '<='} {cat.threshold}) {{
             return {{
                 'color': 'black',
                 'backgroundColor': '{cat.color}'
             }};
         }}
-    """ for cat in CATEGORY.ALL) + f"""
+    """ for cat in map(operator.itemgetter(k), CATEGORY.ALL)
+        ) + f"""
         // i.e. not a number
         return {{
             'color': 'black',
             'backgroundColor': '{CATEGORY.UNKNOWN.color}'
         }};
-    }}
-    """
+    }}""" for k in valid_directions
+}
 
 hide_table_row_index = """
     <style>
