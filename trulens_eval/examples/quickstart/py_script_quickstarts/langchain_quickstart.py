ec908d1e5d21ae16e7fb839840954c058012ac6d
joshreini1 <joshreini1@users.noreply.github.com>
2023-11-02 01:38:09 +0000

    Automated File Generation from Docs Notebook Changes

--- trulens_eval/examples/quickstart/py_script_quickstarts/langchain_quickstart.py
+++ trulens_eval/examples/quickstart/py_script_quickstarts/langchain_quickstart.py
@@ -2,16 +2,14 @@
 # coding: utf-8
 
 # # Langchain Quickstart
-# 
+#
 # In this quickstart you will create a simple LLM Chain and learn how to log it and get feedback on an LLM response.
-# 
+#
 # [![Open In Colab](https://colab.research.google.com/assets/colab-badge.svg)](https://colab.research.google.com/github/truera/trulens/blob/main/trulens_eval/examples/quickstart/langchain_quickstart.ipynb)
 
 # In[ ]:
 
-
 # ! pip install trulens_eval==0.17.0 langchain>=0.0.263
-
 
 # ## Setup
 # ### Add API keys
@@ -19,22 +17,24 @@
 
 # In[ ]:
 
-
 import os
+
 os.environ["OPENAI_API_KEY"] = "..."
 os.environ["HUGGINGFACE_API_KEY"] = "..."
-
 
 # ### Import from LangChain and TruLens
 
 # In[ ]:
 
-
 from IPython.display import JSON
 
 # Imports main tools:
-from trulens_eval import TruChain, Feedback, Huggingface, Tru
+from trulens_eval import Feedback
+from trulens_eval import Huggingface
+from trulens_eval import Tru
+from trulens_eval import TruChain
 from trulens_eval.schema import FeedbackResult
+
 tru = Tru()
 
 # Imports from langchain to build app. You may need to install langchain first
@@ -42,16 +42,15 @@ tru = Tru()
 # ! pip install langchain>=0.0.170
 from langchain.chains import LLMChain
 from langchain.llms import OpenAI
-from langchain.prompts.chat import ChatPromptTemplate, PromptTemplate
+from langchain.prompts.chat import ChatPromptTemplate
 from langchain.prompts.chat import HumanMessagePromptTemplate
-
+from langchain.prompts.chat import PromptTemplate
 
 # ### Create Simple LLM Application
-# 
+#
 # This example uses a LangChain framework and OpenAI LLM
 
 # In[ ]:
-
 
 full_prompt = HumanMessagePromptTemplate(
     prompt=PromptTemplate(
@@ -67,27 +66,21 @@ llm = OpenAI(temperature=0.9, max_tokens=128)
 
 chain = LLMChain(llm=llm, prompt=chat_prompt_template, verbose=True)
 
-
 # ### Send your first request
 
 # In[ ]:
 
-
 prompt_input = '¿que hora es?'
 
-
 # In[ ]:
-
 
 llm_response = chain(prompt_input)
 
 display(llm_response)
 
-
 # ## Initialize Feedback Function(s)
 
 # In[ ]:
-
 
 # Initialize Huggingface-based feedback function collection class:
 hugs = Huggingface()
@@ -97,41 +90,33 @@ f_lang_match = Feedback(hugs.language_match).on_input_output()
 # By default this will check language match on the main app input and main app
 # output.
 
-
 # ## Instrument chain for logging with TruLens
 
 # In[ ]:
 
-
-tru_recorder = TruChain(chain,
-    app_id='Chain1_ChatApplication',
-    feedbacks=[f_lang_match])
-
+tru_recorder = TruChain(
+    chain, app_id='Chain1_ChatApplication', feedbacks=[f_lang_match]
+)
 
 # In[ ]:
-
 
 with tru_recorder as recording:
     llm_response = chain(prompt_input)
 
 display(llm_response)
 
-
 # ## Retrieve records and feedback
 
 # In[ ]:
 
-
 # The record of the ap invocation can be retrieved from the `recording`:
 
-rec = recording.get() # use .get if only one record
+rec = recording.get()  # use .get if only one record
 # recs = recording.records # use .records if multiple
 
 display(rec)
 
-
 # In[ ]:
-
 
 # The results of the feedback functions can be rertireved from the record. These
 # are `Future` instances (see `concurrent.futures`). You can use `as_completed`
@@ -139,51 +124,48 @@ display(rec)
 
 from concurrent.futures import as_completed
 
-for feedback_future in  as_completed(rec.feedback_results):
+for feedback_future in as_completed(rec.feedback_results):
     feedback, feedback_result = feedback_future.result()
-    
+
     feedback: Feedback
     feedbac_result: FeedbackResult
 
     display(feedback.name, feedback_result.result)
 
-
 # ## Explore in a Dashboard
 
 # In[ ]:
 
-
-tru.run_dashboard() # open a local streamlit app to explore
+tru.run_dashboard()  # open a local streamlit app to explore
 
 # tru.stop_dashboard() # stop if needed
-
 
 # Alternatively, you can run `trulens-eval` from a command line in the same folder to start the dashboard.
 
 # ### Chain Leaderboard
-# 
+#
 # Understand how your LLM application is performing at a glance. Once you've set up logging and evaluation in your application, you can view key performance statistics including cost and average feedback value across all of your LLM apps using the chain leaderboard. As you iterate new versions of your LLM application, you can compare their performance across all of the different quality metrics you've set up.
-# 
+#
 # Note: Average feedback values are returned and displayed in a range from 0 (worst) to 1 (best).
-# 
+#
 # ![Chain Leaderboard](https://www.trulens.org/assets/images/Leaderboard.png)
-# 
+#
 # To dive deeper on a particular chain, click "Select Chain".
-# 
+#
 # ### Understand chain performance with Evaluations
-#  
+#
 # To learn more about the performance of a particular chain or LLM model, we can select it to view its evaluations at the record level. LLM quality is assessed through the use of feedback functions. Feedback functions are extensible methods for determining the quality of LLM responses and can be applied to any downstream LLM task. Out of the box we provide a number of feedback functions for assessing model agreement, sentiment, relevance and more.
-# 
+#
 # The evaluations tab provides record-level metadata and feedback on the quality of your LLM application.
-# 
+#
 # ![Evaluations](https://www.trulens.org/assets/images/Leaderboard.png)
-# 
+#
 # ### Deep dive into full chain metadata
-# 
+#
 # Click on a record to dive deep into all of the details of your chain stack and underlying LLM, captured by tru_chain_recorder.
-# 
+#
 # ![Explore a Chain](https://www.trulens.org/assets/images/Chain_Explore.png)
-# 
+#
 # If you prefer the raw format, you can quickly get it using the "Display full chain json" or "Display full record json" buttons at the bottom of the page.
 
 # Note: Feedback functions evaluated in the deferred manner can be seen in the "Progress" page of the TruLens dashboard.
@@ -192,6 +174,5 @@ tru.run_dashboard() # open a local streamlit app to explore
 
 # In[ ]:
 
-
-tru.get_records_and_feedback(app_ids=[])[0] # pass an empty list of app_ids to get all
-
+tru.get_records_and_feedback(app_ids=[]
+                            )[0]  # pass an empty list of app_ids to get all
