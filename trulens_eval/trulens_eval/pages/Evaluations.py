d022c1fd67325e88c45d30939b5627040736b325
Josh Reini <joshreini1@gmail.com>
2023-11-18 16:46:12 -0500

    fix conflict

--- trulens_eval/trulens_eval/pages/Evaluations.py
+++ trulens_eval/trulens_eval/pages/Evaluations.py
@@ -327,9 +327,7 @@ else:
                                 [call[i]["args"] for i in range(len(call))]
                             )
                             df["result"] = pd.DataFrame(
-                                [
-                                    float(call[i]["ret"])
-                                    if call[i]["ret"] is not None else -1
+                                [float(call[i]["ret"]) if call[i]["ret"] is not None else -1
                                     for i in range(len(call))
                                 ]
 )
