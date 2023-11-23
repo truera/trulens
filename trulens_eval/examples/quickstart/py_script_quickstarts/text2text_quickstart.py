a87b9ba3afdecbbe2736d00eb1429e6e2193737c
shayaks <47965099+shayaks@users.noreply.github.com>
2023-11-23 12:51:59 -0800

    Releases/rc trulens eval 0.18.1 (#588)
    * Prep for release
    
    * Fix chromadb issues
    
    * Update test notebooks
    
    ---------
    
    Co-authored-by: Shayak Sen <shayak@truera.com>

--- trulens_eval/examples/quickstart/py_script_quickstarts/text2text_quickstart.py
+++ trulens_eval/examples/quickstart/py_script_quickstarts/text2text_quickstart.py
@@ -2,9 +2,9 @@
 # coding: utf-8
 
 # # Text to Text Quickstart
-#
+# 
 # In this quickstart you will create a simple text to text application and learn how to log it and get feedback.
-#
+# 
 # [![Open In Colab](https://colab.research.google.com/assets/colab-badge.svg)](https://colab.research.google.com/github/truera/trulens/blob/main/trulens_eval/examples/quickstart/text2text_quickstart.ipynb)
 
 # ## Setup
@@ -13,30 +13,30 @@
 
 # In[ ]:
 
-import os
 
+import os
 os.environ["OPENAI_API_KEY"] = "..."
+
 
 # ### Import from TruLens
 
 # In[ ]:
 
+
 from IPython.display import JSON
+
 # Create openai client
 from openai import OpenAI
-
 client = OpenAI()
 
 # Imports main tools:
-from trulens_eval import Feedback
-from trulens_eval import OpenAI as fOpenAI
-from trulens_eval import Tru
-
+from trulens_eval import Feedback, OpenAI as fOpenAI, Tru
 tru = Tru()
 tru.reset_database()
 
+
 # ### Create Simple Text to Text Application
-#
+# 
 # This example uses a bare bones OpenAI LLM, and a non-LLM just for demonstration purposes.
 
 # In[ ]:
@@ -44,17 +44,10 @@ tru.reset_database()
 
 def llm_standalone(prompt):
     return client.chat.completions.create(
-        model="gpt-3.5-turbo",
-        messages=[
-            {
-                "role":
-                    "system",
-                "content":
-                    "You are a question and answer bot, and you answer super upbeat."
-            }, {
-                "role": "user",
-                "content": prompt
-            }
+    model="gpt-3.5-turbo",
+    messages=[
+            {"role": "system", "content": "You are a question and answer bot, and you answer super upbeat."},
+            {"role": "user", "content": prompt}
         ]
     ).choices[0].message.content
 
@@ -63,13 +56,16 @@ def llm_standalone(prompt):
 
 # In[ ]:
 
-prompt_input = "How good is language AI?"
+
+prompt_input="How good is language AI?"
 prompt_output = llm_standalone(prompt_input)
 prompt_output
+
 
 # ## Initialize Feedback Function(s)
 
 # In[ ]:
+
 
 # Initialize OpenAI-based feedback function collection class:
 fopenai = fOpenAI()
@@ -77,28 +73,32 @@ fopenai = fOpenAI()
 # Define a relevance function from openai
 f_relevance = Feedback(fopenai.relevance).on_input_output()
 
+
 # ## Instrument the callable for logging with TruLens
 
 # In[ ]:
 
-from trulens_eval import TruBasicApp
 
-tru_llm_standalone_recorder = TruBasicApp(
-    llm_standalone, app_id="Happy Bot", feedbacks=[f_relevance]
-)
+from trulens_eval import TruBasicApp
+tru_llm_standalone_recorder = TruBasicApp(llm_standalone, app_id="Happy Bot", feedbacks=[f_relevance])
+
 
 # In[ ]:
 
+
 with tru_llm_standalone_recorder as recording:
     tru_llm_standalone_recorder.app(prompt_input)
+
 
 # ## Explore in a Dashboard
 
 # In[ ]:
 
-tru.run_dashboard()  # open a local streamlit app to explore
+
+tru.run_dashboard() # open a local streamlit app to explore
 
 # tru.stop_dashboard() # stop if needed
+
 
 # Alternatively, you can run `trulens-eval` from a command line in the same folder to start the dashboard.
 
@@ -106,5 +106,6 @@ tru.run_dashboard()  # open a local streamlit app to explore
 
 # In[ ]:
 
-tru.get_records_and_feedback(app_ids=[]
-                            )[0]  # pass an empty list of app_ids to get all
+
+tru.get_records_and_feedback(app_ids=[])[0] # pass an empty list of app_ids to get all
+
