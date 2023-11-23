3d2628a548b9a2563afc36dff9c9a01914ca7088
shayaks <shayaks@users.noreply.github.com>
2023-11-23 19:14:51 +0000

    Automated File Generation from Docs Notebook Changes

--- trulens_eval/examples/quickstart/py_script_quickstarts/all_tools.py
+++ trulens_eval/examples/quickstart/py_script_quickstarts/all_tools.py
@@ -162,7 +162,7 @@ tru.get_records_and_feedback(app_ids=[]
 
 # In[ ]:
 
-# pip install trulens-eval==0.18.0 llama_index>=0.8.69 html2text>=2020.1.16
+# pip install trulens-eval==0.18.1 llama_index>=0.8.69 html2text>=2020.1.16
 
 # ### Add API keys
 # For this quickstart, you will need Open AI and Huggingface keys. The OpenAI key is used for embeddings and GPT, and the Huggingface key is used for evaluation.
