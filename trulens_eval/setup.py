7280f00d4509bd7a3a4a8e128ef3f68e2b60433a
Piotr Mardziel <piotrm@gmail.com>
2023-11-29 16:37:30 -0800

    cleanup setup

--- trulens_eval/setup.py
+++ trulens_eval/setup.py
@@ -38,44 +38,49 @@ setup(
         ],
     },
     install_requires=[
-        'cohere>=4.4.1',
-        'datasets>=2.12.0',
-        'python-dotenv>=1.0.0',
-        'kaggle>=1.5.13',
-        f'langchain>={langchain_version}',  # required for cost tracking even outside of langchain
-        'typing-inspect>=0.8.0',  # langchain with python < 3.9 fix
-        'typing_extensions>=4.5.0',  # langchain with python < 3.9 fix
-
-        f'llama_index>={llama_version}',
-        # VectorStoreIndex changes need to be taken account for in later versions
-        'html2text>=2020.1.16', # for llama_index example using SimpleWebPageReader
-
-        'merkle-json>=1.0.0',
-        'millify>=0.1.1',
-
-        'openai >=1.1.1, <2',
-        # NOTE(piotrm): openai v2 likely to break things
-        'httpx >= 0.23.3', # for openai client serialization
-
-        'pinecone-client>=2.2.1',
-        'pydantic >=1.10.7, <2',
-        # TODO(piotrm): need some migration for pydantic 2
-        'humanize>=4.6.0',
-        'slack-bolt>=1.18.0',
-        'slack-sdk>=3.21.3',
-        'streamlit>=1.27.0',
-        'streamlit-aggrid>=0.3.4.post3',
-        'streamlit-extras>=0.2.7',
-        'streamlit-javascript>=0.1.5',  # for copy to clipboard functionality (in progress)
-        'transformers>=4.10.0',
-        'frozendict>=2.3.8',
-        'munch>=3.0.0',
-        'ipywidgets>=8.0.6',
-        'numpy>=1.23.5',
-        'sqlalchemy>=2.0.19',
-        'alembic>=1.11.2',
-
+        'numpy              >= 1.23.5',
+        'merkle-json        >= 1.0.0',
+        'frozendict         >= 2.3.8',
+        'munch              >= 3.0.0',
+        'python-dotenv      >= 1.0.0',
+        'pydantic           >=2, <3',
         # 'nest_asyncio>=1.5.6',
         # NOTE(piotrm): disabling for now, need more investigation of compatibility issues
+
+        f'langchain         >= {langchain_version}',  # required for cost tracking even outside of langchain
+        'typing-inspect     >= 0.8.0',  # fixes bug with langchain on python < 3.9 
+        'typing_extensions  >= 4.5.0',  # fixes big with langchain on python < 3.9
+
+        f'llama_index       >= {llama_version}',
+        # VectorStoreIndex changes need to be taken account for in later versions
+        'html2text          >= 2020.1.16', # for llama_index example using SimpleWebPageReader
+
+        # Models
+        'transformers       >= 4.10.0',
+        'cohere             >= 4.4.1',
+        'openai             >= 1.1.1, <2',
+        'httpx              >= 0.23.3', # for openai client serialization
+
+        # Vector DBs
+        'pinecone-client    >= 2.2.1',
+
+        # UI
+        'millify            >= 0.1.1',
+        'humanize           >= 4.6.0',
+        'slack-bolt         >= 1.18.0',
+        'slack-sdk          >= 3.21.3',
+        'streamlit          >= 1.27.0',
+        'streamlit-aggrid   >= 0.3.4.post3',
+        'streamlit-extras   >= 0.2.7',
+        'streamlit-javascript   >= 0.1.5',  # for copy to clipboard functionality (in progress)
+        'ipywidgets         >= 8.0.6',
+
+        # DB and migration
+        'sqlalchemy         >= 2.0.19',
+        'alembic            >= 1.11.2',
+
+        # Datasets
+        'datasets           >= 2.12.0',
+        'kaggle             >= 1.5.13',
     ],
 )
