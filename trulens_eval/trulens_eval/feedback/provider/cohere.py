f42a45c94f13510ec0ce8e6b645e30ba5b2a0788
Piotr Mardziel <piotrm@gmail.com>
2023-12-11 13:21:20 -0800

    required packages and imports cleanup (#663)
    * cleaned up
    
    * typo
    
    * lower bounds
    
    * typo
    
    * nits
    
    * nits
    
    * adjust pipelines
    
    * move chroma db req to optional packs list
    
    * revert displayName first
    
    ---------
    
    Co-authored-by: Josh Reini <60949774+joshreini1@users.noreply.github.com>

--- trulens_eval/trulens_eval/feedback/provider/cohere.py
+++ trulens_eval/trulens_eval/feedback/provider/cohere.py
@@ -1,7 +1,23 @@
 from trulens_eval.feedback import prompts
 from trulens_eval.feedback.provider.base import Provider
 from trulens_eval.feedback.provider.endpoint.base import Endpoint
-from trulens_eval.keys import get_cohere_agent
+from trulens_eval.utils.imports import REQUIREMENT_COHERE, OptionalImports
+
+with OptionalImports(message=REQUIREMENT_COHERE):
+    import cohere
+    from cohere import Client
+
+def get_cohere_agent() -> Client:
+    """
+    Gete a singleton cohere agent. Sets its api key from env var COHERE_API_KEY.
+    """
+
+    global cohere_agent
+    if cohere_agent is None:
+        cohere.api_key = os.environ['CO_API_KEY']
+        cohere_agent = Client(cohere.api_key)
+
+    return cohere_agent
 
 
 class Cohere(Provider):
