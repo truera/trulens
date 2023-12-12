a67cd14db25e424d4c3bc308ef5bd15719884df9
Nacho Villaluenga <32406287+Nvillaluenga@users.noreply.github.com>
2023-12-12 20:45:07 -0300

    basic langchain provider implementation (#664)
    * basic langchain provider implementation
    
    * langchain provider, minor refactor
    
    * update docstring
    
    ---------
    
    Co-authored-by: Josh Reini <60949774+joshreini1@users.noreply.github.com>

--- trulens_eval/trulens_eval/feedback/__init__.py
+++ trulens_eval/trulens_eval/feedback/__init__.py
@@ -12,19 +12,31 @@ AggCallable = Callable[[Iterable[float]], float]
 
 # Specific feedback functions:
 from trulens_eval.feedback.embeddings import Embeddings
+
 # Main class holding and running feedback functions:
 from trulens_eval.feedback.feedback import Feedback
 from trulens_eval.feedback.groundedness import Groundedness
 from trulens_eval.feedback.groundtruth import GroundTruthAgreement
 from trulens_eval.feedback.provider.bedrock import Bedrock
 from trulens_eval.feedback.provider.cohere import Cohere
+
 # Providers of feedback functions evaluation:
 from trulens_eval.feedback.provider.hugs import Huggingface
+from trulens_eval.feedback.provider.langchain import Langchain
 from trulens_eval.feedback.provider.litellm import LiteLLM
 from trulens_eval.feedback.provider.openai import AzureOpenAI
 from trulens_eval.feedback.provider.openai import OpenAI
 
 __all__ = [
-    'Feedback', 'Embeddings', 'Groundedness', 'GroundTruthAgreement', 'OpenAI',
-    'AzureOpenAI', 'Huggingface', 'Cohere', 'LiteLLM', 'Bedrock'
+    "Feedback",
+    "Embeddings",
+    "Groundedness",
+    "GroundTruthAgreement",
+    "OpenAI",
+    "AzureOpenAI",
+    "Huggingface",
+    "Cohere",
+    "LiteLLM",
+    "Bedrock",
+    "Langchain",
 ]
--- trulens_eval/trulens_eval/feedback/provider/__init__.py
+++ trulens_eval/trulens_eval/feedback/provider/__init__.py
@@ -1,7 +1,15 @@
 from trulens_eval.feedback.provider.base import Provider
 from trulens_eval.feedback.provider.bedrock import Bedrock
 from trulens_eval.feedback.provider.hugs import Huggingface
+from trulens_eval.feedback.provider.langchain import Langchain
 from trulens_eval.feedback.provider.litellm import LiteLLM
 from trulens_eval.feedback.provider.openai import OpenAI
 
-__all__ = ['Provider', 'OpenAI', 'Huggingface', 'LiteLLM', 'Bedrock']
+__all__ = [
+    "Provider",
+    "OpenAI",
+    "Huggingface",
+    "LiteLLM",
+    "Bedrock",
+    "Langchain",
+]
