d60a163329c864f7d317996848873b0f775f9b3e
Eric Pinzur <epinzur@gmail.com>
2023-12-19 10:12:13 -0600

    exposed AzureOpenAI provider

--- trulens_eval/trulens_eval/feedback/provider/__init__.py
+++ trulens_eval/trulens_eval/feedback/provider/__init__.py
@@ -3,11 +3,12 @@ from trulens_eval.feedback.provider.bedrock import Bedrock
 from trulens_eval.feedback.provider.hugs import Huggingface
 from trulens_eval.feedback.provider.langchain import Langchain
 from trulens_eval.feedback.provider.litellm import LiteLLM
-from trulens_eval.feedback.provider.openai import OpenAI
+from trulens_eval.feedback.provider.openai import OpenAI, AzureOpenAI
 
 __all__ = [
     "Provider",
     "OpenAI",
+    "AzureOpenAI",
     "Huggingface",
     "LiteLLM",
     "Bedrock",
