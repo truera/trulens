2544226f840f3702972cc441d1a1a4342089ee01
Piotr Mardziel <piotrm@gmail.com>
2024-01-03 15:49:19 -0800

    add instructions for installing from github (#740)
    * add instructions and text wrapping
    
    * format
    
    * clean up github scripts and update README sources
    
    * typo
    
    ---------
    
    Co-authored-by: Josh Reini <60949774+joshreini1@users.noreply.github.com>

--- trulens_eval/examples/quickstart/py_script_quickstarts/text2text_quickstart.py
+++ trulens_eval/examples/quickstart/py_script_quickstarts/text2text_quickstart.py
@@ -11,25 +11,12 @@
 # ### Add API keys
 # For this quickstart you will need an OpenAI Key.
 
-# In[ ]:
-
-
 # ! pip install trulens_eval==0.20.0 openai==1.3.1
-
-
-# In[ ]:
-
 
 import os
 os.environ["OPENAI_API_KEY"] = "..."
 
-
 # ### Import from TruLens
-
-# In[ ]:
-
-
-from IPython.display import JSON
 
 # Create openai client
 from openai import OpenAI
@@ -40,13 +27,9 @@ from trulens_eval import Feedback, OpenAI as fOpenAI, Tru
 tru = Tru()
 tru.reset_database()
 
-
 # ### Create Simple Text to Text Application
 # 
 # This example uses a bare bones OpenAI LLM, and a non-LLM just for demonstration purposes.
-
-# In[ ]:
-
 
 def llm_standalone(prompt):
     return client.chat.completions.create(
@@ -57,21 +40,13 @@ def llm_standalone(prompt):
         ]
     ).choices[0].message.content
 
-
 # ### Send your first request
-
-# In[ ]:
-
 
 prompt_input="How good is language AI?"
 prompt_output = llm_standalone(prompt_input)
 prompt_output
 
-
 # ## Initialize Feedback Function(s)
-
-# In[ ]:
-
 
 # Initialize OpenAI-based feedback function collection class:
 fopenai = fOpenAI()
@@ -79,39 +54,23 @@ fopenai = fOpenAI()
 # Define a relevance function from openai
 f_relevance = Feedback(fopenai.relevance).on_input_output()
 
-
 # ## Instrument the callable for logging with TruLens
-
-# In[ ]:
-
 
 from trulens_eval import TruBasicApp
 tru_llm_standalone_recorder = TruBasicApp(llm_standalone, app_id="Happy Bot", feedbacks=[f_relevance])
 
-
-# In[ ]:
-
-
 with tru_llm_standalone_recorder as recording:
     tru_llm_standalone_recorder.app(prompt_input)
 
-
 # ## Explore in a Dashboard
-
-# In[ ]:
-
 
 tru.run_dashboard() # open a local streamlit app to explore
 
 # tru.stop_dashboard() # stop if needed
 
-
 # Alternatively, you can run `trulens-eval` from a command line in the same folder to start the dashboard.
 
 # ## Or view results directly in your notebook
-
-# In[ ]:
-
 
 tru.get_records_and_feedback(app_ids=[])[0] # pass an empty list of app_ids to get all
 
