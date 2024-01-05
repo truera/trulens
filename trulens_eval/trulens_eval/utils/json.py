4f5c5ca4e31745e1f6a385f8cbbbc09980ea4596
Piotr Mardziel <piotrm@gmail.com>
2024-01-05 13:22:15 -0800

    dont include excluded in SerialModel objects

--- trulens_eval/trulens_eval/utils/json.py
+++ trulens_eval/trulens_eval/utils/json.py
@@ -26,7 +26,7 @@ from trulens_eval.utils.pyschema import noserio
 from trulens_eval.utils.pyschema import safe_getattr
 from trulens_eval.utils.pyschema import WithClassInfo
 from trulens_eval.utils.python import safe_hasattr
-from trulens_eval.utils.serial import JSON
+from trulens_eval.utils.serial import JSON, SerialModel
 from trulens_eval.utils.serial import JSON_BASES
 from trulens_eval.utils.serial import Lens
 from trulens_eval.utils.serial import SerialBytes
@@ -145,16 +145,24 @@ def jsonify(
 
         JSON | Sequence[JSON]
     """
+    skip_excluded = not include_excluded
+    # Hack so that our models do not get exludes dumped which causes many
+    # problems. Another variable set here so we can recurse with the original
+    # include_excluded .
+    if isinstance(obj, SerialModel):
+        skip_excluded = True
 
     from trulens_eval.instruments import Instrument
 
-    instrument = instrument or Instrument()
+    if instrument is None:
+        instrument = Instrument()
+
     dicted = dicted or dict()
 
     if skip_specials:
-        recur_key = lambda k: k not in ALL_SPECIAL_KEYS
+        recur_key = lambda k: isinstance(k, JSON_BASES) and k not in ALL_SPECIAL_KEYS
     else:
-        recur_key = lambda k: True
+        recur_key = lambda k: isinstance(k, JSON_BASES) and True
 
     if id(obj) in dicted:
         if skip_specials:
@@ -179,14 +187,15 @@ def jsonify(
         return pydantic.v1.json.ENCODERS_BY_TYPE[type(obj)](obj)
 
     # TODO: should we include duplicates? If so, dicted needs to be adjusted.
-    new_dicted = {k: v for k, v in dicted.items()}
+    new_dicted = dict(dicted)
 
     recur = lambda o: jsonify(
         obj=o,
         dicted=new_dicted,
         instrument=instrument,
         skip_specials=skip_specials,
-        redact_keys=redact_keys
+        redact_keys=redact_keys,
+        include_excluded=include_excluded
     )
 
     content = None
@@ -233,7 +242,7 @@ def jsonify(
             {
                 k: recur(safe_getattr(obj, k))
                 for k, v in obj.__fields__.items()
-                if (include_excluded or not v.field_info.exclude) and recur_key(k)
+                if (not skip_excluded or not v.field_info.exclude) and recur_key(k)
             }
         )
 
@@ -256,7 +265,7 @@ def jsonify(
             {
                 k: recur(safe_getattr(obj, k))
                 for k, v in obj.model_fields.items()
-                if (include_excluded or not v.exclude) and recur_key(k)
+                if (not skip_excluded or not v.exclude) and recur_key(k)
             }
         )
 
