8bb7307144b6ad67131c033dc97a0851f3da45e2
Piotr Mardziel <piotrm@gmail.com>
2024-01-23 06:36:11 -0800

    error on deprecated passthrough methods (#803)
    * testing dedups in endpoint base
    
    * notes and update llama_index
    
    * deduping base endpoint asyncs
    
    * avoid weird "super" error in CI test
    
    * more notes and type aliases
    
    * run desync in seperate threads
    
    * typ
    
    * more hack notes
    
    * fixing bug with async langchain
    
    * remove debug print
    
    * notes and remove uneeded
    
    * throwing errors instead of printing dep message for pass through methods
    
    * should have been committed
    
    ---------
    
    Co-authored-by: Josh Reini <60949774+joshreini1@users.noreply.github.com>

--- trulens_eval/trulens_eval/tru_chain.py
+++ trulens_eval/trulens_eval/tru_chain.py
@@ -193,11 +193,9 @@ class TruChain(App):
     app: Any  # Chain
 
     # TODO: what if _acall is being used instead?
-    root_callable: ClassVar[Any] = Field(
+    root_callable: ClassVar[FunctionOrMethod] = Field(
         default_factory=lambda: FunctionOrMethod.of_callable(TruChain._call)
     )
-
-    # FunctionOrMethod
 
     # Normally pydantic does not like positional args but chain here is
     # important enough to make an exception.
@@ -258,29 +256,6 @@ class TruChain(App):
             Select.RecordCalls + retrievers[0][0]
         ).get_relevant_documents.rets
 
-    # TODEP
-    # Chain requirement
-    @property
-    def _chain_type(self):
-        return "TruChain"
-
-    # TODEP
-    # Chain requirement
-    @property
-    def input_keys(self) -> List[str]:
-        if safe_hasattr(self.app, "input_keys"):
-            return self.app.input_keys
-        else:
-            raise TypeError("App does not have input_keys.")
-
-    # TODEP
-    # Chain requirement
-    @property
-    def output_keys(self) -> List[str]:
-        if safe_hasattr(self.app, "output_keys"):
-            return self.app.output_keys
-        else:
-            raise TypeError("App does not have output_keys.")
 
     def main_input(
         self, func: Callable, sig: Signature, bindings: BoundArguments
@@ -354,72 +329,57 @@ class TruChain(App):
             return RuntimeError(
                 f"TruChain has no attribute {__name} but the wrapped app ({type(self.app)}) does. ",
                 f"If you are calling a {type(self.app)} method, retrieve it from that app instead of from `TruChain`. "
-                f"TruChain presently only wraps Chain.__call__, Chain._call, and Chain._acall ."
             )
         else:
             raise RuntimeError(f"TruChain has no attribute named {__name}.")
 
     # NOTE: Input signature compatible with langchain.chains.base.Chain.acall
-    # TODEP
-    async def acall_with_record(self, *args, **kwargs) -> Tuple[Any, Record]:
+    # TOREMOVE
+    async def acall_with_record(self, *args, **kwargs) -> None:
         """
-        Run the chain acall method and also return a record metadata object.
+        DEPRECATED: Run the chain acall method and also return a record metadata object.
         """
 
-        self._with_dep_message(method="acall", is_async=True, with_record=True)
-
-        return await self.awith_record(self.app.acall, *args, **kwargs)
+        self._throw_dep_message(method="acall", is_async=True, with_record=True)
 
     # NOTE: Input signature compatible with langchain.chains.base.Chain.__call__
-    # TODEP
-    def call_with_record(self, *args, **kwargs) -> Tuple[Any, Record]:
+    # TOREMOVE
+    def call_with_record(self, *args, **kwargs) -> None:
         """
-        Run the chain call method and also return a record metadata object.
+        DEPRECATED: Run the chain call method and also return a record metadata object.
         """
 
-        self._with_dep_message(
+        self._throw_dep_message(
             method="__call__", is_async=False, with_record=True
         )
 
-        return self.with_record(self.app.__call__, *args, **kwargs)
-
-    # TODEP
+    # TOREMOVE
     # Mimics Chain
-    def __call__(self, *args, **kwargs) -> Dict[str, Any]:
+    def __call__(self, *args, **kwargs) -> None:
         """
         Wrapped call to self.app._call with instrumentation. If you need to
         get the record, use `call_with_record` instead. 
         """
-
-        self._with_dep_message(
+        self._throw_dep_message(
             method="__call__", is_async=False, with_record=False
         )
 
-        return self.with_(self.app, *args, **kwargs)
-
-    # TODEP
+    # TOREMOVE
     # Chain requirement
-    def _call(self, *args, **kwargs) -> Any:
+    def _call(self, *args, **kwargs) -> None:
 
-        self._with_dep_message(
+        self._throw_dep_message(
             method="_call", is_async=False, with_record=False
         )
 
-        ret, _ = self.with_(self.app._call, *args, **kwargs)
-
-        return ret
-
-    # TODEP
+    # TOREMOVE
     # Optional Chain requirement
-    async def _acall(self, *args, **kwargs) -> Any:
+    async def _acall(self, *args, **kwargs) -> None:
 
-        self._with_dep_message(
+        self._throw_dep_message(
             method="_acall", is_async=True, with_record=False
         )
 
-        ret, _ = await self.awith_(self.app.acall, *args, **kwargs)
-
-        return ret
 
 
 TruChain.model_rebuild()
