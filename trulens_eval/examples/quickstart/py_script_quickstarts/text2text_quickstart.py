aa875e8ab04ac7f7c6a46caae29793f04b0920b2
Josh Reini <joshreini1@gmail.com>
2024-02-16 15:41:38 -0500

    bump pyscripts

--- trulens_eval/examples/quickstart/py_script_quickstarts/text2text_quickstart.py
+++ trulens_eval/examples/quickstart/py_script_quickstarts/text2text_quickstart.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# # Text to Text Quickstart
+# # 📓 Text to Text Quickstart
 # 
 # In this quickstart you will create a simple text to text application and learn how to log it and get feedback.
 # 
@@ -11,11 +11,12 @@
 # ### Add API keys
 # For this quickstart you will need an OpenAI Key.
 
-# In[ ]:
 
-# ! pip install trulens_eval==0.22.2 openai==1.3.1
 
-# In[ ]:
+# ! pip install trulens_eval==0.23.0 openai==1.3.1
+
+
+
 
 import os
 os.environ["OPENAI_API_KEY"] = "sk-..."
@@ -23,7 +24,7 @@ os.environ["OPENAI_API_KEY"] = "sk-..."
 
 # ### Import from TruLens
 
-# In[ ]:
+
 
 # Create openai client
 from openai import OpenAI
@@ -39,7 +40,6 @@ tru.reset_database()
 # 
 # This example uses a bare bones OpenAI LLM, and a non-LLM just for demonstration purposes.
 
-# In[ ]:
 
 
 def llm_standalone(prompt):
@@ -51,9 +51,10 @@ def llm_standalone(prompt):
         ]
     ).choices[0].message.content
 
+
 # ### Send your first request
 
-# In[ ]:
+
 
 prompt_input="How good is language AI?"
 prompt_output = llm_standalone(prompt_input)
@@ -62,7 +63,7 @@ prompt_output
 
 # ## Initialize Feedback Function(s)
 
-# In[ ]:
+
 
 # Initialize OpenAI-based feedback function collection class:
 fopenai = fOpenAI()
@@ -73,12 +74,13 @@ f_relevance = Feedback(fopenai.relevance).on_input_output()
 
 # ## Instrument the callable for logging with TruLens
 
-# In[ ]:
+
 
 from trulens_eval import TruBasicApp
 tru_llm_standalone_recorder = TruBasicApp(llm_standalone, app_id="Happy Bot", feedbacks=[f_relevance])
 
-# In[ ]:
+
+
 
 with tru_llm_standalone_recorder as recording:
     tru_llm_standalone_recorder.app(prompt_input)
@@ -86,7 +88,7 @@ with tru_llm_standalone_recorder as recording:
 
 # ## Explore in a Dashboard
 
-# In[ ]:
+
 
 tru.run_dashboard() # open a local streamlit app to explore
 
@@ -97,6 +99,7 @@ tru.run_dashboard() # open a local streamlit app to explore
 
 # ## Or view results directly in your notebook
 
-# In[ ]:
+
 
 tru.get_records_and_feedback(app_ids=[])[0] # pass an empty list of app_ids to get all
+
