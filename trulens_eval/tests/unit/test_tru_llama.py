c54efd4ae67f20e695c64c3e18e82b8b9deb0ebf
Josh Reini <joshreini1@gmail.com>
2024-02-20 14:59:49 -0500

    change data load to dir reader in test

--- trulens_eval/tests/unit/test_tru_llama.py
+++ trulens_eval/tests/unit/test_tru_llama.py
@@ -24,24 +24,13 @@ class TestLlamaIndex(JSONTestCase):
     def setUp(self):
         check_keys("OPENAI_API_KEY", "HUGGINGFACE_API_KEY")
 
-        from llama_index import ServiceContext
-        from llama_index import set_global_service_context
-        from llama_index import VectorStoreIndex
-        from llama_index.llms import OpenAI
-        from llama_index.readers.web import SimpleWebPageReader
+        from llama_index.core import VectorStoreIndex, SimpleDirectoryReader
 
-        # NOTE: Need temp = 0 for consistent tests. Some tests are still
-        # non-deterministic despite this temperature, perhaps there is some
-        # other temperature setting or this one is not taken up.
-        llm = OpenAI(temperature=0.0)
-        service_context = ServiceContext.from_defaults(llm=llm)
-        set_global_service_context(service_context)
+        import os
+        os.system('wget https://raw.githubusercontent.com/run-llama/llama_index/main/docs/examples/data/paul_graham/paul_graham_essay.txt -P data/')
 
-        # llama_index 0.8.15 bug: need to provide metadata_fn
-        self.documents = SimpleWebPageReader(
-            html_to_text=True, metadata_fn=lambda url: dict(url=url)
-        ).load_data(["http://paulgraham.com/worked.html"])
-        self.index = VectorStoreIndex.from_documents(self.documents)
+        documents = SimpleDirectoryReader("data").load_data()
+        self.index = VectorStoreIndex.from_documents(documents)
 
     def test_query_engine_async(self):
         # Check that the instrumented async aquery method produces the same result as the query method.
