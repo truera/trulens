d0ad99d4656ce7c9f884f17fca78597f48d20fbc
Piotr Mardziel <piotrm@gmail.com>
2024-02-20 16:37:44 -0800

    cost tracking tests and litellm cost tracking (#910)
    * adding cost tracking tests and cost tracking with litellm
    
    * move endpoint cost tests outside of unit tests folder
    
    * adjust docs parameters
    
    * finishing bedrock issues and moved around some tests

--- trulens_eval/trulens_eval/feedback/provider/endpoint/litellm.py
+++ trulens_eval/trulens_eval/feedback/provider/endpoint/litellm.py
@@ -1,7 +1,9 @@
 import inspect
 import logging
 import pprint
-from typing import Any, Callable, ClassVar, Dict, Optional
+from typing import Any, Callable, ClassVar, Optional
+
+import pydantic
 
 from trulens_eval.feedback.provider.endpoint.base import Endpoint
 from trulens_eval.feedback.provider.endpoint.base import EndpointCallback
@@ -23,19 +25,78 @@ class LiteLLMCallback(EndpointCallback):
 
     model_config: ClassVar[dict] = dict(arbitrary_types_allowed=True)
 
-    def handle_classification(self, response: Dict) -> None:
+    def handle_classification(self, response: pydantic.BaseModel) -> None:
         super().handle_classification(response)
 
-    def handle_generation(self, response: Any) -> None:
-        super().handle_generation(response)
+    def handle_generation(self, response: pydantic.BaseModel) -> None:
+        """Get the usage information from litellm response's usage field."""
+        
+        response = response.model_dump()
+
+        usage = response['usage']
+
+        if self.endpoint.litellm_provider not in ["openai", "azure", "bedrock"]:
+            # We are already tracking costs from the openai or bedrock endpoint so we
+            # should not double count here.
+
+            # Incremente number of requests.
+            super().handle_generation(response)
+
+            # Assume a response that had usage field was successful. Otherwise
+            # litellm does not provide success counts unlike openai.
+            self.cost.n_successful_requests += 1
+
+            for cost_field, litellm_field in [
+                ("n_tokens", "total_tokens"),
+                ("n_prompt_tokens", "prompt_tokens"),
+                ("n_completion_tokens", "completion_tokens"),
+            ]:
+                setattr(
+                    self.cost, cost_field,
+                    usage.get(litellm_field, 0)
+                )
+
+        if self.endpoint.litellm_provider not in ["openai"]:
+            # The total cost does not seem to be properly tracked except by
+            # openai so we can use litellm costs for this.
+
+            from litellm import completion_cost
+            setattr(self.cost, "cost", completion_cost(response))
 
 
 class LiteLLMEndpoint(Endpoint):
-    """
-    LiteLLM endpoint. Instruments "completion" methods in litellm.* classes.
+    """LiteLLM endpoint."""
+
+    litellm_provider: str = "openai"
+    """The litellm provider being used.
+    
+    This is checked to determine whether cost tracking should come from litellm
+    or from another endpoint which we already have cost tracking for. Otherwise
+    there will be double counting.
     """
 
-    def __new__(cls, *args, **kwargs):
+
+    def __init__(self, litellm_provider: str = "openai", **kwargs):
+        if hasattr(self, "name"):
+            # singleton already made
+            return
+
+        kwargs['name'] = "litellm"
+        kwargs['callback_class'] = LiteLLMCallback
+
+        super().__init__(
+            litellm_provider=litellm_provider,
+            **kwargs
+        )
+
+        import litellm
+        self._instrument_module_members(litellm, "completion")
+
+    def __new__(cls, litellm_provider: str = "openai", **kwargs):
+        # Problem here if someone uses litellm with different providers. Only a
+        # single one will be made. Cannot make a fix just here as
+        # track_all_costs creates endpoints via the singleton mechanism.
+
         return super(Endpoint, cls).__new__(cls, name="litellm")
 
     def handle_wrapped_call(
@@ -43,30 +104,18 @@ class LiteLLMEndpoint(Endpoint):
         callback: Optional[EndpointCallback]
     ) -> None:
 
-        model_name = ""
-        if hasattr(response, 'model'):
-            model_name = response.model
-
         counted_something = False
+
         if hasattr(response, 'usage'):
             counted_something = True
-            usage = response.usage.dict()
 
-            self.global_callback.handle_generation(response=usage)
+            self.global_callback.handle_generation(response=response)
 
             if callback is not None:
-                callback.handle_generation(response=usage)
+                callback.handle_generation(response=response)
 
         if not counted_something:
             logger.warning(
-                f"Unrecognized litellm response format. It did not have usage information:\n"
-                + pp.pformat(response)
+                "Unrecognized litellm response format. It did not have usage information:\n%s",
+                pp.pformat(response)
             )
-
-    def __init__(self, *args, **kwargs):
-        import os
-
-        kwargs['name'] = "litellm"
-        kwargs['callback_class'] = LiteLLMCallback
-
-        super().__init__(*args, **kwargs)
