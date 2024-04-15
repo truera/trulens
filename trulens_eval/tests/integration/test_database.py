7dfd2e6b0fb01f4cb4fd0d376f8d478ad8dfb3bd
Piotr Mardziel <piotrm@gmail.com>
2024-04-15 13:13:38 -0700

    first

--- trulens_eval/tests/integration/test_database.py
+++ trulens_eval/tests/integration/test_database.py
@@ -1,12 +1,36 @@
+"""Database Tests
+
+Some of the tests in file require a running docker container which hosts the
+tested databases. See `trulens_eval/docker/test-database.yaml` and/or
+`trulens_eval/Makefile` target `test-database` for how to get this container
+running.
+
+- Tests migration of old databases to new ones.
+
+- Tests uses of various DB vender types. A subset of types supported by
+  sqlalchemy:
+
+    - sqlite
+    - postgres (in docker)
+    - mysql (in docker)
+
+- Tests database options like prefix.
+
+- Tests database utilities:
+
+    - `copy_database`
+"""
+
 from contextlib import contextmanager
 import json
 from pathlib import Path
 import shutil
 from tempfile import TemporaryDirectory
-from typing import Literal, Union
+from typing import Any, Dict, Iterator, Literal, Union
 from unittest import main
 from unittest import TestCase
 
+import pandas as pd
 from sqlalchemy import Engine
 from sqlalchemy import text
 
@@ -22,42 +46,96 @@ from trulens_eval.database.migrations import DbRevisions
 from trulens_eval.database.migrations import downgrade_db
 from trulens_eval.database.migrations import get_revision_history
 from trulens_eval.database.migrations import upgrade_db
+#from trulens_eval.database.sqlalchemy import AppsExtractor
+#from trulens_eval.database.sqlalchemy import SQLAlchemyDB
 from trulens_eval.database.sqlalchemy_db import AppsExtractor
-from trulens_eval.database.sqlalchemy_db import SqlAlchemyDB
+from trulens_eval.database.sqlalchemy_db import SqlAlchemyDB as SQLAlchemyDB
 from trulens_eval.database.utils import is_legacy_sqlite
+#from trulens_eval.database.legacy.sqlite import LocalSQLite
+# from trulens_eval.database.base import DB
 from trulens_eval.db import DB
 from trulens_eval.db import LocalSQLite
 
 
+class TestDBSpecifications(TestCase):
+    """
+    Tests for database options.
+    """
+
+    def test_prefix(self):
+
+        db_types = ["sqlite_file"]#, "postgres", "mysql", "sqlite_memory"
+        # sqlite_memory might have problems with multithreading of tests
+
+        for db_type in db_types:
+            with self.subTest(msg=f"prefix for {db_type}"):
+                with clean_db(db_type) as db:
+
+                    tables = ["apps", "records", "feedback_defs", "feedbacks"]
+
+                    #for table in tables:
+                    #    print(table)
+                    #    df = pd.read_sql_table(table_name="test_" + table, con=db.engine)
+                    #    print(df)
+                    #    print()
+
+                    _test_db_consistency(self, db)
+
+                    # Check that we have the correct table names.
+                    with db.engine.begin() as conn:
+                        df = pd.read_sql("SELECT * FROM test_alembic_version", conn)
+                        print(df)
+
+
+                    # self.assertTrue(db.engine.url.database.startswith("test_"))
+
+
+    def test_copy(self):
+        pass
+
+    def test_migrate_prefix(self):
+        pass
+
 class TestDbV2Migration(TestCase):
+    """Migrations from legacy sqlite db to sqlalchemy-managed databases of
+    various kinds.
+    """
 
     def test_db_migration_sqlite_file(self):
+        """Test migration from legacy sqlite db to sqlite db."""
         with clean_db("sqlite_file") as db:
             _test_db_migration(db)
 
     def test_db_migration_postgres(self):
+        """Test migration from legacy sqlite db to postgres db."""
         with clean_db("postgres") as db:
             _test_db_migration(db)
 
     def test_db_migration_mysql(self):
+        """Test migration from legacy sqlite db to mysql db."""
         with clean_db("mysql") as db:
             _test_db_migration(db)
 
     def test_db_consistency_sqlite_file(self):
+        """Test database consistency after migration to sqlite."""
         with clean_db("sqlite_file") as db:
-            _test_db_consistency(db)
+            _test_db_consistency(self, db)
 
     def test_db_consistency_postgres(self):
+        """Test database consistency after migration to postgres."""
         with clean_db("postgres") as db:
-            _test_db_consistency(db)
+            _test_db_consistency(self, db)
 
     def test_db_consistency_mysql(self):
+        """Test database consistency after migration to mysql."""
         with clean_db("mysql") as db:
-            _test_db_consistency(db)
+            _test_db_consistency(self, db)
 
     def test_future_db(self):
-        # Check handling of database that is newer than the current
-        # trulens_eval's db version. We expect a warning and exception.
+        """Check handling of database that is newer than the current
+        trulens_eval's db version. 
+        
+        We expect a warning and exception."""
 
         for folder in (Path(__file__).parent.parent.parent /
                        "release_dbs").iterdir():
@@ -75,7 +153,7 @@ class TestDbV2Migration(TestCase):
                     self._test_future_db(dbfile=dbfile)
 
     def _test_future_db(self, dbfile: Path = None):
-        db = SqlAlchemyDB.from_db_url(f"sqlite:///{dbfile}")
+        db = SQLAlchemyDB.from_db_url(f"sqlite:///{dbfile}")
         self.assertFalse(is_legacy_sqlite(db.engine))
 
         # Migration should state there is a future version present which we
@@ -96,8 +174,10 @@ class TestDbV2Migration(TestCase):
         )
 
     def test_migrate_legacy_legacy_sqlite_file(self):
-        # Migration from non-latest lagecy db files all the way to v2 database.
-        # This involves migrating the legacy dbs to the latest legacy first.
+        """Migration from non-latest lagecy db files all the way to v2 database.
+
+        This involves migrating the legacy dbs to the latest legacy first.
+        """
 
         for folder in (Path(__file__).parent.parent.parent /
                        "release_dbs").iterdir():
@@ -117,7 +197,7 @@ class TestDbV2Migration(TestCase):
 
     def _test_migrate_legacy_legacy_sqlite_file(self, dbfile: Path = None):
         # run migration
-        db = SqlAlchemyDB.from_db_url(f"sqlite:///{dbfile}")
+        db = SQLAlchemyDB.from_db_url(f"sqlite:///{dbfile}")
         self.assertTrue(is_legacy_sqlite(db.engine))
         db.migrate_database()
 
@@ -140,7 +220,7 @@ class TestDbV2Migration(TestCase):
             fb, app, rec = _populate_data(legacy_db)
 
             # run migration
-            db = SqlAlchemyDB.from_db_url(f"sqlite:///{file}")
+            db = SQLAlchemyDB.from_db_url(f"sqlite:///{file}")
             self.assertTrue(is_legacy_sqlite(db.engine))
             db.migrate_database()
 
@@ -171,31 +251,54 @@ class TestDbV2Migration(TestCase):
 
 
 class MockFeedback(Provider):
+    """Provider for testing purposes."""
 
     def length(self, text: str) -> float:  # noqa
+        """Test feedback that does nothing except return length of input"""
+
         return float(len(text))
 
 
 @contextmanager
-def clean_db(alias: str) -> SqlAlchemyDB:
-    with TemporaryDirectory() as tmp:
+def clean_db(
+    alias: str,
+    **kwargs: Dict[str, Any]
+) -> Iterator[SQLAlchemyDB]:
+    """Yields a clean database instance for the given database type.
+    
+    Args:
+        alias: Database type to use from the following: `sqlite_file`,
+            `sqlite_memory`, `postgres`, `mysql`.
+
+        kwargs: Additional keyword arguments to pass to the database
+            constructor.
+    """
+
+    with TemporaryDirectory(ignore_cleanup_errors=True) as tmp:
+        # NOTE: The parameters below come from the docker definition in the
+        # `trulens_eval/docker/test-database.yaml` file.
         url = {
+            "sqlite_memory":
+                "sqlite:///:memory:",
+                # TODO: Test this one more.
+                # NOTE: Sqlalchemy docs say this should be written
+                # "sqlite://:memory:" but that gives an error on mac at least.
             "sqlite_file":
-                f"sqlite:///{Path(tmp).joinpath('test.sqlite')}",
+                f"sqlite:///{Path(tmp) / 'test.sqlite'}",
             "postgres":
                 "postgresql+psycopg2://pg-test-user:pg-test-pswd@localhost/pg-test-db",
             "mysql":
                 "mysql+pymysql://mysql-test-user:mysql-test-pswd@localhost/mysql-test-db",
         }[alias]
 
-        db = SqlAlchemyDB.from_db_url(url)
+        db = SQLAlchemyDB.from_db_url(url, **kwargs)
 
-        downgrade_db(
-            db.engine, revision="base"
-        )  # drops all tables except `alembic_version`
-
-        with db.engine.connect() as conn:
-            conn.execute(text("DROP TABLE alembic_version"))
+        # NOTE(piotrm): I couldn't figure out why these things were done here.
+        #downgrade_db(
+        #    db.engine, revision="base"
+        #)  # drops all tables except `db.version_table`
+        #with db.engine.connect() as conn:
+        #    conn.execute(text(f"DROP TABLE {db.table_prefix}version_table"))
 
         yield db
 
@@ -204,12 +307,15 @@ def assert_revision(
     engine: Engine, expected: Union[None, str], status: Literal["in_sync",
                                                                 "behind"]
 ):
+    """Asserts that the version of the database `engine` is `expected` and
+    has the `status` flag set."""
+
     revisions = DbRevisions.load(engine)
     assert revisions.current == expected, f"{revisions.current} != {expected}"
     assert getattr(revisions, status)
 
 
-def _test_db_migration(db: SqlAlchemyDB):
+def _test_db_migration(db: SQLAlchemyDB):
     engine = db.engine
     history = get_revision_history(engine)
     curr_rev = None
@@ -231,51 +337,120 @@ def _test_db_migration(db: SqlAlchemyDB):
     assert_revision(engine, None, "behind")
 
 
-def _test_db_consistency(db: SqlAlchemyDB):
+class db:
+    orm = orm
+
+def _test_db_consistency(test: TestCase, db: SQLAlchemyDB):
     db.migrate_database()  # ensure latest revision
 
     _populate_data(db)
+
     with db.Session.begin() as session:
-        session.delete(
-            session.query(orm.AppDefinition).one()
-        )  # delete the only app
-        assert session.query(orm.Record
-                            ).all() == []  # records are deleted in cascade
-        assert session.query(orm.FeedbackResult).all() == [
-        ]  # feedbacks results are deleted in cascade
-        session.query(orm.FeedbackDefinition
-                     ).one()  # feedback defs are preserved
+        print("feedback defs")
+        ress = session.query(db.orm.FeedbackDefinition).all()
+        for res in ress:
+            print(res.feedback_definition_id)
+
+        print("apps")
+        ress = session.query(db.orm.AppDefinition).all()
+        for res in ress:
+            print(res.app_id, res.records) # no feedback results
+
+        print("records")
+        ress = session.query(db.orm.Record).all()
+        for res in ress:
+            print(res.record_id, res.feedback_results)
+
+        print("feedbacks")
+        ress = session.query(db.orm.FeedbackResult).all()
+        for res in ress:
+            print(res.feedback_result_id, res.feedback_definition)
+
+    with db.Session.begin() as session:
+        # delete the only app
+        session.delete(session.query(db.orm.AppDefinition).one())
+
+        # records are deleted in cascade
+        test.assertEqual(
+            session.query(db.orm.Record).all(),
+            [],
+            "Expected no records."
+        )
+
+        # feedbacks results are deleted in cascade
+        test.assertEqual(
+            session.query(db.orm.FeedbackResult).all(),
+            [],
+            "Expected no feedback results."
+        )
+
+
+        # feedback defs are preserved
+        test.assertEqual(
+            len(session.query(db.orm.FeedbackDefinition).all()), 1,
+            "Expected exactly one feedback to be in the db."
+        )
 
     _populate_data(db)
+
     with db.Session.begin() as session:
-        session.delete(
-            session.query(orm.Record).one()
-        )  # delete the only record
-        assert session.query(orm.FeedbackResult).all() == [
-        ]  # feedbacks results are deleted in cascade
-        session.query(orm.AppDefinition).one()  # apps are preserved
-        session.query(orm.FeedbackDefinition
-                     ).one()  # feedback defs are preserved
+        test.assertEqual(
+            len(session.query(db.orm.Record).all()), 1,
+            "Expected exactly one record."
+        )
+
+        test.assertEqual(
+            len(session.query(db.orm.FeedbackResult).all()), 1,
+            "Expected exactly one feedback result."
+        )
+
+        ress = session.query(db.orm.FeedbackResult).all()
+        for res in ress:
+            print("result record=", res.record)
+            print("result def=", res.feedback_definition)
+
+        for n, t in db.orm.registry.items():
+            print(n, t)
+
+        # delete the only record
+        session.delete(session.query(db.orm.Record).one())
+
+        ress = session.query(db.orm.FeedbackResult).all()
+        for res in ress:
+            print("result record=", res.record)
+            print("result def=", res.feedback_definition)
+
+        # feedbacks results are deleted in cascade
+        test.assertEqual(session.query(db.orm.FeedbackResult).all(), [], "Expected no feedback results.")
+
+        # apps are preserved
+        test.assertTrue(session.query(db.orm.AppDefinition).one(), "Expected an app.")
+
+        # feedback defs are preserved
+        test.assertTrue(session.query(db.orm.FeedbackDefinition).one(), "Expected a feedback definition.")
 
 
 def _populate_data(db: DB):
     tru = Tru()
     tru.db = db  # because of the singleton behavior, db must be changed manually
+    
     fb = Feedback(
         imp=MockFeedback().length,
-        feedback_definition_id="mock",
+        # feedback_definition_id="mock",
         selectors={"text": Select.RecordOutput},
     )
     app = TruBasicApp(
         text_to_text=lambda x: x,
-        app_id="test",
+        # app_id="test",
         db=db,
         feedbacks=[fb],
         feedback_mode=FeedbackMode.WITH_APP,
     )
     _, rec = app.with_record(app.app.__call__, "boo")
-    return fb, app, rec
 
+    print(rec.wait_for_feedback_results())
+
+    return fb, app, rec
 
 if __name__ == '__main__':
     main()
