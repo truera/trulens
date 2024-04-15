e24c6530f7f9f64bd288d23db7ff94a07f85a4bd
Piotr Mardziel <piotrm@gmail.com>
2024-04-15 09:29:51 -0700

    order

--- trulens_eval/tests/e2e/test_tru.py
+++ trulens_eval/tests/e2e/test_tru.py
@@ -11,11 +11,11 @@ from unittest import TestCase
 from tests.unit.test import optional_test
 
 from trulens_eval import Feedback
+from trulens_eval import schema
 from trulens_eval import Tru
 from trulens_eval import TruCustomApp
 from trulens_eval.feedback.provider.hugs import Dummy
 from trulens_eval.keys import check_keys
-from trulens_eval import schema
 from trulens_eval.tru_custom_app import TruCustomApp
 
 
