d5c08251f9ebdd7e3b6197d8400d23cde83a5e09
Piotr Mardziel <piotrm@gmail.com>
2024-04-16 23:12:49 -0700

    configurable table prefix (#971)
    * prototyping
    
    * nothing
    
    * adjust ini usage
    
    * ini
    
    * Fixing docs.
    
    * satisfy abc for deprecated db
    
    * indent fix
    
    * few more formatting fixes
    
    * more format fixes
    
    * refactor db init in Tru
    
    * move large design decision sections from docstrings to a single md file.
    
    * add design.md and notes
    
    * doc fixes
    
    * cleaning up db implementation docs and some code
    
    * work
    
    * forgot some
    
    * add database_prefix to Tru
    
    * moving and renaming things
    
    * ignores
    
    * fixes from the move/renames
    
    * remove accidental file
    
    * more fixes, and fix reset_database
    
    * print arg parse exception
    
    * streamlit state fixes
    
    * forgot to remove old import
    
    * update database migration notes and copy_database
    
    * move old database to legacy databases folder
    
    * rename class
    
    * typos and doc fixes
    
    * nits
    
    * nits
    
    * testing copy_database
    
    * format
    
    * working on prefix tests
    
    * debugging the foreign keys
    
    * debugging more
    
    * clean up
    
    * typo
    
    * moved on_done_callback content into result() and added some database tests
    
    * undo non-needed
    
    * change alembic logging output
    
    * moved db revision migration check to Tru init
    
    ---------
    
    Co-authored-by: Aaron <163380706+arn-tru@users.noreply.github.com>

--- trulens_eval/trulens_eval/database/migrations/__init__.py
+++ trulens_eval/trulens_eval/database/migrations/__init__.py
@@ -1,7 +1,9 @@
+from __future__ import annotations
+
 from contextlib import contextmanager
 import logging
 import os
-from typing import List, Optional
+from typing import Iterator, List, Optional
 
 from alembic import command
 from alembic.config import Config
@@ -10,11 +12,17 @@ from alembic.script import ScriptDirectory
 from pydantic import BaseModel
 from sqlalchemy import Engine
 
+from trulens_eval.database import base as mod_db
+
 logger = logging.getLogger(__name__)
 
 
 @contextmanager
-def alembic_config(engine: Engine) -> Config:
+def alembic_config(
+    engine: Engine,
+    prefix: str = mod_db.DEFAULT_DATABASE_PREFIX
+) -> Iterator[Config]:
+
     alembic_dir = os.path.dirname(os.path.abspath(__file__))
     db_url = str(engine.url).replace("%", "%%")  # Escape any '%' in db_url
     config = Config(os.path.join(alembic_dir, "alembic.ini"))
@@ -23,31 +31,48 @@ def alembic_config(engine: Engine) -> Config:
         "calling_context", "PYTHON"
     )  # skips CLI-specific setup
     config.set_main_option("sqlalchemy.url", db_url)
+    config.set_main_option("trulens.table_prefix", prefix)
     config.attributes["engine"] = engine
+
     yield config
 
 
-def upgrade_db(engine: Engine, revision: str = "head"):
-    with alembic_config(engine) as config:
+def upgrade_db(
+    engine: Engine,
+    revision: str = "head",
+    prefix: str = mod_db.DEFAULT_DATABASE_PREFIX
+):
+    with alembic_config(engine, prefix=prefix) as config:
         command.upgrade(config, revision)
 
 
-def downgrade_db(engine: Engine, revision: str = "base"):
-    with alembic_config(engine) as config:
+def downgrade_db(
+    engine: Engine,
+    revision: str = "base",
+    prefix: str = mod_db.DEFAULT_DATABASE_PREFIX
+):
+    with alembic_config(engine, prefix=prefix) as config:
         command.downgrade(config, revision)
 
 
-def get_current_db_revision(engine: Engine) -> Optional[str]:
+def get_current_db_revision(
+    engine: Engine,
+    prefix: str = mod_db.DEFAULT_DATABASE_PREFIX
+) -> Optional[str]:
     with engine.connect() as conn:
-        return MigrationContext.configure(conn).get_current_revision()
+        return MigrationContext.configure(
+            conn, opts=dict(version_table=prefix + "alembic_version")
+        ).get_current_revision()
 
 
-def get_revision_history(engine: Engine) -> List[str]:
+def get_revision_history(
+    engine: Engine, prefix: str = mod_db.DEFAULT_DATABASE_PREFIX
+) -> List[str]:
     """
     Return list of all revisions, from base to head.
     Warn: Branching not supported, fails if there's more than one head.
     """
-    with alembic_config(engine) as config:
+    with alembic_config(engine, prefix=prefix) as config:
         scripts = ScriptDirectory.from_config(config)
         return list(
             reversed(
@@ -72,10 +97,14 @@ class DbRevisions(BaseModel):
         return self.history[-1]
 
     @classmethod
-    def load(cls, engine: Engine) -> "DbRevisions":
+    def load(
+        cls,
+        engine: Engine,
+        prefix: str = mod_db.DEFAULT_DATABASE_PREFIX
+    ) -> DbRevisions:
         return cls(
-            current=get_current_db_revision(engine),
-            history=get_revision_history(engine),
+            current=get_current_db_revision(engine, prefix=prefix),
+            history=get_revision_history(engine, prefix=prefix),
         )
 
     @property
