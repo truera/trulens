b085ceb9a093d7a95b7f4eab14400680e53c6bca
Piotr Mardziel <piotrm@gmail.com>
2024-04-17 15:55:18 -0700

    remove unneeded

--- trulens_eval/trulens_eval/database/utils.py
+++ trulens_eval/trulens_eval/database/utils.py
@@ -18,50 +18,6 @@ from trulens_eval.database.sqlalchemy import SQLAlchemyDB
 logger = logging.getLogger(__name__)
 
 
-def for_all_methods(decorator, _except: Optional[List[str]] = None):
-    """
-    Applies decorator to all methods except classmethods, private methods and
-    the ones specified with `_except`.
-    """
-
-    def decorate(cls):
-
-        for attr_name, attr in cls.__dict__.items(
-        ):  # does not include classmethods
-
-            if not inspect.isfunction(attr):
-                continue  # skips non-method attributes
-
-            if attr_name.startswith("_"):
-                continue  # skips private methods
-
-            if _except is not None and attr_name in _except:
-                continue
-
-            logger.debug("Decorating %s", attr_name)
-            setattr(cls, attr_name, decorator(attr))
-
-        return cls
-
-    return decorate
-
-
-def run_before(callback: Callable):
-    """
-    Create decorator to run the callback before the function.
-    """
-
-    def decorator(func):
-
-        def wrapper(*args, **kwargs):
-            callback(*args, **kwargs)
-            return func(*args, **kwargs)
-
-        return wrapper
-
-    return decorator
-
-
 def is_legacy_sqlite(engine: Engine) -> bool:
     """Check if DB is an existing file-based SQLite created with the legacy
     `LocalSQLite` implementation.
