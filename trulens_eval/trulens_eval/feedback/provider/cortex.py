20b592386e72eb56d332c9d196b794b6e3d3531d
Daniel <daniel.huang@snowflake.com>
2024-06-12 16:34:49 -0700

    endpoint + provider wip

--- trulens_eval/trulens_eval/feedback/provider/cortex.py
+++ trulens_eval/trulens_eval/feedback/provider/cortex.py
@@ -4,8 +4,9 @@ from typing import ClassVar, Dict, Optional, Sequence
 import os
 import pydantic
 from trulens_eval.feedback.provider.base import LLMProvider
-from trulens_eval.feedback.provider.endpoint.base import Endpoint
-from snowflake.cortex import Complete
+from snowflake.snowpark import Session
+from trulens_eval.feedback.provider.endpoint.cortex import CortexEndpoint
+
 
 class Cortex(LLMProvider):
     # require `pip install snowflake-snowpark-python` and a active Snowflake account with proper privileges
@@ -18,32 +19,33 @@ class Cortex(LLMProvider):
        Reference: https://docs.snowflake.com/en/sql-reference/functions/complete-snowflake-cortex
     """
 
-
-
-    endpoint: Endpoint
+    endpoint: CortexEndpoint
 
     def __init__(
         self,
         model_engine: Optional[str] = None,
         snowflake_connection_params: Optional[Dict] = None,
-        endpoint: Optional[Endpoint] = None,
+        *args,
         **kwargs: dict    
     ):
-        if model_engine is None:
-            model_engine = self.DEFAULT_MODEL_ENGINE
-        
-        from snowflake.snowpark import Session
+        self_kwargs = dict(kwargs)
+
+        self_kwargs['model_engine'] = self.DEFAULT_MODEL_ENGINE if model_engine is None else model_engine
+        self_kwargs["endpoint"] = CortexEndpoint(
+            *args, **kwargs
+        )
+    
         connection_params = {
             "account": os.environ["SNOWFLAKE_ACCOUNT"],
             "user": os.environ["SNOWFLAKE_USER"],
             "password": os.environ["SNOWFLAKE_USER_PASSWORD"],
-        }
+        } if snowflake_connection_params is None else snowflake_connection_params
 
         # Create a Snowflake session
-        snowflake_session = Session.builder.configs(connection_params).create()
-
-
-    
+        self_kwargs['snowflake_session'] = Session.builder.configs(connection_params).create()
+        
+        super().__init__(**self_kwargs)
+        
     def _create_chat_completion(
         self,
         prompt: Optional[str] = None,
@@ -52,7 +54,39 @@ class Cortex(LLMProvider):
     ) -> str:
         if 'model' not in kwargs:
             kwargs['model'] = self.model_engine
-        
+    
+    def _create_chat_completion(
+        self,
+        prompt: Optional[str] = None,
+        messages: Optional[Sequence[Dict]] = None,
+        **kwargs
+    ) -> str:
+
+        completion_args = kwargs
+        if 'model' not in kwargs:
+            kwargs['model'] = self.model_engine
+
+        completion_args.update(self.completion_args)
+
+        if messages is not None:
+            completion_args['messages'] = messages
+
+        elif prompt is not None:
+            completion_args['messages'] = [
+                {
+                    "role": "system",
+                    "content": prompt
+                }
+            ]
+
+        else:
+            raise ValueError("`prompt` or `messages` must be specified.")
+
+        comp = completion(**completion_args)
+
+        assert isinstance(comp, object)
+
+        return comp["choices"][0]["message"]["content"]
         
         
         
