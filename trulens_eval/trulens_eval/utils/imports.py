f3f6aa4247b54de00082d1123f5678f6b6ae59d1
Daniel <daniel.huang@snowflake.com>
2024-06-12 17:21:45 -0700

    WIP provider almost done

--- trulens_eval/trulens_eval/utils/imports.py
+++ trulens_eval/trulens_eval/utils/imports.py
@@ -358,7 +358,7 @@ REQUIREMENT_OPENAI = format_import_errors(
 )
 
 REQUIREMENT_CORTEX = format_import_errors(
-    ['snowflake-snowpark-python'], purpose="using Snowflake Cortex serverless LLM functions"
+    ['snowflake-snowpark-python', 'json'], purpose="using Snowflake Cortex serverless LLM functions"
 )
 
 REQUIREMENT_GROUNDEDNESS = format_import_errors(
