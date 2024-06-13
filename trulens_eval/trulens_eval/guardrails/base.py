8c0c82ee12757b1f23f213dd8716eb28431da27a
Josh Reini <josh.reini@snowflake.com>
2024-06-13 00:14:00 -0400

    base context filter for custom apps

---
+++ trulens_eval/trulens_eval/guardrails/base.py
@@ -0,0 +1,58 @@
+from concurrent.futures import wait
+from trulens_eval.feedback import Feedback
+from trulens_eval.utils.containers import first
+from trulens_eval.utils.containers import second
+from trulens_eval.utils.threading import ThreadPoolExecutor
+from functools import wraps
+
+class context_filter:
+    def __init__(self, feedback: Feedback, threshold: float):
+        self.feedback = feedback
+        self.threshold = threshold
+    """
+    ContextFilter is a class that filters contexts based on a given feedback and threshold.
+
+    Parameters:
+    feedback (Feedback): The feedback object to use for filtering.
+    threshold (float): The minimum feedback value required for a context to be included.
+
+    !!! example
+
+        ```python
+        feedback = Feedback(provider.groundedness_measure_with_cot_reasons, name="Groundedness")
+        @context_filter(feedback, 0.5)
+        def retrieve(query: str) -> list:
+            results = vector_store.query(
+            query_texts=query,
+            n_results=3
+        )
+        return [doc for sublist in results['documents'] for doc in sublist]
+        ```
+    """
+
+    def __call__(self, func):
+        def wrapper(*args, **kwargs):
+            contexts = func(*args, **kwargs)
+            ex = ThreadPoolExecutor(max_workers=max(1, len(contexts)))
+            futures = list(
+                (
+                    context,
+                    ex.submit(
+                        (
+                            lambda context: self.feedback(
+                                args[1], context
+                            ) > self.threshold
+                        ),
+                        context
+                    )
+                ) for context in contexts
+            )
+            wait([future for (_, future) in futures])
+            results = list((context, future.result()) for (context, future) in futures)
+            filtered = map(first, filter(second, results))
+            wrapper.__name__ = func.__name__
+            wrapper.__doc__ = func.__doc__
+            return list(filtered)
+        wrapper.__name__ = func.__name__
+        wrapper.__doc__ = func.__doc__
+        return wrapper
