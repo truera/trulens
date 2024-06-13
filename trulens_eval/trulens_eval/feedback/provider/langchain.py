a8eced9e331f34218a140d948167215e1451c961
Nicolò Boschi <boschi1997@gmail.com>
2024-06-13 19:08:53 +0200

    improve langchain prompting using native messages (#1194)
    Co-authored-by: Josh Reini <josh.reini@snowflake.com>

--- trulens_eval/trulens_eval/feedback/provider/langchain.py
+++ trulens_eval/trulens_eval/feedback/provider/langchain.py
@@ -1,14 +1,25 @@
-import json
 import logging
 from typing import Dict, Optional, Sequence, Union
 
 from langchain.chat_models.base import BaseChatModel
 from langchain.llms.base import BaseLLM
+from langchain_core.messages import (
+    AIMessage,
+    HumanMessage,
+    BaseMessage
+)
 
 from trulens_eval.feedback.provider.base import LLMProvider
 from trulens_eval.feedback.provider.endpoint import LangchainEndpoint
 
 logger = logging.getLogger(__name__)
+
+
+def _convert_message(message: Dict) -> BaseMessage:
+    """Convert a message to a LangChain BaseMessage."""
+    if not "role" in message or message["role"] == "user":
+        return HumanMessage(content=message["content"])
+    return AIMessage(content=message["content"])
 
 
 class Langchain(LLMProvider):
@@ -57,8 +68,8 @@ class Langchain(LLMProvider):
             predict = self.endpoint.chain.predict(prompt, **kwargs)
 
         elif messages is not None:
-            prompt = json.dumps(messages)
-            predict = self.endpoint.chain.predict(prompt, **kwargs)
+            messages = [_convert_message(message) for message in messages]
+            predict = self.endpoint.chain.predict_messages(messages, **kwargs).content
 
         else:
             raise ValueError("`prompt` or `messages` must be specified.")
