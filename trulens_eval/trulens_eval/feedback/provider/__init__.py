a7367520f1a6ccb95f1614e6ade073ceb5b23de4
Daniel <daniel.huang@snowflake.com>
2024-06-13 15:30:51 -0700

    imports

--- trulens_eval/trulens_eval/feedback/provider/__init__.py
+++ trulens_eval/trulens_eval/feedback/provider/__init__.py
@@ -1,7 +1,7 @@
 from trulens_eval.feedback.provider.base import Provider
 from trulens_eval.feedback.provider.hugs import Huggingface
 from trulens_eval.feedback.provider.langchain import Langchain
-from trulens_eval.utils.imports import OptionalImports
+from trulens_eval.utils.imports import REQUIREMENT_CORTEX, OptionalImports
 from trulens_eval.utils.imports import REQUIREMENT_BEDROCK
 from trulens_eval.utils.imports import REQUIREMENT_LITELLM
 from trulens_eval.utils.imports import REQUIREMENT_OPENAI
@@ -15,6 +15,9 @@ with OptionalImports(messages=REQUIREMENT_BEDROCK):
 with OptionalImports(messages=REQUIREMENT_OPENAI):
     from trulens_eval.feedback.provider.openai import AzureOpenAI
     from trulens_eval.feedback.provider.openai import OpenAI
+
+with OptionalImports(messages=REQUIREMENT_CORTEX):
+    from trulens_eval.feedback.provider.cortex import Cortex
 
 __all__ = [
     "Provider",
