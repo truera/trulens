92b341d1549f4f30598ad8d9d40f05f1eb153649
Josh Reini <josh.reini@snowflake.com>
2024-06-13 09:50:15 -0400

    make display util more efficient

--- trulens_eval/trulens_eval/utils/display.py
+++ trulens_eval/trulens_eval/utils/display.py
@@ -1,18 +1,8 @@
 import pandas as pd
 
 def get_feedback_result(tru_record, feedback_name):
-    feedback_calls = None
-    for feedback_definition, future_result in tru_record.feedback_and_future_results:
-        if feedback_definition.name == feedback_name:
-            feedback_calls = future_result.result()
-            break
-
+    feedback_calls = next((future_result.result() for feedback_definition, future_result in tru_record.feedback_and_future_results if feedback_definition.name == feedback_name), None)
     if feedback_calls is None:
         return pd.DataFrame()
-
-    feedback_result = []
-    for i in range(len(feedback_calls.calls)):
-        args = feedback_calls.calls[i].model_dump()['args']
-        ret = feedback_calls.calls[i].model_dump()['ret']
-        feedback_result.append({**args, 'ret': ret})
+    feedback_result = [{**call.model_dump()['args'], 'ret': call.model_dump()['ret']} for call in feedback_calls.calls]
     return pd.DataFrame(feedback_result)
