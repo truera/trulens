c04cb71675de3b72ad0102dbadaf4911e32e646b
David Kurokawa <david.kurokawa@snowflake.com>
2024-06-17 13:26:10 -0700

    Create custom feedback to use Corey's custom small model.

---
+++ trulens_eval/examples/snowflake_demo/custom_feedback/small_local_models.py
@@ -0,0 +1,37 @@
+import torch
+from transformers import AutoModelForSequenceClassification
+from transformers import AutoTokenizer
+from scipy.special import expit
+
+from trulens_eval import Provider
+
+
+class SmallLocalModels(Provider):
+
+    def __init__(self, context_relevance_model_path: str):
+        self.model_config["context_relevance_tokenizer"] = (
+            AutoTokenizer.from_pretrained(context_relevance_model_path)
+        )
+        self.model_config["context_relevance_model"] = (
+            AutoModelForSequenceClassification.from_pretrained(
+                context_relevance_model_path
+            )
+        )
+
+    def context_relevance(
+        self, question: str, context: str, temperature: float = 0.0
+    ) -> float:
+        tokenizer = self.model_config["context_relevance_tokenizer"]
+        model = self.model_config["context_relevance_model"]
+        with torch.no_grad():
+            logit = model.forward(
+                torch.tensor(
+                    tokenizer.convert_tokens_to_ids(
+                        tokenizer.tokenize(f"{question} [SEP] {context}")
+                    )
+                ).reshape(1, -1)
+            ).logits.numpy()
+            if logit.size != 1:
+                raise ValueError("Unexpected number of results from model!")
+            logit = float(logit[0, 0])
+            return expit(logit)
--- trulens_eval/examples/snowflake_demo/feedback.py
+++ trulens_eval/examples/snowflake_demo/feedback.py
@@ -1,56 +1,79 @@
 import os
+
 from dotenv import load_dotenv
-from trulens_eval import Tru
-from trulens_eval import Feedback, Select
-from trulens_eval.feedback.provider.litellm import LiteLLM
 import numpy as np
+
+from trulens_eval import Feedback
+from trulens_eval import Select
+from trulens_eval import Tru
+from trulens_eval.feedback.provider.litellm import LiteLLM
+from custom_feedback.small_local_models import SmallLocalModels
 
 load_dotenv()
 
-db_url = 'snowflake://{user}:{password}@{account}/{dbname}/{schema}?warehouse={warehouse}&role={role}'.format(
-    user=os.environ['SF_USER'],
-    account=os.environ['SF_ACCOUNT'],
-    password=os.environ['SF_PASSWORD'],
-    dbname=os.environ['SF_DB_NAME'],
-    schema=os.environ['SF_SCHEMA'],
-    warehouse=os.environ['SF_WAREHOUSE'],
-    role=os.environ['SF_ROLE'],
+db_url = "snowflake://{user}:{password}@{account}/{dbname}/{schema}?warehouse={warehouse}&role={role}".format(
+    user=os.environ["SF_USER"],
+    account=os.environ["SF_ACCOUNT"],
+    password=os.environ["SF_PASSWORD"],
+    dbname=os.environ["SF_DB_NAME"],
+    schema=os.environ["SF_SCHEMA"],
+    warehouse=os.environ["SF_WAREHOUSE"],
+    role=os.environ["SF_ROLE"],
 )
 
 tru = Tru(database_url=db_url)
 
 provider = LiteLLM(model_engine="replicate/snowflake/snowflake-arctic-instruct")
+small_local_model_provider = SmallLocalModels(
+    os.getenv(
+        "SMALL_LOCAL_MODELS_CONTEXT_RELEVANCE_MODEL_PATH",
+        "/trulens_demo/small_local_models/context_relevance",
+    ),
+)
 
 f_groundedness = (
-    Feedback(provider.groundedness_measure_with_cot_reasons, name = "Groundedness")
-    .on(Select.RecordCalls.retrieve_context.rets[1][:])
-    .on_output()
+    Feedback(
+        provider.groundedness_measure_with_cot_reasons, name="Groundedness"
+    ).on(Select.RecordCalls.retrieve_context.rets[1][:]).on_output()
 )
-
 f_context_relevance = (
-    Feedback(provider.context_relevance, name = "Context Relevance")
-    .on_input()
-    .on(Select.RecordCalls.retrieve_context.rets[1][:])
-    .aggregate(np.mean) # choose a different aggregation method if you wish
+    Feedback(provider.context_relevance,
+             name="Context Relevance").on_input().on(
+                 Select.RecordCalls.retrieve_context.rets[1][:]
+             ).aggregate(np.mean
+                        )  # choose a different aggregation method if you wish
+)
+f_small_local_models_context_relevance = (
+    Feedback(
+        small_local_model_provider.context_relevance,
+        name="[Small Local Model] Context Relevance",
+    ).on_input().on(Select.RecordCalls.retrieve_context.rets[1][:]).aggregate(
+        np.mean
+    )  # choose a different aggregation method if you wish
 )
 f_answer_relevance = (
-    Feedback(provider.relevance_with_cot_reasons, name = "Answer Relevance")
-    .on_input()
-    .on_output()
-    .aggregate(np.mean)
+    Feedback(provider.relevance_with_cot_reasons,
+             name="Answer Relevance").on_input().on_output().aggregate(np.mean)
 )
-f_criminality_input = (
-    Feedback(provider.criminality_with_cot_reasons,
-             name = "Criminality input",
-             higher_is_better=False)
-             .on(Select.RecordInput)
-)
-f_criminality_output = (
-    Feedback(provider.criminality_with_cot_reasons,
-             name = "Criminality output",
-             higher_is_better=False)
-             .on_output()
-)
+f_criminality_input = Feedback(
+    provider.criminality_with_cot_reasons,
+    name="Criminality input",
+    higher_is_better=False,
+).on(Select.RecordInput)
+f_criminality_output = Feedback(
+    provider.criminality_with_cot_reasons,
+    name="Criminality output",
+    higher_is_better=False,
+).on_output()
 
-feedbacks_rag = [f_context_relevance, f_answer_relevance, f_groundedness, f_criminality_input, f_criminality_output]
-feedbacks_no_rag = [f_answer_relevance, f_criminality_input, f_criminality_output]
+feedbacks_rag = [
+    f_context_relevance,
+    f_small_local_models_context_relevance,
+    f_answer_relevance,
+    f_groundedness,
+    f_criminality_input,
+    f_criminality_output,
+]
+feedbacks_no_rag = [
+    f_answer_relevance, f_criminality_input, f_criminality_output
+]
