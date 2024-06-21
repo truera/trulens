90a627c70605edf28a940fc7d821092ff6926318
Daniel <daniel.huang@snowflake.com>
2024-06-21 15:40:14 -0700

    fixing imports base ut for python 3.12

--- trulens_eval/trulens_eval/feedback/provider/endpoint/__init__.py
+++ trulens_eval/trulens_eval/feedback/provider/endpoint/__init__.py
@@ -23,7 +23,8 @@ with OptionalImports(messages=REQUIREMENT_OPENAI):
 if sys.version_info < (3, 12):
     # the dependency snowflake-snowpark-python not yet supported in 3.12
     with OptionalImports(messages=REQUIREMENT_CORTEX):
-        from trulens_eval.feedback.provider.endpoint.cortex import CortexEndpoint
+        from trulens_eval.feedback.provider.endpoint.cortex import \
+            CortexEndpoint
 
 __all__ = [
     "Endpoint",
