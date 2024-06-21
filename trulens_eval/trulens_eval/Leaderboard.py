62896cc4f853955f5b53c7a843ace4db9c88f99d
Josh Reini <josh.reini@snowflake.com>
2024-06-21 16:01:39 -0400

    remove deprecated legacy caching (#1233)
    * remove deprecated legacy caching
    
    * fmt

--- trulens_eval/trulens_eval/Leaderboard.py
+++ trulens_eval/trulens_eval/Leaderboard.py
@@ -9,18 +9,14 @@ from millify import millify
 import streamlit as st
 from streamlit_extras.switch_page_button import switch_page
 
+from trulens_eval import Tru
 from trulens_eval.database import base as mod_db
 from trulens_eval.database.legacy.migration import MIGRATION_UNKNOWN_STR
 from trulens_eval.utils.streamlit import init_from_args
-from trulens_eval.ux.page_config import set_page_config
-from trulens_eval.ux.styles import CATEGORY
-
-st.runtime.legacy_caching.clear_cache()
-
-from trulens_eval import Tru
 from trulens_eval.ux import styles
 from trulens_eval.ux.components import draw_metadata
 from trulens_eval.ux.page_config import set_page_config
+from trulens_eval.ux.styles import CATEGORY
 
 if __name__ == "__main__":
     # If not imported, gets args from command line and creates Tru singleton
