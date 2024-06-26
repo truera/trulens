768ff9094d742835000881c820081f1b185d0374
Josh Reini <josh.reini@snowflake.com>
2024-06-26 13:32:05 -0400

    Update common_ui.py

--- trulens_eval/examples/snowflake_demo/common_ui.py
+++ trulens_eval/examples/snowflake_demo/common_ui.py
@@ -136,7 +136,11 @@ def get_tru_app_id(
     model: str, temperature: float, top_p: float, max_new_tokens: int,
     use_rag: bool, retriever: str, retrieval_filter: float, filter_feedback_function: str, provider: str) -> str:
     # Args are hashed for cache'(' lookup
-    return f"app-dev-{model}{'-' + retriever if use_rag else ''}{('-retrieval-filter-' + filter_feedback_function + '-' + str(retrieval_filter)) if use_rag else ''} (provider-{provider}-temp-{temperature}-topp-{top_p}-maxtokens-{max_new_tokens})"
+    ret = f"app-dev-{model.lower()}"
+    if use_rag:
+        ret += f"-{retriever}-retrieval-filter-{filter_feedback_function}-{str(retrieval_filter)}"
+    ret += f"-provider-{provider}-temp-{temperature}-topp-{top_p}-maxtokens-{max_new_tokens}"
+    return ret
 
 @st.cache_resource
 def get_trulens_app(app_id: str, metadata: dict[str, Any]):
