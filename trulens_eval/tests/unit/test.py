9ff70287d40cc9bb49e36ecdc30e10d9bdb8ceed
Piotr Mardziel <piotr.mardziel@snowflake.com>
2024-06-26 15:21:33 -0700

    docs

--- trulens_eval/tests/unit/test.py
+++ trulens_eval/tests/unit/test.py
@@ -21,10 +21,11 @@ OPTIONAL_ENV_VAR = "TEST_OPTIONAL"
 
 
 def optional_test(testmethodorclass):
-    """
-    Only run the decorated test if the environment variable with_optional
-    evalutes true. These are meant to be run only in an environment where
-    optional packages have been installed.
+    """Only run the decorated test if the environment variable with_optional
+    evalutes true.
+    
+    These are meant to be run only in an environment where optional packages
+    have been installed.
     """
 
     return unittest.skipIf(
@@ -35,8 +36,10 @@ def optional_test(testmethodorclass):
 def requiredonly_test(testmethodorclass):
     """
     Only runs the decorated test if the environment variable with_optional
-    evalutes to false or is not set. Decorated tests are meant to run
-    specifically when optional imports are not installed.
+    evalutes to false or is not set.
+    
+    Decorated tests are meant to run specifically when optional imports are not
+    installed.
     """
 
     return unittest.skipIf(
@@ -45,6 +48,8 @@ def requiredonly_test(testmethodorclass):
 
 
 def module_installed(module: str) -> bool:
+    """Check if a module is installed."""
+
     try:
         __import__(module)
         return True
@@ -53,6 +58,7 @@ def module_installed(module: str) -> bool:
 
 
 class JSONTestCase(TestCase):
+    """TestCase class that adds JSON comparisons and golden expectation handling."""
 
     def assertGoldenJSONEqual(
         self,
@@ -61,6 +67,14 @@ class JSONTestCase(TestCase):
         skips=None,
         numeric_places: int = 7,
     ):
+        """Assert equality between JSON-like `actual` and the content of
+        `golden_filename`.
+
+        If the environment variable `WRITE_GOLDEN` is set, the golden file will
+        be overwritten with the `actual` content. See `assertJSONEqual` for
+        details on the equality check.
+        """
+
         write_golden: bool = bool(os.environ.get("WRITE_GOLDEN", ""))
 
         caller_path = Path(caller_frame(offset=1).f_code.co_filename).parent
@@ -91,6 +105,40 @@ class JSONTestCase(TestCase):
         skips=None,
         numeric_places: int = 7
     ) -> None:
+        """Assert equality between JSON-like `j1` and `j2`.
+        
+        The `path` argument is used to track the path to the current object in
+        the JSON structure. It is used to provide more informative error
+        messages in case of a mismatch. The `skips` argument is used to skip
+        certain keys in the comparison. The `numeric_places` argument is used to
+        specify the number of decimal places to compare for floating point
+        numbers.
+
+        Data types supported for comparison are:
+
+        - int
+        - float
+        - str
+        - dict
+        - list
+        - datetime
+        - dataclasses
+        - pydantic models
+        
+        Args:
+            j1: The first JSON-like object.
+
+            j2: The second JSON-like object.
+            
+            path: The path to the current object in the JSON structure.
+
+            skips: A set of keys to skip in the comparison.
+
+            numeric_places: The number of decimal places to compare for floating
+            point
+                numbers.
+        """
+
         skips = skips or set([])
         path = path or Lens()
 
