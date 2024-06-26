dfdd254f44039286fbb42591d06ca71826c4ba4c
David Kurokawa <david.kurokawa@snowflake.com>
2024-06-26 11:06:15 -0700

    Establish connection to Snowflake everytime during Cortex Search since it seems that the session can close out on its own. (#1259)

--- trulens_eval/examples/snowflake_demo/retrieve.py
+++ trulens_eval/examples/snowflake_demo/retrieve.py
@@ -1,11 +1,12 @@
 import json
 import os
-import streamlit as st
+
 from langchain_community.embeddings import HuggingFaceEmbeddings
 from langchain_community.vectorstores.pinecone import \
     Pinecone as PineconeVectorStore
 from snowflake.core import Root
 from snowflake.snowpark import Session
+import streamlit as st
 
 
 class PineconeRetriever:
@@ -33,6 +34,8 @@ class CortexSearchRetriever:
 
     def __init__(self, limit_to_retrieve: int = 4):
         self._limit_to_retrieve = limit_to_retrieve
+
+    def retrieve(self, query: str):
         connection_parameters = {
             "account": os.environ["SNOWFLAKE_ACCOUNT"],
             "user": os.environ["SNOWFLAKE_USER"],
@@ -40,23 +43,28 @@ class CortexSearchRetriever:
             "role": os.environ["SNOWFLAKE_ROLE"],
             "warehouse": os.environ["SNOWFLAKE_WAREHOUSE"],
         }
-        session = Session.builder.configs(connection_parameters).create()
-        root = Root(session)
-        self._cortex_search_service = root.databases[
-            os.environ["SNOWFLAKE_DATABASE"]].schemas[
-                os.environ["SNOWFLAKE_SCHEMA"]].cortex_search_services[
-                    os.environ["SNOWFLAKE_CORTEX_SEARCH_SERVICE"]]
-        print(os.environ["SNOWFLAKE_DATABASE"], os.environ["SNOWFLAKE_SCHEMA"], os.environ["SNOWFLAKE_CORTEX_SEARCH_SERVICE"])
-
-    def retrieve(self, query: str):
-        resp = self._cortex_search_service.search(
-            query=query,
-            columns=["doc_text"],
-            limit=self._limit_to_retrieve,
-        )
-        if resp.results:
-            return [curr["doc_text"] for curr in resp.results]
-        return []
+        session = None
+        try:
+            session = Session.builder.configs(connection_parameters).create()
+            root = Root(session)
+            cortex_search_service = root.databases[
+                os.environ["SNOWFLAKE_DATABASE"]].schemas[
+                    os.environ["SNOWFLAKE_SCHEMA"]].cortex_search_services[
+                        os.environ["SNOWFLAKE_CORTEX_SEARCH_SERVICE"]]
+            resp = cortex_search_service.search(
+                query=query,
+                columns=["doc_text"],
+                limit=self._limit_to_retrieve,
+            )
+            if resp.results:
+                return [curr["doc_text"] for curr in resp.results]
+            return []
+        finally:
+            try:
+                if session is not None:
+                    session.close()
+            except:
+                pass
 
 
 AVAILABLE_RETRIEVERS = [
@@ -73,4 +81,3 @@ def get_retriever(retriever_name: str):
         return CortexSearchRetriever()
     elif retriever_name == "Pinecone":
         return PineconeRetriever()
-    
