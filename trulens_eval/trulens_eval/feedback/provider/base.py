326b06a2b33e76a28d35f3232d529fabd3c69472
Piotr Mardziel <piotr.mardziel@snowflake.com>
2024-07-09 17:48:33 -0700

    format

--- trulens_eval/trulens_eval/feedback/provider/base.py
+++ trulens_eval/trulens_eval/feedback/provider/base.py
@@ -21,27 +21,31 @@ logger = logging.getLogger(__name__)
 class Provider(WithClassInfo, SerialModel):
     """Base Provider class.
     
-    TruLens makes use of *Feedback Providers* to generate evaluations of
-    large language model applications. These providers act as an access point
-    to different models, most commonly classification models and large language models.
+    TruLens makes use of *Feedback Providers* to generate evaluations of large
+    language model applications. These providers act as an access point to
+    different models, most commonly classification models and large language
+    models.
 
-    These models are then used to generate feedback on application outputs or intermediate
-    results.
+    These models are then used to generate feedback on application outputs or
+    intermediate results.
 
     `Provider` is the base class for all feedback providers. It is an abstract
-    class and should not be instantiated directly. Rather, it should be subclassed
-    and the subclass should implement the methods defined in this class.
+    class and should not be instantiated directly. Rather, it should be
+    subclassed and the subclass should implement the methods defined in this
+    class.
 
-    There are many feedback providers available in TruLens that grant access to a wide range 
-    of proprietary and open-source models.
+    There are many feedback providers available in TruLens that grant access to
+    a wide range of proprietary and open-source models.
 
-    Providers for classification and other non-LLM models should directly subclass `Provider`.
-    The feedback functions available for these providers are tied to specific providers, as they
-    rely on provider-specific endpoints to models that are tuned to a particular task.
+    Providers for classification and other non-LLM models should directly
+    subclass `Provider`. The feedback functions available for these providers
+    are tied to specific providers, as they rely on provider-specific endpoints
+    to models that are tuned to a particular task.
 
-    For example, the Huggingface feedback provider provides access to a number of classification models
-    for specific tasks, such as language detection. These models are than utilized by a feedback function
-    to generate an evaluation score.
+    For example, the Huggingface feedback provider provides access to a number
+    of classification models for specific tasks, such as language detection.
+    These models are than utilized by a feedback function to generate an
+    evaluation score.
 
     !!! example
 
@@ -51,14 +55,17 @@ class Provider(WithClassInfo, SerialModel):
         huggingface_provider.language_match(prompt, response)
         ```
 
-    Providers for LLM models should subclass `LLMProvider`, which itself subclasses `Provider`.
-    Providers for LLM-generated feedback are more of a plug-and-play variety. This means that the
-    base model of your choice can be combined with feedback-specific prompting to generate feedback.
+    Providers for LLM models should subclass `LLMProvider`, which itself
+    subclasses `Provider`. Providers for LLM-generated feedback are more of a
+    plug-and-play variety. This means that the base model of your choice can be
+    combined with feedback-specific prompting to generate feedback.
 
-    For example, `relevance` can be run with any base LLM feedback provider. Once the feedback provider
-    is instantiated with a base model, the `relevance` function can be called with a prompt and response.
+    For example, `relevance` can be run with any base LLM feedback provider.
+    Once the feedback provider is instantiated with a base model, the
+    `relevance` function can be called with a prompt and response.
 
-    This means that the base model selected is combined with specific prompting for `relevance` to generate feedback.
+    This means that the base model selected is combined with specific prompting
+    for `relevance` to generate feedback.
 
     !!! example
 
@@ -296,8 +303,8 @@ class LLMProvider(Provider):
         )
 
     def qs_relevance(self, question: str, context: str) -> float:
-        """
-        Question statement relevance is deprecated and will be removed in future versions. Please use context relevance in its place.
+        """Question statement relevance is deprecated and will be removed in
+        future versions. Please use context relevance in its place.
         """
 
         warnings.warn(
@@ -1195,8 +1202,9 @@ class LLMProvider(Provider):
                 .on(context.collect() .on_output() )
             ```
         Args:
-            source: The source that should support the statement. statement: The
-            statement to check groundedness.
+            source: The source that should support the statement.
+            
+            statement: The statement to check groundedness.
 
         Returns:
             Tuple[float, dict]: A tuple containing a value between 0.0 (not
