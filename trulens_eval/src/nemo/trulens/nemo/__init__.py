fc591f82201e2fbd80f79b6d31c36fb380d9b299
corey <corey.hu@snowflake.com>
2024-07-24 10:42:37 -0700

    add nemo/trurails subpackage

---
+++ trulens_eval/src/nemo/trulens/nemo/__init__.py
@@ -0,0 +1,9 @@
+from trulens.nemo.tru_rails import RailsActionSelect
+from trulens.nemo.tru_rails import RailsInstrument
+from trulens.nemo.tru_rails import TruRails
+
+__all__ = [
+    'TruRails',
+    'RailsInstrument',
+    'RailsActionSelect'
+]
