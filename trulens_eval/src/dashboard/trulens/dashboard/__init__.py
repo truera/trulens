b6159e638822336fd7360458b5ce7bd31f6c9844
corey <corey.hu@snowflake.com>
2024-07-24 10:43:54 -0700

    factor out get_feedback_result

---
+++ trulens_eval/src/dashboard/trulens/dashboard/__init__.py
@@ -0,0 +1,7 @@
+from trulens.dashboard.run import run_dashboard
+from trulens.dashboard.run import stop_dashboard
+
+__all__ = [
+    'run_dashboard',
+    'stop_dashboard',
+]
--- trulens_eval/src/dashboard/trulens/dashboard/display.py
+++ trulens_eval/src/dashboard/trulens/dashboard/display.py
@@ -6,53 +6,6 @@ from trulens.core.schema.record import Record
 from trulens.dashboard.ux.styles import CATEGORY
 
 
-def get_feedback_result(
-    tru_record: Record, feedback_name: str, timeout: int = 60
-) -> pd.DataFrame:
-    """
-    Retrieve the feedback results for a given feedback name from a TruLens record.
-
-    Args:
-        tru_record (Record): The record containing feedback and future results.
-        feedback_name (str): The name of the feedback to retrieve results for.
-
-    Returns:
-        pd.DataFrame: A DataFrame containing the feedback results. If no feedback
-                      results are found, an empty DataFrame is returned.
-    """
-    start_time = time.time()
-    feedback_calls = None
-
-    while time.time() - start_time < timeout:
-        feedback_calls = next(
-            (
-                future_result.result()
-                for feedback_definition, future_result in
-                tru_record.feedback_and_future_results
-                if feedback_definition.name == feedback_name
-            ), None
-        )
-        if feedback_calls is not None:
-            break
-        time.sleep(1)  # Wait for 1 second before checking again
-
-    if feedback_calls is None:
-        raise TimeoutError(
-            f"Feedback for '{feedback_name}' not available within {timeout} seconds."
-        )
-
-    # Ensure feedback_calls is iterable
-    if not hasattr(feedback_calls, '__iter__'):
-        raise ValueError('feedback_calls is not iterable')
-
-    feedback_result = [
-        {
-            **call.model_dump()['args'], 'ret': call.model_dump()['ret']
-        } for call in feedback_calls.calls
-    ]
-    return pd.DataFrame(feedback_result)
-
-
 def get_icon(fdef: FeedbackDefinition, result: float) -> str:
     """
     Get the icon for a given feedback definition and result.
