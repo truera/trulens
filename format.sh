7d8b2e967e8977156be08cff1abe97857f530d37
corey <corey.hu@snowflake.com>
2024-07-30 15:08:19 -0700

    setup precommit ruff formatter and linter

--- format.sh
+++ format.sh
@@ -2,15 +2,15 @@
 if [ $# -eq 0 ] ; then
     FORMAT_PATH=.
 elif [ $1 = "--explain" ]; then
-    FORMAT_PATH=./trulens_explain
+    FORMAT_PATH=./trulens_explain/*
 elif [ $1 = "--eval" ]; then
-    FORMAT_PATH=./trulens_eval
+    FORMAT_PATH=./trulens_eval/*
 else
     echo "Got invalid flag $1"
     exit 1
 fi
 
 echo "Sorting imports in $FORMAT_PATH"
-isort $FORMAT_PATH -s .conda -s trulens_eval/.conda
+ruff check --fix $FORMAT_PATH
 echo "Formatting $FORMAT_PATH"
-yapf --style .style.yapf -r -i --verbose --parallel -r -i $FORMAT_PATH -e .conda -e trulens_eval/.conda
+ruff format $FORMAT_PATH
