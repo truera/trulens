b543e9e37fab290fe655dffa36165d92eb31b09f
Josh Reini <joshua.reini@snowflake.com>
2024-08-01 12:19:28 -0400

    update tests

--- trulens_eval/tests/unit/test_embedding_feedback.py
+++ trulens_eval/tests/unit/test_embedding_feedback.py
@@ -19,43 +19,49 @@ class TestEmbeddings(unittest.TestCase):
         self.embeddings = Embeddings(embed_model=self.embed_model)
 
     def test_cosine_distance_type(self):
-        assert type(
-            self.embeddings.cosine_distance("apple", "banana")
-        ) == np.float64
+        self.assertIsInstance(
+            self.embeddings.cosine_distance("apple", "banana"), np.float64
+        )
 
     def test_cosine_distance_order(self):
-        assert self.embeddings.cosine_distance(
-            "apple", "john f kennedy"
-        ) > self.embeddings.cosine_distance("apple", "banana")
-        assert self.embeddings.cosine_distance(
-            "apple", "banana"
-        ) > self.embeddings.cosine_distance("apple", "apple")
+        self.assertGreater(
+            self.embeddings.cosine_distance("apple", "john f kennedy"),
+            self.embeddings.cosine_distance("apple", "banana")
+        )
+        self.assertGreater(
+            self.embeddings.cosine_distance("apple", "banana"),
+            self.embeddings.cosine_distance("apple", "apple")
+        )
 
     def test_manhattan_distance_type(self):
-        assert type(
-            self.embeddings.manhattan_distance("apple", "banana")
-        ) == np.float64
+        self.assertIsInstance(
+            self.embeddings.manhattan_distance("apple", "banana"), np.float64
+        )
 
     def test_manhattan_distance_order(self):
-        assert self.embeddings.manhattan_distance(
-            "apple", "john f kennedy"
-        ) > self.embeddings.manhattan_distance("apple", "banana")
-        assert self.embeddings.manhattan_distance(
-            "apple", "banana"
-        ) > self.embeddings.manhattan_distance("apple", "apple")
+        self.assertGreater(
+            self.embeddings.manhattan_distance("apple", "john f kennedy"),
+            self.embeddings.manhattan_distance("apple", "banana")
+        )
+        self.assertGreater(
+            self.embeddings.manhattan_distance("apple", "banana"),
+            self.embeddings.manhattan_distance("apple", "apple")
+        )
 
     def test_euclidean_distance_type(self):
-        assert type(
-            self.embeddings.euclidean_distance("apple", "banana")
-        ) == np.float64
+        self.assertIsInstance(
+            self.embeddings.euclidean_distance("apple", "banana"), np.float64
+        )
 
     def test_euclidean_distance_order(self):
-        assert self.embeddings.euclidean_distance(
-            "apple", "john f kennedy"
-        ) > self.embeddings.euclidean_distance("apple", "banana")
-        assert self.embeddings.euclidean_distance(
-            "apple", "banana"
-        ) > self.embeddings.euclidean_distance("apple", "apple")
+        self.assertGreater(
+            self.embeddings.euclidean_distance("apple", "john f kennedy"),
+            self.embeddings.euclidean_distance("apple", "banana")
+        )
+        self.assertGreater(
+            self.embeddings.euclidean_distance("apple", "banana"),
+            self.embeddings.euclidean_distance("apple", "apple")
+        )
 
 
 if __name__ == "__main__":
