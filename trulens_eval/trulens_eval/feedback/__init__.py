9460432cb656e6d9444f992f8cce8008f1ed1d66
Daniel <daniel.huang@snowflake.com>
2024-08-01 19:02:11 -0700

    import

--- trulens_eval/trulens_eval/feedback/__init__.py
+++ trulens_eval/trulens_eval/feedback/__init__.py
@@ -2,8 +2,10 @@
 # Main class holding and running feedback functions:
 from trulens_eval.feedback import feedback as mod_feedback
 from trulens_eval.feedback.embeddings import Embeddings
-from trulens_eval.feedback.groundtruth import BenchmarkAggregator
-from trulens_eval.feedback.groundtruth import GroundTruthAgreement
+from trulens_eval.feedback.groundtruth import (
+    GroundTruthAgreement,
+    BenchmarkAggregator,
+)
 
 # Providers of feedback functions evaluation:
 from trulens_eval.feedback.provider.hugs import Huggingface
