ad794aba316ec03d6c784a85637de23e0b7628d8
corey <corey.hu@snowflake.com>
2024-08-09 17:15:58 -0700

    fail if npm subcommands fail

--- src/dashboard/setup.py
+++ src/dashboard/setup.py
@@ -10,16 +10,35 @@ python -m build
 """
 
 import os
+import subprocess
 
 from setuptools import setup
 
 
 def build_record_viewer():
     if os.path.exists("react_components/record_viewer"):
-        print("running npm i")
-        os.system("npm i --prefix react_components/record_viewer")
+        try:
+            print("running npm i")
+            subprocess.check_call(
+                [
+                    "npm",
+                    "i",
+                    "--prefix",
+                    "react_components/record_viewer",
+                ]
+            )
+        except FileNotFoundError:
+            print("`npm` is not installed. Skipping build")
         print("running npm run build")
-        os.system("npm run --prefix react_components/record_viewer build")
+        subprocess.check_call(
+            [
+                "npm",
+                "run",
+                "--prefix",
+                "react_components/record_viewer",
+                "build",
+            ]
+        )
 
 
 if __name__ == "__main__":
