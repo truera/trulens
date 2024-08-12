d3cbf53120f1ace9b7d336897c542d1e8aa7b6bc
corey <corey.hu@snowflake.com>
2024-08-12 11:21:17 -0700

    raise original error through subprocess.check_call

--- src/dashboard/setup.py
+++ src/dashboard/setup.py
@@ -17,18 +17,15 @@ from setuptools import setup
 
 def build_record_viewer():
     if os.path.exists("react_components/record_viewer"):
-        try:
-            print("running npm i")
-            subprocess.check_call(
-                [
-                    "npm",
-                    "i",
-                    "--prefix",
-                    "react_components/record_viewer",
-                ]
-            )
-        except FileNotFoundError:
-            print("`npm` is not installed. Skipping build")
+        print("running npm i")
+        subprocess.check_call(
+            [
+                "npm",
+                "i",
+                "--prefix",
+                "react_components/record_viewer",
+            ]
+        )
         print("running npm run build")
         subprocess.check_call(
             [
