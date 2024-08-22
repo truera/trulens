c517908b8d66ef11a0d960c6f6c677d444341553
Piotr Mardziel <piotr.mardziel@snowflake.com>
2024-08-21 19:45:07 -0700

    revert

--- src/core/trulens/core/feedback/__init__.py
+++ src/core/trulens/core/feedback/__init__.py
@@ -1,6 +1,6 @@
 # TODO: No aliases in production API.
 
-"""
+
 from trulens.core.feedback.endpoint import Endpoint
 from trulens.core.feedback.endpoint import EndpointCallback
 from trulens.core.feedback.feedback import Feedback
@@ -16,4 +16,3 @@ __all__ = [
     "EndpointCallback",
     "SkipEval",
 ]
-"""
