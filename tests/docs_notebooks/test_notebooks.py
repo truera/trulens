6b7dbd05267b582a35de2e4cee4b789245448f87
corey <corey.hu@snowflake.com>
2024-08-22 08:29:12 -0700

    more references

--- tests/docs_notebooks/test_notebooks.py
+++ tests/docs_notebooks/test_notebooks.py
@@ -55,7 +55,7 @@ class DBMigrationPreprocessor(VariableSettingPreprocessor):
         if "TruSession()" in cell["source"]:
             cell["source"] = (
                 cell["source"]
-                + "\nfrom trulens.core import Tru\ntru=TruSession()\ntru.migrate_database()\n"
+                + "\nfrom trulens.core import TruSession\ntru=TruSession()\ntru.migrate_database()\n"
                 + "\nfrom trulens.core.database.migrations.data import _sql_alchemy_serialization_asserts\n_sql_alchemy_serialization_asserts(tru.connector.db)\n"
             )
         ret = super().preprocess_cell(cell, resources, index, **kwargs)
