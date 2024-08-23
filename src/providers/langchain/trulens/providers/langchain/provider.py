4c90356217bb8171b573a22d89145f65b1f5db24
Josh Reini <josh.reini@snowflake.com>
2024-08-22 20:48:40 -0400

    predict/predict_messages -> invoke

--- src/providers/langchain/trulens/providers/langchain/provider.py
+++ src/providers/langchain/trulens/providers/langchain/provider.py
@@ -62,11 +62,11 @@ class Langchain(LLMProvider):
         **kwargs,
     ) -> str:
         if prompt is not None:
-            predict = self.endpoint.chain.predict(prompt, **kwargs)
+            predict = self.endpoint.chain.invoke(prompt, **kwargs)
 
         elif messages is not None:
             messages = [_convert_message(message) for message in messages]
-            predict = self.endpoint.chain.predict_messages(
+            predict = self.endpoint.chain.invoke(
                 messages, **kwargs
             ).content
 
