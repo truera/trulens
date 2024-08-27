5c8b4a5355d2bdd12aba46e0bdfa70a6d1908894
corey <corey.hu@snowflake.com>
2024-08-27 12:22:56 -0700

    test fixes

--- tests/unit/test_app.py
+++ tests/unit/test_app.py
@@ -10,28 +10,50 @@ from trulens.core.schema.app import AppDefinition
 
 
 class TestApp(TestCase):
-    def test_app_id(self):
-        # Most naive test to make sure the basic app runs at all.
-        app_1 = AppDefinition(app_name="test_application", app_version="v1")
-        app_2 = AppDefinition(app_name="test_application", app_version="v1")
+    def setUp(self):
+        self.session = TruSession()
+        self.session.reset_database()
 
-        self.assertEqual(app_1.app_id, app_2.app_id)
-
-        session = TruSession()
-        self.assertEquals(len(session.get_apps()), 1)
-
-        session.add_app(app_1)
-        self.assertEquals(len(session.get_apps()), 1)
-
-        AppDefinition(
-            app_name="test_application", app_version="v1", app_id=app_2.app_id
+        self.app_1 = AppDefinition(
+            app_name="test_application",
+            app_version="v1",
+            root_class={"name": "App", "module": {"module_name": "app"}},
+            app={},
         )
-        self.assertEquals(len(session.get_apps()), 1)
+        self.app_2 = AppDefinition(
+            app_name="test_application",
+            app_version="v1",
+            root_class={"name": "App", "module": {"module_name": "app"}},
+            app={},
+        )
+
+    def test_deterministic_app_id(self):
+        # Most naive test to make sure the basic app runs at all.
+        self.assertEqual(self.app_1.app_id, self.app_2.app_id)
+
+    def test_db_primary_key(self):
+        self.session.add_app(self.app_1)
+        self.assertEqual(len(self.session.get_apps()), 1)
+
+        self.session.add_app(self.app_2)
+        self.assertEqual(len(self.session.get_apps()), 1)
+
+    def test_app_id_override(self):
+        AppDefinition(
+            app_name="test_application",
+            app_version="v1",
+            app_id=self.app_2.app_id,
+            root_class={"name": "App", "module": {"module_name": "app"}},
+            app={},
+        )
+
         with self.assertRaises(ValueError):
             AppDefinition(
                 app_name="test_application",
                 app_version="v1",
                 app_id="invalid_app_id",
+                root_class={"name": "App", "module": {"module_name": "app"}},
+                app={},
             )
 
 
