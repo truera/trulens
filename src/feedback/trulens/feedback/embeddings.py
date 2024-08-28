f00f1789567e5d5d3c9e4a4a4d777ef6827d2018
corey <corey.hu@snowflake.com>
2024-08-28 16:30:30 -0700

    assert installed

--- src/feedback/trulens/feedback/embeddings.py
+++ src/feedback/trulens/feedback/embeddings.py
@@ -10,15 +10,19 @@ with OptionalImports(
     messages=format_import_errors(
         "scikit-learn", purpose="using embedding vector distances"
     )
-):
+) as opt:
     import sklearn.metrics
+opt.assert_installed(sklearn.metrics)
+
 
 with OptionalImports(
     messages=format_import_errors(
         "llama-index", purpose="instrumenting LlamaIndex apps"
     )
-):
+) as opt:
+    import llama_index.core.base.embeddings.base
     from llama_index.core.base.embeddings.base import BaseEmbedding
+opt.assert_installed(llama_index.core.base.embeddings.base)
 
 
 class Embeddings(WithClassInfo, SerialModel):
