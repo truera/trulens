ce5c48fb2ec31da978713318c9b9fd1cdf9f6a52
corey hu <corey.hu@snowflake.com>
2024-08-29 16:59:55 -0700

    codespell and spellcheck fixes (#1426)

--- examples/dev/__init__.py
+++ examples/dev/__init__.py
@@ -2,6 +2,6 @@
 
 These are meant to aid in trulens development.
 
-- dummy_app: Dummy app with dummy components to simulate behaviour of a real app
+- dummy_app: Dummy app with dummy components to simulate behavior of a real app
   but without making any remote api calls.
 """
