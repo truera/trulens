4738610174eb4ae64b32622bafcd6ad2b6eddd13
corey <corey.hu@snowflake.com>
2024-09-09 14:14:42 -0500

    fix compat

--- src/trulens_eval/trulens_eval/streamlit.py
+++ src/trulens_eval/trulens_eval/streamlit.py
@@ -10,7 +10,7 @@ from trulens.core.utils import deprecation as deprecation_utils
 deprecation_utils.packages_dep_warn()
 
 from trulens.core.database.legacy.migration import MIGRATION_UNKNOWN_STR
-from trulens.dashboard.Leaderboard import render_leaderboard
 from trulens.dashboard.streamlit import FeedbackDisplay
 from trulens.dashboard.streamlit import trulens_feedback
+from trulens.dashboard.streamlit import trulens_leaderboard
 from trulens.dashboard.streamlit import trulens_trace
