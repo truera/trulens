9c7533963c8aab83708244c6ccc036049a688e0a
Josh Reini <joshua.reini@snowflake.com>
2024-09-27 15:09:22 -0400

    don't normalize the score twice

--- src/feedback/trulens/feedback/llm_provider.py
+++ src/feedback/trulens/feedback/llm_provider.py
@@ -1615,10 +1615,6 @@ class LLMProvider(Provider):
                 temperature=temperature,
             )
 
-            normalized_score = (score - min_score_val) / (
-                max_score_val - min_score_val
-            )
-
             score_pattern = re.compile(r"Score:\s*([0-9.]+)")
             match = score_pattern.search(reason["reason"])
             if match:
@@ -1635,7 +1631,7 @@ class LLMProvider(Provider):
                     "reason"
                 ].replace(original_string, replacement_string)
 
-            return index, normalized_score, normalized_reason
+            return index, score, normalized_reason
 
         results = []
 
@@ -1659,12 +1655,16 @@ class LLMProvider(Provider):
             )
             reasons_str += f"STATEMENT {i}:\n{reason_str}\n"
 
+        print("groundedness scores \n")
+        print(list(groundedness_scores.values()))
+
         # Calculate the average groundedness score from the scores dictionary
         average_groundedness_score = float(
             np.mean(list(groundedness_scores.values()))
         )
+        print(average_groundedness_score)
 
-        print(reasons_str)
+        print(f"\n {reasons_str}")
 
         return average_groundedness_score, {"reasons": reasons_str}
 
