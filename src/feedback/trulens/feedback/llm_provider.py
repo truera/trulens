77155d7aa2aef81d99feea548f3a001823b354ad
Josh Reini <joshua.reini@snowflake.com>
2024-09-27 15:09:58 -0400

    remove prints

--- src/feedback/trulens/feedback/llm_provider.py
+++ src/feedback/trulens/feedback/llm_provider.py
@@ -1655,16 +1655,10 @@ class LLMProvider(Provider):
             )
             reasons_str += f"STATEMENT {i}:\n{reason_str}\n"
 
-        print("groundedness scores \n")
-        print(list(groundedness_scores.values()))
-
         # Calculate the average groundedness score from the scores dictionary
         average_groundedness_score = float(
             np.mean(list(groundedness_scores.values()))
         )
-        print(average_groundedness_score)
-
-        print(f"\n {reasons_str}")
 
         return average_groundedness_score, {"reasons": reasons_str}
 
