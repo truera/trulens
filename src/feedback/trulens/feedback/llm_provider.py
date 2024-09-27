681e303ca87d0a12c0b3425fb941a598b5f15445
Josh Reini <joshua.reini@snowflake.com>
2024-09-27 15:02:16 -0400

    normalize groundedness reasons

--- src/feedback/trulens/feedback/llm_provider.py
+++ src/feedback/trulens/feedback/llm_provider.py
@@ -1615,9 +1615,12 @@ class LLMProvider(Provider):
                 temperature=temperature,
             )
 
+            normalized_score = (score - min_score_val) / (
+                max_score_val - min_score_val
+            )
+
             score_pattern = re.compile(r"Score:\s*([0-9.]+)")
             match = score_pattern.search(reason["reason"])
-            normalized_reason = None
             if match:
                 original_reason_score = float(match.group(1))
                 normalized_reason_score = (
@@ -1632,7 +1635,7 @@ class LLMProvider(Provider):
                     "reason"
                 ].replace(original_string, replacement_string)
 
-            return index, score, normalized_reason
+            return index, normalized_score, normalized_reason
 
         results = []
 
@@ -1660,6 +1663,8 @@ class LLMProvider(Provider):
         average_groundedness_score = float(
             np.mean(list(groundedness_scores.values()))
         )
+
+        print(reasons_str)
 
         return average_groundedness_score, {"reasons": reasons_str}
 
