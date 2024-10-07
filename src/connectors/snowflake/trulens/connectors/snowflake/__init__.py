8858c3a8e244ebbb3347568c82998938c0d7fccd
Piotr Mardziel <piotr.mardziel@snowflake.com>
2024-10-07 15:55:49 -0700

    revert init

--- src/connectors/snowflake/trulens/connectors/snowflake/__init__.py
+++ src/connectors/snowflake/trulens/connectors/snowflake/__init__.py
@@ -8,10 +8,17 @@
     ```
 """
 
+# NOTE: This file does not follow the no-init aliases import standard.
+
 from importlib.metadata import version
 
+from trulens.connectors.snowflake.connector import SnowflakeConnector
 from trulens.core.utils import imports as import_utils
 
 __version__ = version(
     import_utils.safe_importlib_package_name(__package__ or __name__)
 )
+
+__all__ = [
+    "SnowflakeConnector",
+]
