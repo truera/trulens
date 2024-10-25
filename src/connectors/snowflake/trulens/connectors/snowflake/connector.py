2891ce5617d50ec4c580479180273c970394d5e7
David Kurokawa <david.kurokawa@snowflake.com>
2024-10-25 11:43:53 -0700

    Fix bad merge for snowflake connector. (#1588)

--- src/connectors/snowflake/trulens/connectors/snowflake/connector.py
+++ src/connectors/snowflake/trulens/connectors/snowflake/connector.py
@@ -74,7 +74,7 @@ class SnowflakeConnector(DBConnector):
             database_prefix,
             database_args,
             database_check_revision,
-            connection_parameters=connection_parameters,
+            URL(**connection_parameters),
         )
 
     def _create_snowpark_session(
