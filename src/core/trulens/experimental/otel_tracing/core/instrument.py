977c2918b964d7c483028780012fcf3c639996a2
Garett Tok Ern Liang <garett.tok@snowflake.com>
2024-12-11 16:54:42 -0800

    update

--- src/core/trulens/experimental/otel_tracing/core/instrument.py
+++ src/core/trulens/experimental/otel_tracing/core/instrument.py
@@ -1,6 +1,6 @@
 from functools import wraps
 import logging
-from typing import Any, Callable, Union
+from typing import Any, Callable, Optional, Union
 
 from opentelemetry import trace
 from trulens.apps.custom import instrument as custom_instrument
@@ -12,12 +12,12 @@ from trulens.experimental.otel_tracing.core.semantic import (
 
 logger = logging.getLogger(__name__)
 
-type Attributes = Union[
-    dict[str, Any], Callable[[Any, Any, Any], dict[str, Any]]
+type Attributes = Optional[
+    Union[dict[str, Any], Callable[[Any, Any, Any], dict[str, Any]]]
 ]
 
 
-def instrument(func: Callable, attributes: Attributes = {}):
+def instrument(attributes: Attributes = {}):
     """
     Decorator for marking functions to be instrumented in custom classes that are
     wrapped by TruCustomApp, with OpenTelemetry tracing.
@@ -36,37 +36,47 @@ def instrument(func: Callable, attributes: Attributes = {}):
         # TODO: validate OTEL attributes.
         # TODO: validate span type attributes.
 
-    @wraps(func)
-    def wrapper(*args, **kwargs):
-        with (
-            trace.get_tracer_provider()
-            .get_tracer(TRULENS_SERVICE_NAME)
-            .start_as_current_span(
-                name=func.__name__,
-            )
-        ) as span:
-            _instrumented_object, *rest_args = args
+    def inner_decorator(func: Callable):
+        @wraps(func)
+        def wrapper(*args, **kwargs):
+            with (
+                trace.get_tracer_provider()
+                .get_tracer(TRULENS_SERVICE_NAME)
+                .start_as_current_span(
+                    name=func.__name__,
+                )
+            ):
+                span = trace.get_current_span()
+                _instrumented_object, *rest_args = args
 
-            # ? Do we have to validate the args/kwargs and make sure they are serializable?
-            span.set_attribute("function", func.__name__)
-            span.set_attribute("args", json_utils.json_str_of_obj(rest_args))
-            ret = custom_instrument(func)(*args, **kwargs)
-            span.set_attribute("return", ret)
+                # ? Do we have to validate the args/kwargs and make sure they are serializable?
+                span.set_attribute("function", func.__name__)
+                span.set_attribute(
+                    "args", json_utils.json_str_of_obj(rest_args)
+                )
+                ret = custom_instrument(func)(*args, **kwargs)
+                span.set_attribute("return", ret)
 
-            attributes_to_add = {}
+                attributes_to_add = {}
 
-            # Set the user provider attributes.
-            if attributes:
-                if callable(attributes):
-                    attributes_to_add = attributes(ret, *args, **kwargs)
-                else:
-                    attributes_to_add = attributes
+                # Set the user provider attributes.
+                if attributes:
+                    if callable(attributes):
+                        attributes_to_add = attributes(
+                            ret, *rest_args, **kwargs
+                        )
+                    else:
+                        attributes_to_add = attributes
 
-            _validate_attributes(attributes_to_add)
+                logger.info(f"Attributes to add: {attributes_to_add}")
 
-            for key, value in attributes_to_add.items():
-                span.set_attribute(key, value)
+                _validate_attributes(attributes_to_add)
 
-            return ret
+                for key, value in attributes_to_add.items():
+                    span.set_attribute(key, value)
 
-    return wrapper
+                return ret
+
+        return wrapper
+
+    return inner_decorator
