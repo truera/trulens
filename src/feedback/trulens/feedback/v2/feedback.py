469af4148a6e70840be0e593b7190d9a082e0a50
Daniel <daniel.huang@snowflake.com>
2024-12-16 11:06:41 -0800

    save progress - optimizing context relevance cortex prompt

--- src/feedback/trulens/feedback/v2/feedback.py
+++ src/feedback/trulens/feedback/v2/feedback.py
@@ -399,6 +399,24 @@ class Trivial(Semantics, WithPrompt):
     )
 
 
+adalflow_v2_context_relevance = """You are a RELEVANCE grader; providing the relevance of the given CONTEXT to the given QUESTION.
+Respond only as a number from 0 to 3, where 0 is the lowest score according to the criteria and 3 is the highest possible score.
+
+Criteria for evaluating relevance:
+- CONTEXT that is IRRELEVANT to the QUESTION should score 0.
+- CONTEXT that is RELEVANT to some of the QUESTION should get an intermediate score.
+- CONTEXT that is RELEVANT to most of the QUESTION should get a score closer to 3.
+- CONTEXT that is RELEVANT to the entirety of the QUESTION should get a score of 3, which is the full mark.
+- CONTEXT must be relevant and helpful for answering the entire QUESTION to get a score of 3.
+
+A few additional scoring guidelines:
+- Long CONTEXTS should score equally well as short CONTEXTS.
+- RELEVANCE score should increase as the CONTEXT provides more RELEVANT context to the QUESTION.
+- RELEVANCE score should increase as the CONTEXT provides RELEVANT context to more parts of the QUESTION.
+- Never elaborate. Only provide the score.",
+"""
+
+
 @dataclass
 class ContextRelevance(Relevance, WithPrompt, CriteriaOutputSpaceMixin):
     output_space_prompt: ClassVar[str] = LIKERT_0_3_PROMPT
@@ -422,11 +440,13 @@ class ContextRelevance(Relevance, WithPrompt, CriteriaOutputSpaceMixin):
 
         - Long CONTEXTS should score equally well as short CONTEXTS.
 
-        - RELEVANCE score should increase as the CONTEXTS provides more RELEVANT context to the QUESTION.
+        - RELEVANCE score should increase as the CONTEXT provides more RELEVANT context to the QUESTION.
 
-        - RELEVANCE score should increase as the CONTEXTS provides RELEVANT context to more parts of the QUESTION.
+        - RELEVANCE score should increase as the CONTEXT provides RELEVANT context to more parts of the QUESTION.
 
         - Never elaborate.
+
+        -
         """
     )
 
