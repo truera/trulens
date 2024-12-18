7fd7de277f017efb86a2817073e7dd67ee2f996a
Garett Tok Ern Liang <garett.tok@snowflake.com>
2024-12-18 17:16:51 -0500

    save

--- src/core/trulens/experimental/otel_tracing/core/exporter.py
+++ src/core/trulens/experimental/otel_tracing/core/exporter.py
@@ -1,6 +1,6 @@
 from datetime import datetime
 import logging
-from typing import Sequence
+from typing import Optional, Sequence
 
 from opentelemetry.sdk.trace import ReadableSpan
 from opentelemetry.sdk.trace.export import SpanExporter
@@ -9,6 +9,13 @@ from trulens.core.database import connector as core_connector
 from trulens.core.schema import event as event_schema
 
 logger = logging.getLogger(__name__)
+
+
+def to_timestamp(timestamp: Optional[int]) -> datetime:
+    if timestamp:
+        return datetime.fromtimestamp(timestamp * 1e-9)
+
+    return datetime.now()
 
 
 class TruLensDBSpanExporter(SpanExporter):
@@ -21,46 +28,34 @@ class TruLensDBSpanExporter(SpanExporter):
     def __init__(self, connector: core_connector.DBConnector):
         self.connector = connector
 
+    def _construct_event(self, span: ReadableSpan) -> event_schema.Event:
+        context = span.get_span_context()
+        parent = span.parent
+
+        if context is None:
+            raise ValueError("Span context is None")
+
+        return event_schema.Event(
+            record=span.attributes,
+            record_attributes={},
+            record_type=event_schema.EventRecordType.SPAN,
+            resource_attributes=span.resource.attributes,
+            start_timestamp=to_timestamp(span.start_time),
+            timestamp=to_timestamp(span.end_time),
+            trace={
+                "span_id": str(context.span_id),
+                "trace_id": str(context.trace_id),
+                "parent_id": str(parent.span_id if parent else ""),
+            },
+        )
+
     def export(self, spans: Sequence[ReadableSpan]) -> SpanExportResult:
         try:
-            for span in spans:
-                context = span.get_span_context()
-                parent = span.parent
-
-                if context is None:
-                    logger.error(
-                        "Error exporting spans to the database: Span context is None"
-                    )
-                    return SpanExportResult.FAILURE
-
-                event = event_schema.Event(
-                    event_id=str(context.span_id),
-                    record=span.attributes,
-                    record_attributes=span.attributes,
-                    record_type="SPAN",
-                    resource_attributes=span.resource.attributes,
-                    start_timestamp=datetime.fromtimestamp(
-                        span.start_time / pow(10, 9)
-                    )
-                    if span.start_time
-                    else datetime.now(),
-                    timestamp=datetime.fromtimestamp(span.end_time / pow(10, 9))
-                    if span.end_time
-                    else datetime.now(),
-                    trace={
-                        "trace_id": str(context.trace_id),
-                        "parent_id": str(parent.span_id if parent else ""),
-                    },
-                )
-                self.connector.add_event(event)
+            events = list(map(self._construct_event, spans))
+            self.connector.add_events(events)
 
         except Exception as e:
             logger.error("Error exporting spans to the database: %s", e)
             return SpanExportResult.FAILURE
 
         return SpanExportResult.SUCCESS
-
-    """Immediately export all spans"""
-
-    def force_flush(self, timeout_millis: int = 30000) -> bool:
-        return True
