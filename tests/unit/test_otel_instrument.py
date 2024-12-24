5db8a521a09798f969d3b75a3072bd81c3ce18e7
David Kurokawa <david.kurokawa@snowflake.com>
2024-12-23 20:49:52 -0800

    Refactor tests.

--- tests/unit/test_otel_instrument.py
+++ tests/unit/test_otel_instrument.py
@@ -4,18 +4,15 @@ Tests for OTEL instrument decorator.
 
 from unittest import main
 
-import pandas as pd
-import sqlalchemy as sa
 from trulens.apps.custom import TruCustomApp
-from trulens.core.schema.event import EventRecordType
 from trulens.core.session import TruSession
 from trulens.experimental.otel_tracing.core.init import init
 from trulens.experimental.otel_tracing.core.instrument import instrument
 
-from tests.test import TruTestCase
 from tests.util.df_comparison import (
     compare_dfs_accounting_for_ids_and_timestamps,
 )
+from tests.util.otel_test_case import OtelTestCase
 
 
 class _TestApp:
@@ -57,58 +54,7 @@ class _TestApp:
         return "nested3"
 
 
-class TestOtelInstrument(TruTestCase):
-    @classmethod
-    def clear_TruSession_singleton(cls) -> None:
-        # [HACK!] Clean up any instances of `TruSession` so tests don't
-        # interfere with each other.
-        for key in [
-            curr
-            for curr in TruSession._singleton_instances
-            if curr[0] == "trulens.core.session.TruSession"
-        ]:
-            del TruSession._singleton_instances[key]
-
-    @classmethod
-    def setUpClass(cls) -> None:
-        cls.clear_TruSession_singleton()
-        tru_session = TruSession()
-        tru_session.experimental_enable_feature("otel_tracing")
-        return super().setUpClass()
-
-    @classmethod
-    def tearDownClass(cls) -> None:
-        cls.clear_TruSession_singleton()
-        return super().tearDownClass()
-
-    @staticmethod
-    def _get_events() -> pd.DataFrame:
-        tru_session = TruSession()
-        db = tru_session.connector.db
-        with db.session.begin() as db_session:
-            q = sa.select(db.orm.Event).order_by(db.orm.Event.start_timestamp)
-            return pd.read_sql(q, db_session.bind)
-
-    @staticmethod
-    def _convert_column_types(df: pd.DataFrame) -> None:
-        # Writing to CSV and the reading back causes some type issues so we
-        # hackily convert things here.
-        df["event_id"] = df["event_id"].apply(str)
-        df["record_type"] = df["record_type"].apply(
-            lambda x: EventRecordType(x[len("EventRecordType.") :])
-            if x.startswith("EventRecordType.")
-            else EventRecordType(x)
-        )
-        df["start_timestamp"] = df["start_timestamp"].apply(pd.Timestamp)
-        df["timestamp"] = df["timestamp"].apply(pd.Timestamp)
-        for json_column in [
-            "record",
-            "record_attributes",
-            "resource_attributes",
-            "trace",
-        ]:
-            df[json_column] = df[json_column].apply(lambda x: eval(x))
-
+class TestOtelInstrument(OtelTestCase):
     def test_instrument_decorator(self) -> None:
         # Set up.
         tru_session = TruSession()
@@ -123,11 +69,11 @@ class TestOtelInstrument(TruTestCase):
             test_app.respond_to_query("throw")
         # Compare results to expected.
         GOLDEN_FILENAME = "tests/unit/static/golden/test_otel_instrument__test_instrument_decorator.csv"
-        actual = self._get_events()
+        actual = self.get_events()
         self.assertEqual(len(actual), 8)
         self.write_golden(GOLDEN_FILENAME, actual)
         expected = self.load_golden(GOLDEN_FILENAME)
-        self._convert_column_types(expected)
+        self.convert_column_types(expected)
         compare_dfs_accounting_for_ids_and_timestamps(
             self,
             expected,
