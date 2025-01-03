929187fe5cd8074c73f6b12cbabad43e076e1bcd
Garett Tok Ern Liang <garett.tok@snowflake.com>
2025-01-03 14:59:48 -0500

    move things out of app.py

--- src/core/trulens/core/app.py
+++ src/core/trulens/core/app.py
@@ -3,6 +3,7 @@ from __future__ import annotations
 from abc import ABC
 from abc import ABCMeta
 from abc import abstractmethod
+import contextlib
 import contextvars
 import datetime
 import inspect
@@ -53,6 +54,7 @@ from trulens.core.utils import json as json_utils
 from trulens.core.utils import pyschema as pyschema_utils
 from trulens.core.utils import python as python_utils
 from trulens.core.utils import serial as serial_utils
+from trulens.core.utils import signature as signature_utils
 from trulens.core.utils import threading as threading_utils
 
 logger = logging.getLogger(__name__)
@@ -421,6 +423,18 @@ class App(
         pydantic.PrivateAttr(default_factory=dict)
     )
 
+    tokens: List[object] = []
+    """
+    OTEL context tokens for the current context manager. These tokens are how the OTEL
+    context api keeps track of what is changed in the context, and used to undo the changes.
+    """
+
+    span_context: Optional[contextlib.AbstractContextManager] = None
+    """
+    Span context manager. Required to help keep track of the appropriate span context
+    to enter/exit.
+    """
+
     def __init__(
         self,
         connector: Optional[core_connector.DBConnector] = None,
@@ -692,61 +706,13 @@ class App(
             The extracted content, which may be a single value, a list of values,
             or a nested structure with content extracted from all levels.
         """
-        if isinstance(value, pydantic.BaseModel):
-            content = getattr(value, "content", None)
-            if content is not None:
-                return content
-
-            # If 'content' is not found, check for 'choices' attribute which indicates a ChatResponse
-            choices = getattr(value, "choices", None)
-            if choices is not None:
-                # Extract 'content' from the 'message' attribute of each _Choice in 'choices'
-                return [
-                    self._extract_content(choice.message) for choice in choices
-                ]
-
-            # Recursively extract content from nested pydantic models
-            try:
-                return {
-                    k: self._extract_content(v)
-                    if isinstance(v, (pydantic.BaseModel, dict, list))
-                    else v
-                    for k, v in value.model_dump().items()
-                }
-            except Exception as e:
-                logger.warning(
-                    "Failed to extract content from pydantic model: %s", e
-                )
-                # Unsure what is best to do here. Lets just return the exception
-                # so that it might reach the user if nothing else gets picked up
-                # as main input/output.
-                return str(e)
-
-        elif isinstance(value, dict):
-            # Check for 'content' key in the dictionary
-            for key in content_keys:
-                content = value.get(key)
-                if content is not None:
-                    return content
-
-            # Recursively extract content from nested dictionaries
-            return {
-                k: self._extract_content(v)
-                if isinstance(v, (dict, list))
-                else v
-                for k, v in value.items()
-            }
-
-        elif isinstance(value, list):
-            # Handle lists by extracting content from each item
-            return [self._extract_content(item) for item in value]
-
-        else:
-            return value
+        return signature_utils._extract_content(
+            value, content_keys=content_keys
+        )
 
     def main_input(
         self, func: Callable, sig: Signature, bindings: BoundArguments
-    ) -> serial_utils.JSON:
+    ) -> str:
         """Determine (guess) the main input string for a main app call.
 
         Args:
@@ -759,68 +725,7 @@ class App(
         Returns:
             The main input string.
         """
-
-        if bindings is None:
-            raise RuntimeError(
-                f"Cannot determine main input of unbound call to {func}: {sig}."
-            )
-
-        # ignore self
-        all_args = list(
-            v
-            for k, v in bindings.arguments.items()
-            if k not in ["self", "_self"]
-        )  # llama_index is using "_self" in some places
-
-        # If there is only one string arg, it is a pretty good guess that it is
-        # the main input.
-
-        # if have only containers of length 1, find the innermost non-container
-        focus = all_args
-
-        while (
-            not isinstance(focus, serial_utils.JSON_BASES) and len(focus) == 1
-        ):
-            focus = focus[0]
-            focus = self._extract_content(
-                focus, content_keys=["content", "input"]
-            )
-
-            if not isinstance(focus, Sequence):
-                logger.warning("Focus %s is not a sequence.", focus)
-                break
-
-        if isinstance(focus, serial_utils.JSON_BASES):
-            return str(focus)
-
-        # Otherwise we are not sure.
-        logger.warning(
-            "Unsure what the main input string is for the call to %s with args %s.",
-            python_utils.callable_name(func),
-            bindings,
-        )
-
-        # After warning, just take the first item in each container until a
-        # non-container is reached.
-        focus = all_args
-        while (
-            not isinstance(focus, serial_utils.JSON_BASES) and len(focus) >= 1
-        ):
-            focus = focus[0]
-            focus = self._extract_content(focus)
-
-            if not isinstance(focus, Sequence):
-                logger.warning("Focus %s is not a sequence.", focus)
-                break
-
-        if isinstance(focus, serial_utils.JSON_BASES):
-            return str(focus)
-
-        logger.warning(
-            "Could not determine main input/output of %s.", str(all_args)
-        )
-
-        return "TruLens: Could not determine main input from " + str(all_args)
+        return signature_utils.main_input(func, sig, bindings)
 
     def main_output(
         self,
