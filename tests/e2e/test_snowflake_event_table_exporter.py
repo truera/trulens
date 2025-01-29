7994a11e58fbd1ab4c6f8c11e688d7685cd41b6c
David Kurokawa <david.kurokawa@snowflake.com>
2025-01-28 19:14:49 -0800

    Clean  up.

--- tests/e2e/test_snowflake_event_table_exporter.py
+++ tests/e2e/test_snowflake_event_table_exporter.py
@@ -1,7 +1,7 @@
+import logging
 import os
 import time
-from typing import Dict, Sequence
-import unittest
+from typing import Sequence
 import uuid
 
 from snowflake.snowpark import Session
@@ -14,84 +14,46 @@ from trulens.experimental.otel_tracing.core.exporter.snowflake import (
 )
 
 from tests.unit.test_otel_tru_llama import TestOtelTruLlama
+from tests.util.snowflake_test_case import SnowflakeTestCase
 
 
-class TestSnowflakeEventTableExporter(unittest.TestCase):
-    # TODO: this is duplicated.
-    @classmethod
-    def clear_TruSession_singleton(cls) -> None:
-        # [HACK!] Clean up any instances of `TruSession` so tests don't
-        # interfere with each other.
-        for key in [
-            curr
-            for curr in TruSession._singleton_instances
-            if curr[0] == "trulens.core.session.TruSession"
-        ]:
-            del TruSession._singleton_instances[key]
-
-    @staticmethod
-    def _create_snowpark_session():
-        _snowflake_connection_parameters: Dict[str, str] = {
-            "account": os.environ["SNOWFLAKE_ACCOUNT"],
-            "user": os.environ["SNOWFLAKE_USER"],
-            "password": os.environ["SNOWFLAKE_USER_PASSWORD"],
-            "database": os.environ["SNOWFLAKE_DATABASE"],
-            "schema": os.environ["SNOWFLAKE_SCHEMA"],
-            "role": os.environ["SNOWFLAKE_ROLE"],
-            "warehouse": os.environ["SNOWFLAKE_WAREHOUSE"],
-        }
-        return Session.builder.configs(
-            _snowflake_connection_parameters
-        ).create()
+class TestSnowflakeEventTableExporter(SnowflakeTestCase):
+    logger = logging.getLogger(__name__)
 
     @staticmethod
     def _create_db_connector(snowpark_session: Session):
         return snowflake_connector.SnowflakeConnector(
             snowpark_session=snowpark_session,
-            init_server_side=False,
-            init_server_side_with_staged_packages=False,
+            init_database=False,
         )
 
-    # TODO: this is duplicated, mostly.
-    @classmethod
-    def setUpClass(cls) -> None:
-        # Set up Snowflake.
-        cls.snowpark_session = cls._create_snowpark_session()
-        cls.db_connector = cls._create_db_connector(cls.snowpark_session)
-        # Set up OTEL.
-        cls.exporter = TruLensSnowflakeSpanExporter(cls.db_connector)
-        # Set up TruSession.
+    def setUp(self) -> None:
+        super().setUp()
+        self.create_and_use_schema(
+            "TestSnowflakeEventTableExporter", append_uuid=True
+        )
         os.environ["TRULENS_OTEL_TRACING"] = "1"
-        cls.clear_TruSession_singleton()
-        cls.tru_session = TruSession(
-            cls.db_connector,
+        db_connector = self._create_db_connector(self._snowpark_session)
+        exporter = TruLensSnowflakeSpanExporter(db_connector)
+        self._tru_session = TruSession(
+            db_connector,
             experimental_feature_flags=[Feature.OTEL_TRACING],
-            _experimental_otel_exporter=cls.exporter,
+            _experimental_otel_exporter=exporter,
         )
-        cls.tru_session.experimental_enable_feature("otel_tracing")
-        return super().setUpClass()
+        self._tru_session.experimental_enable_feature("otel_tracing")
 
-    # TODO: this is duplicated.
-    @classmethod
-    def tearDownClass(cls) -> None:
-        cls.clear_TruSession_singleton()
-        cls.snowpark_session.close()
-        return super().tearDownClass()
-
-    @classmethod
-    def _run_query(cls, q: str):
-        return cls.snowpark_session.sql(q).collect()
-
-    @classmethod
     def _wait_for_num_results(
-        cls,
+        self,
         q: str,
         expected_num_results: int,
         num_retries: int = 30,
         retry_cooldown_in_seconds: int = 10,
     ) -> Sequence:
         for _ in range(num_retries):
-            results = cls._run_query(q)
+            results = self.run_query(q)
+            self.logger.info(
+                f"Got {len(results)} results, expecting {expected_num_results}"
+            )
             if len(results) == expected_num_results:
                 return results
             time.sleep(retry_cooldown_in_seconds)
@@ -110,7 +72,7 @@ class TestSnowflakeEventTableExporter(unittest.TestCase):
         with tru_recorder(run_name=run_name, input_id="42"):
             rag.query("What is multi-headed attention?")
         # Flush exporter and wait for data to be made to stage.
-        self.tru_session.experimental_force_flush()
+        self._tru_session.experimental_force_flush()
         # Check that the data is in the event table.
         self._wait_for_num_results(
             f"""
@@ -119,12 +81,11 @@ class TestSnowflakeEventTableExporter(unittest.TestCase):
             FROM EVENT_DB.PUBLIC.EVENTS
             WHERE
                 RECORD_TYPE = 'SPAN'
-                AND TIMESTAMP >= TO_TIMESTAMP_LTZ('2025-01-21 23:00:00')
-                -- AND TIMESTAMP <=TO_TIMESTAMP_LTZ('2025-01-11 23:00:00')
+                AND TIMESTAMP >= TO_TIMESTAMP_LTZ('2025-01-28 00:00:00')
                 AND RECORD_ATTRIBUTES['ai_observability.run_name'] = '{run_name}'
             ORDER BY TIMESTAMP DESC
             LIMIT 50
         """,
-            13,  # TODO: get this from the exporter or something?
+            8,  # TODO: get this from the exporter or something?
         )
         # TODO: call the feedback computation and check that it's fine.
