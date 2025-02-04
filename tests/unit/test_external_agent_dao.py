ab5cf92f6499c885e074830315bf4f9aa859f328
Daniel <daniel.huang@snowflake.com>
2025-02-04 09:32:10 -0800

    renamed

--- tests/unit/test_external_agent_dao.py
+++ tests/unit/test_external_agent_dao.py
@@ -1,5 +1,6 @@
 import sys
 from unittest import main
+from unittest import skipIf
 from unittest.mock import MagicMock
 from unittest.mock import patch
 
@@ -7,20 +8,14 @@ from tests.test import TruTestCase
 from tests.test import optional_test
 from tests.test import run_optional_tests
 
-try:
-    if run_optional_tests() and sys.version_info < (3, 12):
-        from trulens.connectors.snowflake.dao.external_agent import (
-            ExternalAgentDao,
-        )
-
-    else:
-        raise ImportError("Optional tests disabled")
-except ImportError:
-    # Define a dummy ExternalAgentDao so that patch decorators have something to reference.
-    class ExternalAgentDao:
-        pass
+if run_optional_tests():
+    from trulens.connectors.snowflake.dao.external_agent import ExternalAgentDao
 
 
+@skipIf(
+    sys.version_info >= (3, 12),
+    "trulens-connector-snowflake is not yet supported in Python 3.12",
+)
 @optional_test
 class TestExternalAgentDao(TruTestCase):
     def setUp(self):
@@ -34,7 +29,7 @@ class TestExternalAgentDao(TruTestCase):
         dummy_sql = MagicMock()
         dummy_sql.collect.return_value = []
         self.sf_session.sql.return_value = dummy_sql
-        self.manager = ExternalAgentDao(snowpark_session=self.sf_session)
+        self.dao = ExternalAgentDao(snowpark_session=self.sf_session)
 
     @patch.object(ExternalAgentDao, "_execute_query")
     @patch.object(ExternalAgentDao, "_fetch_query")
@@ -42,7 +37,7 @@ class TestExternalAgentDao(TruTestCase):
         self, mock_fetch_query, mock_execute_query
     ):
         mock_fetch_query.return_value = []
-        self.manager.create_agent_if_not_exist("agent1", "v1")
+        self.dao.create_agent_if_not_exist("agent1", "v1")
         expected_fqn = "DB.SCH.agent1"
         expected_query = "CREATE EXTERNAL AGENT ? WITH VERSION ?;"
         expected_parameters = (expected_fqn, "v1")
@@ -60,7 +55,7 @@ class TestExternalAgentDao(TruTestCase):
         self, mock_fetch_query, mock_execute_query
     ):
         mock_fetch_query.return_value = ["DB.SCH.agent1"]
-        self.manager.create_agent_if_not_exist("agent1", "v1")
+        self.dao.create_agent_if_not_exist("agent1", "v1")
         mock_execute_query.assert_not_called()
         mock_fetch_query.assert_called_once()
 
