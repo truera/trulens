90748ff4bb06b29a2c9aac51ac24c0723d635936
Daniel <daniel.huang@snowflake.com>
2025-02-04 01:43:07 -0800

    add str in enum

--- src/connectors/snowflake/trulens/connectors/snowflake/dao/enums.py
+++ src/connectors/snowflake/trulens/connectors/snowflake/dao/enums.py
@@ -2,6 +2,6 @@ from enum import Enum
 
 
 # snowflake object type
-class OBJECTTYPE(Enum):
+class OBJECTTYPE(str, Enum):
     EXTERNAL_AGENT = "EXTERNAL_AGENT"
     # CORTEX_SEARCH_SERVICE = "CORTEX_SEARCH_SERVICE"  # TODO: to be finalized
