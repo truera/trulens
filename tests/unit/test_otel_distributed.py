1ae22f12328a79ba67a06b14aac12b42ba261ce0
David Kurokawa <david.kurokawa@snowflake.com>
2025-02-06 00:22:59 -0800

    Create distributed test.

---
+++ tests/unit/test_otel_distributed.py
@@ -0,0 +1,108 @@
+from http.server import BaseHTTPRequestHandler
+from http.server import HTTPServer
+import multiprocessing
+import os
+import time
+import unittest
+
+import opentelemetry.context as context_api
+from opentelemetry.propagate import extract
+from opentelemetry.propagate import inject
+import requests
+from trulens.apps.app import TruApp
+from trulens.core.otel.instrument import instrument
+from trulens.core.session import TruSession
+from trulens.otel.semconv.trace import SpanAttributes
+
+from tests.util.otel_app_test_case import OtelAppTestCase
+
+
+class _TestApp:
+    @instrument(
+        span_type=SpanAttributes.SpanType.MAIN,
+        full_scoped_attributes={"process_id": os.getpid()},
+    )
+    def greet(self, name: str) -> str:
+        headers = {}
+        inject(headers, context=context_api.get_current())
+        response = requests.get(
+            f"http://localhost:8000/capitalize?name={name}", headers=headers
+        )
+        return response.text
+
+
+class CapitalizeHandler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        if self.path.startswith("/capitalize"):
+            context = extract(self.headers)
+            context_api.attach(context)
+            name = self.path.split("=")[1]
+            self.send_response(200)
+            self.send_header("Content-type", "text/plain")
+            self.end_headers()
+            self.wfile.write(self.capitalize(name).encode())
+            TruSession().force_flush()
+
+    @instrument(full_scoped_attributes={"process_id": os.getpid()})
+    def capitalize(self, name: str) -> str:
+        return name.upper()
+
+
+def run_server():
+    os.environ["TRULENS_OTEL_TRACING"] = "1"
+    TruSession()  # This starts an exporter.
+    server = HTTPServer(("localhost", 8000), CapitalizeHandler)
+    server.serve_forever()
+
+
+class TestOtelDistributed(OtelAppTestCase):
+    @classmethod
+    def setUpClass(cls):
+        cls.server_process = multiprocessing.Process(target=run_server)
+        cls.server_process.start()
+        time.sleep(2)  # Give the server time to start
+        super().setUpClass()
+
+    @classmethod
+    def tearDownClass(cls):
+        cls.server_process.terminate()
+        cls.server_process.join()
+        super().tearDownClass()
+
+    def test_distributed(self):
+        # Set up.
+        tru_session = TruSession()
+        tru_session.reset_database()
+        # Create TruApp that makes a network call.
+        test_app = _TestApp()
+        custom_app = TruApp(test_app, main_method=test_app.greet)
+        recorder = custom_app(run_name="test run", input_id="789")
+        with recorder:
+            test_app.greet("test")
+        # Compare results to expected.
+        tru_session.force_flush()
+        actual = self._get_events()
+        self.assertEqual(len(actual), 3)
+        self.assertNotEqual(
+            actual.iloc[1]["record_attributes"]["process_id"],
+            actual.iloc[2]["record_attributes"]["process_id"],
+        )
+        for attribute in [
+            "app_name",
+            "app_version",
+            "record_id",
+            "run_name",
+            "input_id",
+        ]:
+            self.assertEqual(
+                actual.iloc[1]["record_attributes"][
+                    f"ai.observability.{attribute}"
+                ],
+                actual.iloc[2]["record_attributes"][
+                    f"ai.observability.{attribute}"
+                ],
+            )
+
+
+if __name__ == "__main__":
+    unittest.main()
