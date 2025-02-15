7636bbf05da9f0031efee5a1833798da2e516900
Daniel <daniel.huang@snowflake.com>
2025-02-14 17:57:07 -0800

    remov run status (not used yet)

--- src/connectors/snowflake/trulens/connectors/snowflake/dao/enums.py
+++ src/connectors/snowflake/trulens/connectors/snowflake/dao/enums.py
@@ -4,9 +4,3 @@ from enum import Enum
 # snowflake object type
 class ObjectType(str, Enum):
     EXTERNAL_AGENT = "EXTERNAL AGENT"
-
-
-class RunStatus(str, Enum):
-    INACTIVE = "INACTIVE"
-    ACTIVE = "ACTIVE"
-    CANCELLED = "CANCELLED"
