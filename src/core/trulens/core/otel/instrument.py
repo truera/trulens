0b9be216c9dd4f6683d50fa2539f3ce6b3063514
David Kurokawa <david.kurokawa@snowflake.com>
2025-02-19 14:39:01 -0800

    Ensure there's only one record root span and also discern the main method if none given for `TruApp`.

--- src/core/trulens/core/otel/instrument.py
+++ src/core/trulens/core/otel/instrument.py
@@ -138,8 +138,8 @@ def _set_span_attributes(
 def instrument(
     *,
     span_type: SpanAttributes.SpanType = SpanAttributes.SpanType.UNKNOWN,
-    attributes: Attributes = dict(),
-    full_scoped_attributes: Attributes = dict(),
+    attributes: Attributes = None,
+    full_scoped_attributes: Attributes = None,
     must_be_first_wrapper: bool = False,
     **kwargs,
 ):
@@ -160,6 +160,10 @@ def instrument(
         If this is True and the function is already wrapped with the TruLens
         decorator, then the function will not be wrapped again.
     """
+    if attributes is None:
+        attributes = {}
+    if full_scoped_attributes is None:
+        full_scoped_attributes = {}
     is_record_root = span_type == SpanAttributes.SpanType.RECORD_ROOT
 
     def inner_decorator(func: Callable):
