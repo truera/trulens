64895afeb4a92e9bb96a05636ab3be2120074d9d
Daniel <daniel.huang@snowflake.com>
2025-02-27 20:46:56 -0800

    ut updated

--- tests/unit/test_snowflake_run_dao.py
+++ tests/unit/test_snowflake_run_dao.py
@@ -5,6 +5,7 @@ from unittest.mock import patch
 
 import pandas as pd
 import pytest
+from trulens.connectors.snowflake.dao.sql_utils import double_quote_identifier
 
 try:
     from trulens.connectors.snowflake.dao.run import RunDao
@@ -48,7 +49,7 @@ class TestRunDao(unittest.TestCase):
         dataset_spec = {"col1": "col1"}
 
         req_payload = {
-            "object_name": object_name,
+            "object_name": double_quote_identifier(object_name),
             "object_type": object_type,
             "object_version": object_version,
             "run_name": run_name,
@@ -122,7 +123,7 @@ class TestRunDao(unittest.TestCase):
     def test_delete_run(self, mock_execute_query):
         req_payload = {
             "run_name": "my_run",
-            "object_name": "MY_AGENT",
+            "object_name": double_quote_identifier("MY_AGENT"),
             "object_type": "EXTERNAL AGENT",
         }
         req_payload_json = json.dumps(req_payload)
