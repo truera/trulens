6dc4a8e8bdf97bff124e1ea502ee7c3f9cbffb0b
Prudhvi Dharmana <prudhvi.dharmana@snowflake.com>
2025-03-05 19:45:31 -0800

    fix copilot suggestion

--- src/core/trulens/core/app.py
+++ src/core/trulens/core/app.py
@@ -1764,9 +1764,7 @@ you use the `%s` wrapper to make sure `%s` does get instrumented. `%s` method
             run_metadata_df,
             {
                 "app": self,
-                "main_method_name": self.main_method_name or ""
-                if self.main_method_name
-                else "",
+                "main_method_name": self.main_method_name or "",
                 "run_dao": self.snowflake_run_dao,
                 "tru_session": self.session,
             },
