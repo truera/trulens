55b84c0730e44ff248d71ab97705f9786eaab594
Nikhil Vytla <nikhil.vytla@snowflake.com>
2025-04-11 15:21:58 -0700

    [SNOW-2005227, SNOW-2037060] Add HuggingFace pytest annotation and deflake E2E test issues (#1907)
    * Add TEST_HUGGINGFACE option to enable/disable HF tests
    
    * Increase HF model response timeout to 30s to reduce flakiness
    
    * Update tested responses, add huggingface annotations to corresponding provider tests
    
    * Update Makefile command with TEST_HUGGINGFACE
    
    * Update deprecated library call
    
    * Update answer list for TestLlamaIndex
    
    * Update answers for TestLlamaIndex
    
    * Change E2E pipeline to run all tests
    
    * Focus on a smaller subset of tests for E2E
    
    * Fix typo in env var
    
    * Fix nit in Jenkinsfile
    
    * Add Azure OpenAI cred paths from secrets vault
    
    * Identify custom huggingface marker to reduce pytest warnings
    
    * Update harmfulness prompt to reduce flakiness of test_langchain_feedback
    
    * Add comment for follow-up
    
    * Push huggingface tests into a separate stage to deal with unstable inference API
    
    * Separate huggingface stage in e2e pipeline

--- tests/e2e/test_tru_llama.py
+++ tests/e2e/test_tru_llama.py
@@ -32,7 +32,9 @@ class TestLlamaIndex(TruTestCase):
     ANSWERS = {
         "What did the author do growing up?": set([
             "The author worked on writing and",
-            "The author worked on writing short"
+            "The author worked on writing short",
+            "The author focused on writing and",
+            "The author focused on writing short",
             "I couldn't find specific information",
         ])
     }
