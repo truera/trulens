1d4b39c8a3c9efa478f87a4612e4215c399c45d5
Garett Tok Ern Liang <garett.tok@snowflake.com>
2025-04-29 12:52:56 -0700

    rebuild

--- src/dashboard/trulens/dashboard/components/record_viewer_otel/__init__.py
+++ src/dashboard/trulens/dashboard/components/record_viewer_otel/__init__.py
@@ -6,7 +6,7 @@ import streamlit.components.v1 as components
 # the component, and True when we're ready to package and distribute it.
 # (This is, of course, optional - there are innumerable ways to manage your
 # release process.)
-_RELEASE = False
+_RELEASE = True
 
 # Declare a Streamlit component. `declare_component` returns a function
 # that is used to create instances of the component. We're naming this
@@ -18,7 +18,7 @@ _RELEASE = False
 # *only thing* you need to do to create the binding between Streamlit and
 # your component frontend. Everything else we do in this file is simply a
 # best practice.
-_record_viewer = None
+_record_viewer_otel = None
 
 
 # Create a wrapper function for the component. This is an optional
@@ -43,13 +43,13 @@ def record_viewer_otel(spans, key=None) -> str:
     #
     # "default" is a special argument that specifies the initial return
     # value of the component before the user has interacted with it.
-    global _record_viewer
+    global _record_viewer_otel
 
-    if _record_viewer is None:
+    if _record_viewer_otel is None:
         if not _RELEASE:
-            _record_viewer = components.declare_component(
+            _record_viewer_otel = components.declare_component(
                 # We give the component a simple, descriptive name
-                "record_viewer",
+                "record_viewer_otel",
                 # Pass `url` here to tell Streamlit that the component will be served
                 # by the local dev server that you run via `npm run start`.
                 # (This is useful while your component is in development.)
@@ -61,10 +61,10 @@ def record_viewer_otel(spans, key=None) -> str:
             # build directory:
             parent_dir = os.path.dirname(os.path.abspath(__file__))
             build_dir = os.path.join(parent_dir, "dist")
-            _record_viewer = components.declare_component(
-                "record_viewer", path=build_dir
+            _record_viewer_otel = components.declare_component(
+                "record_viewer_otel", path=build_dir
             )
 
-    component_value = _record_viewer(spans=spans, key=key, default="")
+    component_value = _record_viewer_otel(spans=spans, key=key, default="")
 
     return component_value
