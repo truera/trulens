a0b381b50c918685de27d5daa08ea6da6b23cacd
David Kurokawa <david.kurokawa@snowflake.com>
2025-05-02 11:45:22 -0700

    Add accordion functionality to Panel.

--- src/dashboard/react_components/record_viewer_otel/src/Panel/Panel.tsx
+++ src/dashboard/react_components/record_viewer_otel/src/Panel/Panel.tsx
@@ -1,9 +1,19 @@
 import { Box, SxProps, Theme, Typography } from '@mui/material';
-import { PropsWithChildren, useId } from 'react';
+import { PropsWithChildren, useId, useState } from 'react';
+import KeyboardArrowDownRounded from '@mui/icons-material/KeyboardArrowDownRounded';
+import KeyboardArrowUpRounded from '@mui/icons-material/KeyboardArrowUpRounded';
 
-type PanelProps = PropsWithChildren<{ header: string; id?: string }>;
+type PanelProps = PropsWithChildren<{
+  header: string;
+  id?: string;
+}>;
 
 export default function Panel({ header, children, id }: PanelProps) {
+  // Handle accordion state
+  const [expanded, setExpanded] = useState(true);
+  const toggleExpand = () => {
+    setExpanded(!expanded);
+  }
   // Generate a unique ID if one isn't provided
   const reactId = useId();
   const panelId = id ?? reactId;
@@ -12,15 +22,24 @@ export default function Panel({ header, children, id }: PanelProps) {
 
   return (
     <Box sx={panelSx} role="section" aria-labelledby={headerId}>
-      <Box className="panel-header" component="header" id={headerId}>
+      <Box
+        className="panel-header"
+        component="header"
+        id={headerId}
+        sx={{ display: 'flex', alignItems: 'center' }}
+        aria-expanded={expanded}
+        aria-controls={contentId}
+        onClick={toggleExpand}
+      >
         <Typography variant="body2" fontWeight="bold" component="h3">
           {header}
         </Typography>
       </Box>
-
-      <Box className="panel-content" id={contentId} aria-labelledby={headerId}>
-        {children}
-      </Box>
+      {expanded && (
+        <Box className="panel-content" id={contentId} aria-labelledby={headerId} aria-hidden={!expanded}>
+          {children}
+        </Box>
+      )}
     </Box>
   );
 }
@@ -34,6 +53,13 @@ const panelSx: SxProps<Theme> = ({ spacing, palette }) => ({
     background: palette.grey[50],
     p: 1,
     borderBottom: `1px solid ${palette.grey[300]}`,
+  },
+
+  '& .panel-accordion-button': {
+    background: 'transparent',
+    border: 'none',
+    cursor: 'pointer',
+    marginRight: spacing(1),
   },
 
   '& .panel-content': {
