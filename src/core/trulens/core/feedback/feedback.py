91f9c58d11dd1c05b97b647dc9560b09cc9b2cd6
David Kurokawa <david.kurokawa@snowflake.com>
2025-05-11 00:03:06 -0700

    Remove unnecessary function.

--- src/core/trulens/core/feedback/feedback.py
+++ src/core/trulens/core/feedback/feedback.py
@@ -7,7 +7,6 @@ from inspect import signature
 import itertools
 import json
 import logging
-import os
 from pprint import pformat
 import traceback
 from typing import (
@@ -619,10 +618,6 @@ class Feedback(feedback_schema.FeedbackDefinition):
 
     # alias
     on_output = on_response
-
-    @staticmethod
-    def _otel_enabled() -> bool:
-        return os.getenv("TRULENS_OTEL_TRACING", "").lower() in ["1", "true"]
 
     def on(self, *args, **kwargs) -> Feedback:
         """
