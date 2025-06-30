37076a102f0b8602ed455661d6cb1243abb5ec05
corey hu <corey.hu@snowflake.com>
2025-06-30 11:27:03 -0700

    Add structured outputs to feedback providers (#2098)
    * add structured outputs and typing fixes
    
    * fix typing for <=3.9
    
    * write api
    
    * update
    
    * use openai client
    
    * rm semconv
    
    * fix cortex formatting args
    
    * provider and output object type fixes
    
    * factor out fields
    
    * cortex typing changes
    
    * use union typing
    
    * fmt
    
    * update openai provider typing

--- src/providers/cortex/trulens/providers/cortex/endpoint.py
+++ src/providers/cortex/trulens/providers/cortex/endpoint.py
@@ -142,8 +142,6 @@ class CortexEndpoint(core_endpoint.Endpoint):
         response: Any,
         callback: Optional[core_endpoint.EndpointCallback],
     ) -> Any:
-        counted_something = False
-
         response_dict = None, None
 
         try:
@@ -163,16 +161,13 @@ class CortexEndpoint(core_endpoint.Endpoint):
                     "finish_reason" in choice
                     and choice["finish_reason"] == "stop"
                 ):
-                    counted_something = True
-
                     self.global_callback.handle_generation(
                         response=response_dict
                     )
 
                     if callback is not None:
                         callback.handle_generation(response=response_dict)
-
-        if not counted_something:
+        else:
             logger.warning(
                 "Unrecognized Cortex response format. It did not have usage information:\n%s",
                 pp.pformat(response_dict),
