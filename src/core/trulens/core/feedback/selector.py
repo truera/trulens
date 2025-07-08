c9005ececaf550439ac18e7430e4529f1e4d30ff
David Kurokawa <david.kurokawa@snowflake.com>
2025-07-08 16:33:57 -0700

    Set `ignore_none_values` to `False` by default.

--- src/core/trulens/core/feedback/selector.py
+++ src/core/trulens/core/feedback/selector.py
@@ -111,7 +111,7 @@ class Selector:
     # If the value extracted from the span is None (i.e. from
     # `span_attributes_processor`, `span_attribute`, or `function_attribute`),
     # then whether to ignore it or not.
-    ignore_none_values: bool = True
+    ignore_none_values: bool = False
 
     # If this selector describes a list, which of the following two should we
     # do:
