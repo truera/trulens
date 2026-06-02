cb259362596290edddc653987ad216f34f438bc1
Animesh452 <68946005+Animesh452@users.noreply.github.com>
2026-06-02 14:21:56 -0700

    standardize provider __init__.py exports for langchain and litellm (#2525)

--- src/providers/langchain/trulens/providers/langchain/__init__.py
+++ src/providers/langchain/trulens/providers/langchain/__init__.py
@@ -10,13 +10,16 @@
 !!! note
     LangChain provider cannot be used in `deferred` mode due to inconsistent serialization capabilities of LangChain apps.
 """
+# WARNING: This file does not follow the no-init aliases import standard.
 
 from importlib.metadata import version
 
-from trulens.core.utils.imports import safe_importlib_package_name
+from trulens.core.utils import imports as import_utils
 from trulens.providers.langchain.provider import Langchain
 
-__version__ = version(safe_importlib_package_name(__package__ or __name__))
+__version__ = version(
+    import_utils.safe_importlib_package_name(__package__ or __name__)
+)
 
 
 __all__ = [
